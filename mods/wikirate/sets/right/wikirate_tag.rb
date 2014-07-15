76d0b15014f8fd3108777f316dda702b5be5fb46
chuenlok <henrytai@henrys-mbp.home>
2014-07-15 22:54:43 +0100

    remove wikirate_tag.rb

--- mods/wikirate/sets/right/wikirate_tag.rb
+++
@@ -1,9 +0,0 @@
-event :create_missing_tags, :after=>:store, :on=>:save do
-	full_tagname = Card[name].content
-	tagname = full_tagname[2..full_tagname.length-3]
-	
-	if !Card.exists? tagname  
-		Card.create! :type=>"Tag", :name=>tagname,:subcards=>{'+Article' => {'content'=> '', "type_id"=>"3"} }
-	
-	end
-end
