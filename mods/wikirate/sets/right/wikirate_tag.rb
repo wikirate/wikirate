def8c5df34dc045b6be6921a9b74f29fc558c684
chuenlok <henrytai@henrys-mbp.home>
2014-07-15 22:25:05 +0100

    add clipboard view for claim, create related tag card when creating claim

--- mods/wikirate/sets/right/wikirate_tag.rb
+++ mods/wikirate/sets/right/wikirate_tag.rb
@@ -1,8 +1,11 @@
 require 'byebug'
 event :create_missing_tags, :after=>:store, :on=>:save do
 	byebug
-	if !Card.exists? name  
-		Card.create! :type=>"Tag", :name=>name,:subcards=>{'+Article' => {'content'=> '', "type_id"=>"3"} }
+	full_tagname = Card[name].content
+	tagname = full_tagname[2..full_tagname.length-3]
+	
+	if !Card.exists? tagname  
+		Card.create! :type=>"Tag", :name=>tagname,:subcards=>{'+Article' => {'content'=> '', "type_id"=>"3"} }
 	
 	end
 end
