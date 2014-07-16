a7cd25fa903dcb17f17998b479db4af2b36055eb
chuenlok <henrytai@Henrys-MacBook-Pro.local>
2014-07-16 15:04:01 +0100

    add back wikirate_tag.rb to add tag card while creating claim with tag

---
+++ mods/wikirate/sets/right/wikirate_tag.rb
@@ -0,0 +1,13 @@
+require 'byebug'
+event :create_missing_tags, :after=>:store, :on=>:save do
+  byebug
+  new_right_tag = Card[name]
+  new_tags = new_right_tag.item_cards
+  new_tags.each do |tag|
+    new_tag_name = tag.name
+    if !Card.exists? new_tag_name  
+      Card.create! :type_id=>Card::WikirateTagID, :name=>new_tag_name
+    end
+  end
+  
+end
