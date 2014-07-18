35bd77b2b4bf4f260292763c65b8df2e33263c62
chuenlok <henrytai@Henrys-MacBook-Pro.local>
2014-07-18 16:19:27 +0100

    add update_tag.rb to add missing tag fix webpage_spec as the sourcebox is changed from true to 'true' and testing action should new not create in checking empty url

--- mods/wikirate/spec/mods/wikirate/sets/type/webpage_spec.rb
+++ mods/wikirate/spec/mods/wikirate/sets/type/webpage_spec.rb
@@ -8,8 +8,8 @@ describe Card::Set::Type::Webpage do
     end
     it "should add title,description" do
       
-        url = 'http://www.google.com/?q=wikirateissocoolyouknow'
-        Card::Env.params[:sourcebox] = true
+        url = 'http://www.google.com/?q=wikirateissocoolandawesomeyouknow'
+        Card::Env.params[:sourcebox] = 'true'
         sourcepage = Card.create! :type_id=>Card::WebpageID,:subcards=>{ '+Link' => {:content=> url} }
         preview = LinkThumbnailer.generate(url)
 
@@ -19,16 +19,16 @@ describe Card::Set::Type::Webpage do
     end
     it "should handle empty url" do
         url = ''
-        Card::Env.params[:sourcebox] = true
-        sourcepage = Card.create! :type_id=>Card::WebpageID,:subcards=>{ '+Link' => {:content=> url} }
+        Card::Env.params[:sourcebox] = 'true'
+        sourcepage = Card.new :type_id=>Card::WebpageID,:subcards=>{ '+Link' => {:content=> url} }
         sourcepage.should_not be_valid
         sourcepage.errors.should have_key :link
         sourcepage.errors[:source]=="is empty"
     end
 
     it "should handle duplicated url " do
-      url = 'http://www.google.com/?q=wikirateissocoolyouknow'
-      Card::Env.params[:sourcebox] = true
+      url = 'http://www.google.com/?q=wikirateissocoolandawesomeyouknow'
+      Card::Env.params[:sourcebox] = 'true'
       firstsourcepage = Card.create! :type_id=>Card::WebpageID,:subcards=>{ '+Link' => {:content=> url} }
       secondsourcepage = Card.create! :type_id=>Card::WebpageID,:subcards=>{ '+Link' => {:content=> url} }
       firstsourcepage.name.should == secondsourcepage.name
