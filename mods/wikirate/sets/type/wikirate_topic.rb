d82341d55ce79aa71b30119599d5bc985d397d70
chuenlok <henrytai@Henrys-MacBook-Pro.local>
2014-07-22 14:45:22 +0100

    remove the image view for topic

--- mods/wikirate/sets/type/wikirate_topic.rb
+++ mods/wikirate/sets/type/wikirate_topic.rb
@@ -1,19 +1,3 @@
 view :missing do |args|
     _render_link args
 end
-
-view :image do |args|
-  #byebug
-  image_card = Card.fetch("#{ card.name }+image")
-  image_source_card = Card.fetch("#{ card.name }+image source")
-  image_url = ""
-  if image_card
-    image_url = image_card.format( :format=>:html)._render(:source) 
-  end
-  title = ""
-  if image_source_card
-    title = strip_tags image_source_card.content
-  end
-  %{<img src='#{image_url}' title='#{title}' />}
-
-end
