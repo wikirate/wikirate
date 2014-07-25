4e957de6c83d84631f9e0d2cafe355e66adb1a84
chuenlok <henrytai@dhcp-172-17-193-97.eduroam.wireless.private.cam.ac.uk>
2014-07-25 12:36:52 +0100

    change from hash to object for metadata

--- mod/wikirate/set/self/webpage.rb
+++ mod/wikirate/set/self/webpage.rb
@@ -6,47 +6,51 @@ end
 
 format :json do
   view :metadata do |args|
-
-    metadata = Hash.new
-    metadata["title"] = ""
-    metadata["description"]  = ""
-    metadata["image_url"]  = ""
-    metadata["website"] = ""
-    metadata["error"] = ""
+    metadata = MetaData.new
     url = Card::Env.params[:url]||args[:url] ||""
     if url.empty?
-      metadata["error"] = 'empty url'
+      metadata.error = 'empty url'
       return metadata.to_json
     end
     begin      
-      metadata["website"] = URI(url).host
+      metadata.website = URI(url).host
     rescue    
     end
-    if !metadata["website"]
-      metadata["error"] = 'invalid url' 
+    if !metadata.website
+      metadata.error = 'invalid url' 
       return metadata.to_json
     end
     duplicates = card.find_duplicates url
     if duplicates.any?
-      
-      origin_link_card = duplicates.first
-      origin_page_card = origin_link_card.left
-      metadata["title"] = Card["#{origin_page_card.name}+title"].content
-      metadata["description"]  = Card["#{origin_page_card.name}+description"].content
-      metadata["image_url"]  = Card["#{origin_page_card.name}+image_url"].content
+      origin_page_card = duplicates.first.left
+      metadata.set_meta_data Card["#{origin_page_card.name}+title"].content, Card["#{origin_page_card.name}+description"].content, Card["#{origin_page_card.name}+image_url"].content
     else
       begin 
         preview = LinkThumbnailer.generate url
         if preview.images.length > 0
           image_url = preview.images.first.src.to_s
         end
-          metadata["title"] = preview.title
-          metadata["description"]  = preview.description
-          metadata["image_url"]  = image_url
+          metadata.set_meta_data preview.title, preview.description, image_url
       rescue
-        Rails.logger.info "Fail to extract information from the #{ url }"
       end
     end
     metadata.to_json
   end
 end
+
+class MetaData  
+  attr_accessor :title,:description,:image_url,:website,:error
+  def initialize()  
+    @title = ""
+    @description = ""
+    @image_url  =""
+    @website = ""
+    @error = ""
+  end  
+  def set_meta_data title,desc,image_url
+    @title = title
+    @description = desc
+    @image_url = image_url
+  end
+end  
+
