3f1433180316ea6b4d77a456056b2ac6e6f1f658
chuenlok <clt58@cam.ac.uk>
2014-10-21 17:03:08 +0100

    no need to test whether there is editor

--- mod/wikirate/set/right/about.rb
+++ mod/wikirate/set/right/about.rb
@@ -2,16 +2,12 @@
 format :html do
   view :better_about ,:perms=>:none do |args|
     editor_card = Card.fetch card.name+"+*editor"
-    show_editor = editor_card.item_names.length>0
     %{
-      
       <div class="page-heading">
         <h1>About</h1>
         <div class="edits-by">
-
-          #{"<div class='subtitle-header'>Edits by</div>" if show_editor}
-          #{editor_card.format.render_shorter_search_result :item=>:link if show_editor }
-          
+          <div class='subtitle-header'>Edits by</div>
+          #{editor_card.format.render_shorter_search_result :item=>:link}
         </div>
       </div>
       #{card.format.render_content :home_view=>"content"}
