ca663beaade0047643426496d4a4e69eeb7be71a
xithan <philipp.kuehl@gmail.com>
2014-10-23 23:33:21 +0200

    working on contribution

--- mod/wikirate/set/type/wikirate_company.rb
+++ mod/wikirate/set/type/wikirate_company.rb
@@ -1,16 +1,15 @@
 card_accessor :contribution_count, :type=>:number, :default=>"0"
+card_accessor :direct_contribution_count, :type=>:number, :default=>"0"
 
 view :missing do |args|
   _render_link args
 end
 
-def contributer_search_args
-  descendants = ['about']
+def indirect_contributer_search_args
   [
     {:type=>'claim', :plus=>['company',:link_to=>self.name]},
     {:type=>'page', :plus=>['company',:link_to=>self.name]},
-    {:right=>{:name=>(descendants.size > 1 ? ['in'].concat(descendants) : descendants.first)}},
-    {:right=>'article', :left=>{:left=>self.name}}
+    {:type=>'analysis', :left=>self.name }
   ]
 end
 
--- mod/wikirate/set/type/wikirate_topic.rb
+++ mod/wikirate/set/type/wikirate_topic.rb
@@ -1,15 +1,14 @@
 card_accessor :contribution_count, :type=>:number, :default=>"0"
+card_accessor :direct_contribution_count, :type=>:number, :default=>"0"
 
 view :missing do |args|
   _render_link args
 end
 
-def contributer_search_args
-  descendants = ['about']
+def indirect_contributer_search_args
   [
     {:type=>'claim', :plus=>['topic',:link_to=>self.name]},
     {:type=>'page', :plus=>['topic',:link_to=>self.name]},
-    {:right=>{:name=>(descendants.size > 1 ? ['in'].concat(descendants) : descendants.first)}},
-    {:right=>'article', :left=>{:right=>self.name}}
+    {:type=>'analysis', :right=>self.name }
   ]
 end
