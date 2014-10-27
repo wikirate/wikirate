b4088e2430fbfc9bb8394029a701e48c69012db3
chuenlok <clt58@cam.ac.uk>
2014-10-27 17:35:02 +0000

    tests for image.rb and some for article

--- mod/wikirate/spec/mod/wikirate/set/right/wikirate_article_spec.rb
+++ mod/wikirate/spec/mod/wikirate/set/right/wikirate_article_spec.rb
@@ -1,3 +1,32 @@
 describe Card::Set::Right::WikirateArticle do
+  describe "views" do
+    it "return edit view when rendering content view if params[:edit_article] is true" do 
+      Card::Env.params[:edit_article] = true
+      company = Card.create :name=>"company1",:type=>"company"
+      topic = Card.create :name=>"topic1",:type=>"topic"
+      article = Card.create :name=>"company1+topic1+article",:type=>"basic",:content=>"hello world"
+      html = article.format.render_content
+      expect(html).to eq(article.format.render_editor)
+    end
+    it "renders content view if params[:edit_article] is false/nil" do 
 
+    end
+    it "return edit view when rendering missing view if params[:edit_article] is true" do 
+      Card::Env.params[:edit_article] = true
+      company = Card.create :name=>"company1",:type=>"company"
+      topic = Card.create :name=>"topic1",:type=>"topic"
+      article = Card.create :name=>"company1+topic1+article",:type=>"basic",:content=>"hello world"
+      html = article.format.render_content
+      expect(html).to eq(article.format.render_editor)
+    end
+    it "renders missing view if params[:edit_article] is false/nil" do 
+
+    end
+    it "renders editor view if citable" do 
+
+    end
+    it "renders editor view normal" do 
+
+    end
+  end
 end
