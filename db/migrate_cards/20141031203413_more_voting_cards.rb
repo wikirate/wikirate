60a9b703e0d932480296652a3cb4837ba906ddd2
xithan <philipp.kuehl@gmail.com>
2014-11-03 20:06:33 +0100

    updated migrations

--- db/migrate_cards/20141031203413_more_voting_cards.rb
+++ db/migrate_cards/20141031203413_more_voting_cards.rb
@@ -1,13 +1,7 @@
 # -*- encoding : utf-8 -*-
 
-class MoreVotingCards < ActiveRecord::Migration
-  include Wagn::MigrationHelper
+class MoreVotingCards < Wagn::Migration
   def up
-    contentedly do
-      filename = 'voting.json'
-      raw_json = File.read File.join( Wagn.root, "db/json/#{filename}" )
-      json = JSON.parse raw_json
-      Card.merge_list json["card"]["value"], :output_file=>"/tmp/unmerged_#{ filename }"
-    end
+    import 'voting.json'
   end
 end
