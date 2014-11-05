079a9948ab7b1a4673adb28d8cc0a86d59822005
Ethan McCutchen <ethan@grasscommons.org>
2014-11-05 15:25:17 -0700

    altered sys email migration to reflect deeper email changes

--- db/migrate_cards/20141104170106_update_system_emails.rb
+++ db/migrate_cards/20141104170106_update_system_emails.rb
@@ -2,7 +2,56 @@
 
 class UpdateSystemEmails < Wagn::Migration
   def up
+
+    #get rid of previous emails
+    
+    [:confirmation_email, :password_reset, :signup_alert].each do |email_template|
+      etmpl = Card[email_template]
+      etmpl.codename = nil
+      etmpl.delete!
+    end
+
+    # create system email cards
     dir = "#{Wagn.gem_root}/db/migrate_cards/data/mailer"
+    json = File.read( File.join( dir, 'mail_config.json' ))
+    data = JSON.parse(json)
+    data.each do |mail|
+      mail = mail.symbolize_keys!
+      Card.create! :name=> mail[:name], :codename=>mail[:codename], :type_id=>Card::EmailTemplateID
+      Card.create! :name=>"#{mail[:name]}+*html message", :content=>File.read( File.join( dir, "#{mail[:codename]}.html" ))
+      Card.create! :name=>"#{mail[:name]}+*text message", :content=>File.read( File.join( dir, "#{mail[:codename]}.txt" ))
+      Card.create! :name=>"#{mail[:name]}+*subject", :content=>mail[:subject] 
+    end
+    
+    
+    # move old hard-coded signup alert email handling to new card-based on_create handling
+    Card.create!(
+      :name=>( [:signup, :type, :on_create].map { |code| Card[code].name } * '+'),
+      :type_id=>Card::PointerID, :content=>"[[signup alert email]]"
+    )
+    if request_card = Card[:request]
+      [:to, :from].each do |field|
+        if old_card = request_card.fetch(:trait=>field) and !old_card.content.blank?
+          Card.create! :name=>"signup alert email+#{Card[field].name}", :content=>old_card.content
+        end
+      end
+      request_card.codename = nil
+      request_card.delete!
+    end
+    
+    # migrate old flexmail cards
+
+    if email_config_card = Card['email_config']
+      
+      # FIXME - add email config migrations here...
+      
+      email_config_card.delete!
+    end
+    
+    
+    
+    
+    
     json = File.read( File.join( dir, 'mail_config.json' ))
     data = JSON.parse(json)
     data.each do |mail|
