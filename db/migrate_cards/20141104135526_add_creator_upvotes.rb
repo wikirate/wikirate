ef452bf106852a01504e441594289d6103bad8c6
Ethan McCutchen <ethan@grasscommons.org>
2014-11-04 19:59:29 -0700

    rescue in vote migration

--- db/migrate_cards/20141104135526_add_creator_upvotes.rb
+++ db/migrate_cards/20141104135526_add_creator_upvotes.rb
@@ -3,17 +3,21 @@
 class AddCreatorUpvotes < Wagn::Migration
   def up
     Card.search(:type_id => [:in, Card::ClaimID, Card::WebpageID], :return=>:name).each do |name|
-      subject = Card[name]
-      if subject.creator.id != Card::WagnBotID   and 
-         !subject.creator.upvotes_card.include_item? "~#{subject.id}" and 
-         !subject.creator.downvotes_card.include_item? "~#{subject.id}"
+      begin
+        subject = Card[name]
+        if subject.creator.id != Card::WagnBotID   and 
+           !subject.creator.upvotes_card.include_item? "~#{subject.id}" and 
+           !subject.creator.downvotes_card.include_item? "~#{subject.id}"
          
-        up_card = subject.creator.upvotes_card
-        up_card.add_id subject.id
-        up_card.save!
-        vc = subject.vote_count_card
-        vc.update_votecount
-        vc.save!
+          up_card = subject.creator.upvotes_card
+          up_card.add_id subject.id
+          up_card.save!
+          vc = subject.vote_count_card
+          vc.update_votecount
+          vc.save!
+        end
+      rescue => e
+        puts "Error migrating votes for #{subject.name} : #{e.message}"
       end
     end
   end
