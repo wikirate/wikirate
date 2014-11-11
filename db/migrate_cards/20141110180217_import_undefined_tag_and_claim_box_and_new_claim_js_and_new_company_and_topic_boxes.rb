2b9b5aa889f0466e01f9f85826113f4d4d83a871
Ethan McCutchen <ethan@grasscommons.org>
2014-11-11 01:21:34 -0700

    fix migration

--- db/migrate_cards/20141110180217_import_undefined_tag_and_claim_box_and_new_claim_js_and_new_company_and_topic_boxes.rb
+++ db/migrate_cards/20141110180217_import_undefined_tag_and_claim_box_and_new_claim_js_and_new_company_and_topic_boxes.rb
@@ -2,6 +2,6 @@
 
 class ImportUndefinedTagAndClaimBoxAndNewClaimJsAndNewCompanyAndTopicBoxes < Wagn::Migration
   def up
-          import_json "undefined_tag_and_claim_box_and_new_claim_js_and_new_company_and_topic_boxes.json"
+          import_json "boxes.json"
       end
 end
