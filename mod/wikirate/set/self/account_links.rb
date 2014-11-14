39aa237a8603f49cbecd38f98b77cec7bd84670d
Ethan McCutchen <ethan@grasscommons.org>
2014-11-13 22:49:07 -0700

    added css classes to account_links

--- mod/wikirate/set/self/account_links.rb
+++ mod/wikirate/set/self/account_links.rb
@@ -3,20 +3,23 @@ format :html do
   view :raw do |args|
     #ENGLISH
     links = []
+    klasses = ''
     if Auth.signed_in?
+      klasses = 'logged-in'
       links << link_to_page( Auth.current.name, nil, :id=>'my-card-link' )
       if Card.new(:type_id=>Card.default_accounted_type_id).ok? :create
         links << link_to( 'Invite', wagn_path('new/:signup'), :id=>'invite-a-friend-link' )
       end
       links << link_to( 'Log out', wagn_path('delete/:signin'), :id=>'signout-link' )
     else
+      klasses = 'logged-out'
       if Card.new(:type_id=>Card::SignupID).ok? :create
         links << link_to( 'Join', wagn_path('new/:signup'), :id=>'signup-link', :class=>'button-primary' )
       end
       links << link_to( 'Log in', wagn_path(':signin'), :id=>'signin-link', :class=>'button-primary button-secondary' )
     end
     
-    %{<span id="logging">#{ links.join ' ' }</span>}
+    %{<span id="logging" class="#{klasses}">#{ links.join ' ' }</span>}
   end
 
 end
