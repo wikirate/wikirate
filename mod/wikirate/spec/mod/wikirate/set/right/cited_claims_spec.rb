05e8c4241f909505f3d5f5514a962344e3532311
chuenlok <clt58@cam.ac.uk>
2014-11-27 17:47:28 +0000

    more tests

--- mod/wikirate/spec/mod/wikirate/set/right/cited_claims_spec.rb
+++ mod/wikirate/spec/mod/wikirate/set/right/cited_claims_spec.rb
@@ -17,9 +17,11 @@ describe Card::Set::Right::CitedClaims do
       sample_article = Card[@sample_analysis.name+"+#{Card[:wikirate_article].name}"]
       sample_article.content = "I need some chewing gum.#{claim_card.default_citation}"
       sample_article.save
-      binding.pry
+      
       html = @sample_analysis.format.render_core
-      puts "hello"
+      expect(html.squish).to include(%{<span class="cited-claim-number">1</span> <div id="whateverclaim"})
+      expect(html.squish).to include(%{<i class=\"fa fa-clipboard claim-clipboard\" id=\"copy-button\" title=\"copy claim citation to clipboard\" data-clipboard-text=\"Death Star uses dark side of the Force {{Death Star uses dark side of the Force|cite}}\"></i> <div id=\"Death_Star_uses_dark_side_of_the_Force\"})
+
     end
   end
 
