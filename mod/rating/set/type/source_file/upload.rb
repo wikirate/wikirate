3c5583df2c689cbd9934bcb9b4e2cc7509050aef
chuenlok <clt58@cam.ac.uk>
2015-01-19 16:03:24 +0100

    upload

---
+++ mod/rating/set/type/source_file/upload.rb
@@ -0,0 +1,16 @@
+include Card::Set::Type::File 
+
+format do 
+  include Card::Set::Type::File::Format
+end
+
+format :file do 
+  include Card::Set::Type::File::FileFormat
+end
+
+format :html do
+  include Card::Set::Type::File::HtmlFormat
+  view :new do |args|
+    super args.merge :success=>{:view=>:import_data}
+  end
+end
