f6978d70c57f52aead6348936496d1f7044530af
henrytai <henry.triniti@gmail.com>
2015-01-26 17:36:21 +0000

    top-bar

--- mod/wikirate/set/type/webpage.rb
+++ mod/wikirate/set/type/webpage.rb
@@ -78,6 +78,161 @@ end
 
 format :html do
 
+  view :preview ,:tags=>:unknown_ok do |args|
+    content = wrap args do
+      [
+
+        %{<div class="top-bar-icon"> #{web_link("/", :text=>raw( nest Card["*logo"], :view=>:content, :size=>:medium ))} </div>},
+        # render_source_preview_options(args),
+        render_company_and_topic_detail(args),
+        %{<div id="webpage-preview" class="webpage-preview"></div>}
+      ]
+    end
+    %{
+      <div id="logo-bar" class="top-bar nodblclick">
+        #{content}
+      </div>
+    }
+
+  end
+  def company_and_topic_match? company, topic , url
+    source = Self::Webpage.find_duplicates url
+    return false if ! source.any?
+    source_name = source.first.left.name 
+    company_pointer = Card[source_name].fetch :trait=>:wikirate_company
+    topic_pointer = Card[source_name].fetch :trait=>:wikirate_topic
+    if company_pointer and topic_pointer
+      if company_pointer.item_names.include?(company) and topic_pointer.item_names.include?(topic)
+        return true 
+      end
+    end
+    false
+  end
+  def first_or_add first_name, type_name, show_add_link
+    no_content_class = "no-content"
+    content = if first_name
+      no_content_class = ""
+      %{
+        <a href="#{first_name}" target="_blank">
+          <span class="#{type_name}-name">#{first_name}</span>
+        </a>
+      }  
+    else
+      if show_add_link
+        %{<a id='add-#{type_name}-link' href='#' >Add #{type_name.humanize}</a>}
+      else
+        no_content_class=""
+        ""
+      end
+    end
+    %{
+      <div class="#{type_name}-name #{no_content_class}">
+        #{content}
+      </div>
+    }
+  end
+
+  view :company_and_topic_detail ,:tags=>:unknown_ok  do |args|
+
+    company = Card::Env.params[:company]
+    topic = Card::Env.params[:topic]
+    url = Card::Env.params[:url]
+
+    from_certh = !card.real? 
+
+
+
+    if card.real?
+      company = card.fetch(:trait=>:wikirate_company).item_names.first
+      topic = card.fetch(:trait=>:wikirate_topic).item_names.first
+      url = card.fetch(:trait=>:wikirate_link).item_names.first
+    end
+    
+    dropdown_class = ""
+    
+    source_name = from_certh ? card.name : nil
+    #if company and topic match exisiting source, show it as a exisiting source
+    option_html = ""
+    if from_certh and not company_and_topic_match? company,topic, url
+      dropdown_class = "no-dropdown"
+      first_company = first_or_add company,"company",false
+      first_topic = first_or_add topic,"topic",false
+      option_html = show_options true, source_name ,url
+    else    
+      first_company = first_or_add company,"company",!from_certh
+      first_topic = first_or_add topic,"topic",!from_certh
+      option_html = show_options false, source_name ,url
+    end
+
+    %{
+      <div class="menu-options">
+        #{option_html}
+      </div>
+      <div id="company-and-topic" class="company-and-topic">
+        #{first_company}
+        #{first_topic}
+        <a href="#" id="company-and-topic-detail-link" class="#{dropdown_class}">
+          <i class="fa fa-caret-square-o-down"></i>
+        </a>
+      </div>
+    } 
+  end
+
+
+  def show_options source_from_certh,source_page_name,url
+    if source_from_certh
+      %{
+        <div id="mark-irrelevant" >
+          <a href="#" id="mark-irrelevant-button" class="button-primary button-secondary">
+            <i class="fa fa-exclamation-triangle">
+            </i>
+            <span>Irrelevant</span>
+          </a>
+        </div>
+        <div id="mark-relevant" >
+          <a href="#" id="mark-relevant-button" class="button-primary">
+            <i class="fa fa-exclamation-triangle">
+            </i>
+            <span>Relevant</span>
+          </a>
+        </div>
+      }
+    else
+      
+      related_claim_wql = {:left=>{:type=>"Claim"},:right=>"source",:link_to=>"#{source_page_name}",:return=>"count"}
+      claim_count = Card.search related_claim_wql
+
+      result = %{
+        <div id="source-page-link" class="mark-irrelevant-button" >
+          <a href="/#{source_page_name}" id="source-page-button" target="_blank">
+            Source Details
+            <i class="fa fa-chevron-circle-right"></i>
+          </a>
+          <a href="#{url}" id="direct-link-button" target="_blank">
+            Direct Link
+            <i class="fa fa-chevron-circle-right"></i>
+          </a>
+        </div>
+        <div id="make-claim" class="button-primary">
+          <a href="#" id="make-a-claim-button">
+            <span>Make a Claim</span>
+          </a>
+        </div>
+      }
+      result+=%{
+        <div id="claim-count">
+        #{
+        "<a class='show-link-in-popup' href='/#{source_page_name}+source claim list' target='_blank'>
+          <span class='claim-count'>
+            #{claim_count}
+          </span>
+          <span class='claim-count'>Claims</span>
+        </a>" if claim_count != 0}
+        </div>} 
+      result
+    end
+  end
+
   def edit_slot args
     # see claim.rb for explanation of core_edit
     super args.merge(:core_edit=>true)
