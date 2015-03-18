e6c216557992cabfabb4f9a063fb28baf6a0ed8a
xithan <philipp.kuehl@gmail.com>
2015-03-18 20:15:09 +0100

    updated import

--- db/migrate_cards/20150314161913_import_yin_yang.rb
+++
@@ -1,9 +0,0 @@
-# -*- encoding : utf-8 -*-
-
-class ImportYinYang < Card::Migration
-  def up
-    import_json "yinyang.json"
-    #Card.create! :name=>"Oxfam spreadsheet 2014", :codename=>'oxfam_spreadsheet', :type=>'file', :attach=>File.new(data_path('oxfam-scorecard-2014.xlsx')),
-    # :subcards=>{'+*self+*read'=> {:content=>"[[Administrator]]", :type=>'pointer'}}
-  end
-end
