600859d75722e5edd94fa801f9a48132eaa5d84c
xithan <philipp.kuehl@gmail.com>
2015-03-23 15:36:55 +0100

    another css fix

--- mod/rating/set/right/analysis_contributions.rb
+++ mod/rating/set/right/analysis_contributions.rb
@@ -1,6 +1,13 @@
 format :html do
-  def data_item content, label, extra_class=false
-    content_tag :div, :class=>"contribution btn btn-highlight #{extra_class if extra_class}" do
+  def data_item content, label, type=:default
+    extra_class = 
+      case type
+      when :highlight then 'btn btn-highlight'
+      when :warning   then 'warning'
+      else 
+        'btn btn-default'
+      end
+    content_tag :div, :class=>"contribution #{extra_class if extra_class}" do
       card_link "#{card.cardname.parts[1]}+#{card.cardname.parts[0]}", :text=>%{
           <div class="content">
             #{content}
@@ -18,13 +25,13 @@ format :html do
     empty = glyphicon 'plus'
     data = []
     if claim_cnt == '0' 
-      data << ['<i class="fa fa-exclamation-circle"></i>', 'Need Claims', 'warning']
-      data << [ empty, 'Claims', 'danger' ]
+      data << ['<i class="fa fa-exclamation-circle"></i>', 'Need Claims', :warning]
+      data << [ empty, 'Claims', :highlight ]
     else 
-      data << [(article_card ? nest(Card.fetch('venn icon'), :view=>:content,:size=>:icon) : empty), 'Article', ('danger' if !article_card)]
+      data << [(article_card ? nest(Card.fetch('venn icon'), :view=>:content,:size=>:icon) : empty), 'Article', (:highlight if !article_card)]
       data << [ claim_cnt, 'Claims']
     end    
-    data << [(source_cnt == '0' ? empty : source_cnt), 'Sources', ('danger' if source_cnt=='0')]
+    data << [(source_cnt == '0' ? empty : source_cnt), 'Sources', (:highlight if source_cnt=='0')]
       
     data.reverse.map {|opts| data_item(*opts)}.join "\n"  # reverse because of float:right
   end
