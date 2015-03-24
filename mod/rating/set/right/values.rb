a6cf56de3616946d852f5a88da833fe0c1625de3
xithan <philipp.kuehl@gmail.com>
2015-03-24 15:55:54 +0100

    fix content_tag content

--- mod/rating/set/right/values.rb
+++ mod/rating/set/right/values.rb
@@ -2,11 +2,12 @@ format :html do
   def timeline_row value_card, connect=false
     year        = value_card.cardname.right
     value       = value_card.content
-    source_link = card_link "#{card.cardname.left}+#{value_card.cardname.right}+Summary",:text=>"View Sources",:class=>'btn btn-yellow btn-xs view-source-btn'
+    source_link = card_link "#{card.cardname.left}+#{value_card.cardname.right}+Summary",
+                            :text=>"View Sources", :class=>'btn btn-yellow btn-xs view-source-btn'
     credit =  nest(value_card, :view=>:content, :structure=>'creator credit')
     credit << content_tag(:p, source_link)
-    line   =  content_tag(:div, :class=>'timeline-dot')
-    line   << content_tag(:div, :class=>'timeline-line') if connect
+    line   =  content_tag(:div, '', :class=>'timeline-dot')
+    line   << content_tag(:div, '', :class=>'timeline-line') if connect
 
     wrap_with :div, :class=>'timeline-row' do
       [
