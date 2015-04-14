93eaf01ad5d3d6ba1ce5015628d6e8e36454bba9
chuenlok <clt58@cam.ac.uk>
2015-04-14 18:32:15 +0100

    import export.js

--- db/migrate_cards/20150414171719_import_reorganise_source.rb
+++ db/migrate_cards/20150414171719_import_reorganise_source.rb
@@ -1,22 +1,17 @@
 # -*- encoding : utf-8 -*-
 
-class ImportSourceReorganization < Card::Migration
+class ImportReorganiseSource < Card::Migration
   def up
     # update the type and its codename
     source_card = Card["Source"]
     source_card.type_id = Card::CardtypeID
     source_card.codename = "source"
     source_card.save!
-  
-    # update webpage type to source
-    webpage_wql = { :type_id => Card::WebpageID }
-    webpages = Card.search webpage_wql
-    webpages.each do |webpage|
-      webpage.type_id = Card::SourceID
-      webpage.save!
-    end
+    Card.where( type_id: Card::WebpageID ).update_all( type_id: Card::SourceID )
     # default website for source+file
     Card.create :name=>"wikirate.org", :type_id=>Card::WikirateWebsiteID
+    
+    import_json "reorganise_source.json"
+    
   end
 end
-
