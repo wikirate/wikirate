11a5cee5cc899f172c5c56f9d8fc896f61cd214a
xithan <philipp.kuehl@gmail.com>
2015-04-15 23:28:00 +0200

    once again

--- db/migrate_cards/20150415000824_import_metric_migration.rb
+++ db/migrate_cards/20150415000824_import_metric_migration.rb
@@ -2,9 +2,6 @@
 
 class ImportMetricMigration < Card::Migration
   def up
-    Card.create! :name=>'Metric value', :codename=>'metric_value', :type_code=>:cardtype
-    Card::Cache.reset_global
     import_json "metric_migration.json"
-
   end
 end
