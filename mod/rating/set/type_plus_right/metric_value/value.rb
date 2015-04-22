e316b4aa9460a2b74146c82b52305ddaa44552d9
xithan <philipp.kuehl@gmail.com>
2015-04-22 16:04:28 +0200

    tweaks for "add new metric value" dialogs

--- mod/rating/set/type_plus_right/metric_value/value.rb
+++ mod/rating/set/type_plus_right/metric_value/value.rb
@@ -1,7 +1,7 @@
 format :html do
   view :timeline_row do |args|
     args.merge!(:hide=>'timeline_header source_link timeline_add_new_link')
-    wrap_with :div, :class=>'timeline' do
+    wrap_with :div, :class=>'timeline container' do
       wrap_with :div, :class=>'timeline-body' do
         [
           (wrap_with :div, :class=>'pull-left timeline-data' do
