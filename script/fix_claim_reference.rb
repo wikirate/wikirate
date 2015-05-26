92a155043070597f6f3de13a5841e2195e39d4a0
chuenlok <clt58@cam.ac.uk>
2015-05-26 16:25:59 +0100

    update the reference refresh script that applys to all + source

--- script/fix_claim_reference.rb
+++ script/fix_claim_reference.rb
@@ -2,15 +2,10 @@ require File.expand_path('../../config/environment',  __FILE__)
 
 
 Card::Auth.as_bot do
-
-  claim_cards = Card.search :type_id=>Card::ClaimID,:not=>{:right_plus=>[Card[:source].name,{:refer_to=>{:type_id=>Card::SourceID} }]}
-  claim_cards.each do |card|
-    claim_source = card.fetch :trait=>:source
-    if claim_source
-      puts claim_source.name
-      claim_source.update_references
-      claim_source.expire_structuree_references
-    end
+  plus_sources = Card.search :type_id=>Card::PointerID,:right=>Card[:source].name, :not=>{:refer_to=>{:type_id=>Card::SourceID}}
+  puts plus_sources.size
+  plus_sources.each do |plus_source_card|
+    puts plus_source_card.name
+    plus_source_card.update_references
   end
-
 end
