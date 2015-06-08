4d357bd227b3a8515fdbadd313a5b233a118c55e
xithan <philipp.kuehl@gmail.com>
2015-06-09 00:46:07 +0200

    profile page for companies

---
+++ mod/wikirate/set/right/contributed_sources.rb
@@ -0,0 +1,14 @@
+format :html do
+  include ContributedAnalysis::HtmlFormat
+
+  def default_header_args args
+    super(args)
+    args[:count] = Card.search :type_id=>SourceID, :or=>
+                        {:created_by=>card.left.name,
+                         :edited_by=>card.left.name,
+                         :linked_to_by=>{:left=>card.left.name, :right=>["in", "*upvotes", "*downvotes"]}
+                        },
+                        :return=>:count
+    args[:icon] = '<i class="fa fa-globe"></i>'
+  end
+end
