540504aeea75158339f7c66388a89dfcb99a1e50
Srivigneshwar <srivigneshwar@Vigneshs-MacBook-Pro.local>
2015-06-11 13:53:51 +0100

    Source preview

--- mod/wikirate_source/set/type/source/source_preview.rb
+++ mod/wikirate_source/set/type/source/source_preview.rb
@@ -5,7 +5,7 @@ format :html do
     topic = Card::Env.params[:topic]
     url = Card::Env.params[:url]
 
-    from_certh = !card.real? 
+    from_certh = !card.real?
 
     if card.real?
       company_card = card.fetch(:trait=>:wikirate_company)
@@ -26,14 +26,30 @@ format :html do
     wrap args do
     [
       render_hidden_information(args),
-      render_logo_bar(args),#render in structure source_preview_nav_bar_structure
+      render_navigation_bar(args),#render in structure source_preview_nav_bar_structure
       # render_content(args.merge({:structure=>"source_preview_nav_bar_structure"})),
-      render_iframe_view(args)
+      # render_iframe_view(args)
+      render_source_preview_container(args)
     ]
     end
   end
+
+  view :source_preview_container,:tags=>:unknown_ok  do |args|
+    %{
+      <div class="row clearfix source-preview-content">
+        <div class="col-md-8 column">
+          #{render_iframe_view(args)}
+        </div>
+        <div class="col-md-4 column">
+          #{render_tab_containers(args)}
+        </div>
+     </div>
+    }
+  end
+
+  #View: for hidden information
   view :hidden_information, :tags=>:unknown_ok do |args|
-    %{  
+    %{
       <div style="display:none">
         #{content_tag(:div, Auth.current_id, {:id=>"user-id"})}
         #{content_tag(:div, card.cardname.url_key, {:id=>"source-name"})}
@@ -43,11 +59,13 @@ format :html do
       </div>
     }
   end
-  view :logo_bar ,:tags=>:unknown_ok  do |args|
-    %{
-      <nav class="navbar navbar-default navbar-fixed-top ">
 
-        <div class="container-fluid">
+  #View: HTML for the navigation bar on preview page
+  view :navigation_bar ,:tags=>:unknown_ok  do |args|
+    %{
+      <nav class="navbar navbar-default  ">
+
+        <div class="">
           <!-- Brand and toggle get grouped for better mobile display -->
           <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
@@ -56,27 +74,22 @@ format :html do
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
             </button>
-            <div class="navbar-brand" href="#">
+            <div id="source-preview-tabs" class="navbar-brand" href="#">
               #{web_link("/", :text=>raw( nest Card["*logo"], :view=>:core, :size=>:original ))}
             </div>
           </div>
 
           <!-- Collect the nav links, forms, and other content for toggling -->
           <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <div class="navbar-left nav-center visible-lg visible-md ">
-              
-              #{render_company_and_topic_detail(args)}
-            </div>
-
-            <ul class="nav navbar-nav navbar-right">
+            <ul class="nav nav-tabs navbar-right">
                #{render_preview_options(args)}
             </ul>
-
           </div>
           <!-- /.navbar-collapse -->
         </div>
         <!-- /.container-fluid -->
       </nav>
+
     }
   end
 
@@ -94,13 +107,13 @@ format :html do
     end
 
     dropdown_class = ""
-    from_certh = !card.real? 
-    
+    from_certh = !card.real?
+
     dropdown_class = "no-dropdown"  if from_certh
-    
+
     first_company = first_or_add company,"company",!from_certh
     first_topic = first_or_add topic,"topic",!from_certh
-    
+
     %{
       <div id="company-and-topic" class="company-and-topic">
         #{first_company}
@@ -109,11 +122,11 @@ format :html do
           <i class="fa fa-caret-square-o-down"></i>
         </a>
       </div>
-    } 
+    }
   end
 
   view :preview_options, :tags=>:unknown_ok  do |args|
-    from_certh = !card.real?   
+    from_certh = !card.real?
     url = args[:url]
     if !url
        if card.real?
@@ -122,9 +135,7 @@ format :html do
       end
     end
     %{
-      <div class="menu-options">
         #{show_options from_certh, card.cardname.url_key ,url}
-      </div>
     }
   end
 
@@ -143,7 +154,7 @@ format :html do
   end
 
   def iframbale_file? mime
-    if mime == "application/pdf" or mime.start_with?("image/") 
+    if mime == "application/pdf" or mime.start_with?("image/")
       return true
     end
     return false
@@ -153,46 +164,47 @@ format :html do
     url = args[:url]
     file_card = Card[card.name+"+File"]
     text_card = Card[card.name+"+Text"]
-    if text_card 
-      ##{content_tag(:div, nest(text_card,:view=>"open",:hide=>"toggle"), {:id=>"text_source", :class=> "webpage-preview "},false) }  
+    if text_card
+      ##{content_tag(:div, nest(text_card,:view=>"open",:hide=>"toggle"), {:id=>"text_source", :class=> "webpage-preview "},false) }
       %{
         <div class="container-fluid">
           <div class="row-fluid">
-            #{content_tag(:div, subformat(text_card).render(:open,args.merge({:home_view=>"open",:hide=>"toggle",:title=>"Text Source"})), {:id=>"text_source", :class=> "webpage-preview "},false) }  
+            #{content_tag(:div, subformat(text_card).render(:open,args.merge({:home_view=>"open",:hide=>"toggle",:title=>"Text Source"})), {:id=>"text_source", :class=> "webpage-preview "},false) }
           </div>
         </div>
       }
       #col-md-12 column
-      
-    elsif file_card 
+
+    elsif file_card
       if mime_type = file_card.content.split("\n")[1] and ( mime_type == "application/pdf" or mime_type.start_with?("image/") )
         if mime_type == "application/pdf"
-          content_tag(:div, '<iframe id="source-preview-iframe" src="pdfjs/viewer.html?file='+ file_card.attach.url+'"  security="restricted" sandbox="allow-same-origin allow-scripts allow-forms" >', {:id=>"pdf-preview", :class=> "webpage-preview"},false)
+          content_tag(:div, '<iframe id="source-preview-iframe" src="pdfjs/viewer.html?file='+ file_card.attach.url+'"  security="restricted" sandbox="allow-same-origin allow-scripts allow-forms" ></iframe>', {:id=>"pdf-preview", :class=> "webpage-preview"},false)
         else
-          content_tag(:div, '<img id="source-preview-iframe" src="'+file_card.attach.url+'"  / >', {:id=>"pdf-preview", :class=> "webpage-preview"},false) 
+          content_tag(:div, '<img id="source-preview-iframe" src="'+file_card.attach.url+'"  / >', {:id=>"pdf-preview", :class=> "webpage-preview"},false)
         end
       else
         redirect_content = _render_content args.merge({:structure=>"source item preview"})
         content_tag(:div, content_tag(:div, redirect_content,{:class=> "redirect-notice"},false),  {:id=>"source-preview-iframe", :class=> "webpage-preview non-previewable"},false)
       end
     else
-      content_tag(:div, '<iframe id="source-preview-iframe" src="' + URI.escape(url) + '"  security="restricted" sandbox="allow-same-origin allow-scripts allow-forms" >', {:id=>"webpage-preview", :class=> "webpage-preview"},false)
+      content_tag(:div, '<iframe id="source-preview-iframe" src="' + URI.escape(url) + '"  security="restricted" sandbox="allow-same-origin allow-scripts allow-forms" ></iframe>', {:id=>"webpage-preview", :class=> "webpage-preview"},false)
     end
   end
 
   def company_and_topic_match? company, topic , url, source_name
     source = Self::Source.find_duplicates url
     return false if ! source.any?
-    source_name = source.first.left.name 
+    source_name = source.first.left.name
     company_pointer = Card[source_name].fetch :trait=>:wikirate_company
     topic_pointer = Card[source_name].fetch :trait=>:wikirate_topic
     if company_pointer and topic_pointer
       if company_pointer.item_names.include?(company) and topic_pointer.item_names.include?(topic)
-        return true 
+        return true
       end
     end
     false
   end
+
   def first_or_add first_name, type_name, show_add_link
     no_content_class = "no-content"
     content = if first_name
@@ -203,7 +215,7 @@ format :html do
         <a href="#{linkname}" target="_blank">
           <span class="#{type_name}-name">#{first_name}</span>
         </a>
-      }  
+      }
     else
       if show_add_link
         %{<a id='add-#{type_name}-link' href='#' >Add #{type_name.humanize}</a>}
@@ -238,45 +250,76 @@ format :html do
         </li>
       }
     else
-      
+
       related_claim_wql = {:left=>{:type=>"Claim"},:right=>"source",:link_to=>"#{source_page_name}",:return=>"count"}
+      related_metric_wql = {:left=>{:type=>"Metric"},:right=>"source",:link_to=>"#{source_page_name}",:return=>"count"}
       claim_count = Card.search related_claim_wql
+      metric_count = Card.search related_metric_wql
       file_card = Card[card.name+"+File"]
       text_card = Card[card.name+"+Text"]
-      result = %{
-        <li>
-          <a href="#" id="make-a-claim-button" class="btn btn-primary">
-            <span><i class="fa fa-quote-left"></i>Make a Claim</span>
-          </a>
-        </li>
-      }
-      result += %{
+      # result = %{
+      #   <li>
+      #     <a href="#" id="make-a-claim-button" class="btn btn-primary">
+      #       <span><i class="fa fa-quote-left"></i>Make a Claim</span>
+      #     </a>
+      #   </li>
+      # }
 
-        <li>
-          <a class='btn btn-default show-link-in-popup popup-original-link' href='/#{source_page_name}+source_claim_list?slot[hide]=header' target='_blank'>
-            <span class='claim-count'>
-              <i class='fa fa-quote-left'></i><span id="claim-count-number">#{claim_count}</span>
-            </span>
+      #Source Details tab
+      result = %{
+        <li role="presentation" class="active" >
+          <a class='' data-target="#tab_details" data-toggle="source_preview_tab_ajax">
+            <i class="fa fa-info-circle"></i> <span>Details</span>
           </a>
         </li>
-        <li>
-          <a class='btn btn-default show-link-in-popup popup-original-link no-header position-right' href='/#{source_page_name}+discussion?slot[hide]=header' target='_blank'>
-            <i class="fa fa-comments"></i>
-          </a>
-          </li>
-          <li>
-          <a class='btn btn-default show-link-in-popup popup-original-link no-header position-right' href='/#{source_page_name}?slot[structure]=source_structure&view=edit&slot[hide]=header' target='_blank'>
-            <i class="fa fa-info-circle"></i>
-          </a>
-          </li>
-          #{%{<li><a class='btn btn-default' href='#{url}' target='_blank'>
-            <i class="fa fa-external-link-square"></i>
-          </a></li>} if !( file_card || text_card )}
-        
       }
-      
+
+      #Claims tab
+      result += %{
+        <li role="presentation" >
+          <a class='' data-target="#tab_claims" data-toggle="source_preview_tab_ajax"  href='/#{source_page_name}+source_claim_list?slot[hide]=header' >
+              <i class='fa fa-quote-left'><span id="claim-count-number " class="count-number">#{claim_count}</span></i><span>Claims</span>
+          </a>
+        </li>
+      }
+
+      #Metrics tab
+      result += %{
+        <li role="presentation" >
+          <a class='' data-target="#tab_metrics" data-toggle="source_preview_tab_ajax" href='/#{source_page_name}+discussion?slot[hide]=header' >
+            <i class="fa fa-glass"><span id="claim-count-number " class="count-number">#{metric_count}</span></i><span>Metrics</span>
+          </a>
+        </li>
+      }
+
+      #External Link
+      result += %{
+          #{
+          %{
+            <li role="presentation" >
+              <a class='' href='#{url}' >
+                <i class="fa fa-external-link-square"></i> View Original
+              </a>
+            </li>
+          }if !( file_card || text_card )
+          }
+      }
+
       result
     end
   end
+  view :source_details, :tags=>:unknown_ok  do |args|
 
+  end
+  view :tab_containers, :tags=>:unknown_ok  do |args|
+    %{
+    <div class="tab-content">
+      <div class="tab-pane active" id="tab_details">#{card.format.render_core ({:structure=>"source_structure",:show=>"header"})}</div>
+      <div class="tab-pane" id="tab_claims">#{Card["loading gif"].format.render_core} </div>
+      <div class="tab-pane" id="tab_metrics">#{Card["loading gif"].format.render_core} </div>
+      <div class="tab-pane" id="tab_view_original"></div>
+    </div>
+    }
+
+  end
 end
