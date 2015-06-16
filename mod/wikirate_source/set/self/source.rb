9fe850934d36a90da6c6f655e231fd4dc6660f16
chuenlok <clt58@cam.ac.uk>
2015-06-16 14:15:03 +0100

    rspec

--- mod/wikirate_source/set/self/source.rb
+++ mod/wikirate_source/set/self/source.rb
@@ -66,57 +66,59 @@ format :json do
     end
     result
   end
-  view :feedback ,:perms=>lambda { |r| Auth.signed_in? } do |args|
-    url = Card::Env.params[:url]
-    company = Card::Env.params[:company]
-    topic = Card::Env.params[:topic]
+  
+#   uncomment until certh's suggestion is back
+#   view :feedback ,:perms=>lambda { |r| Auth.signed_in? } do |args|
+#     url = Card::Env.params[:url]
+#     company = Card::Env.params[:company]
+#     topic = Card::Env.params[:topic]
     
-    type = Card::Env.params[:type]
+#     type = Card::Env.params[:type]
 
     
-    result = {:result => false }
-    case type
-    when "either"
-      rel_topic_score = -1
-      rel_company_score = -1     
-    when "company"
-      rel_topic_score = 1
-      rel_company_score = -1
-    when "topic"
-      rel_topic_score = -1
-      rel_company_score = 1
-    when "relevant"
-      rel_topic_score = 1
-      rel_company_score = 1
-    else
-      return result
-    end
-    user_id = Auth.current_id
-    company_id, company_name = Card[company].id, Card[company].name if Card[company] and Card[company].type_id == Card::WikirateCompanyID
-    topic_id, topic_name = Card[topic].id, Card[topic].name if Card[topic] and Card[topic].type_id == Card::WikirateTopicID
+#     result = {:result => false }
+#     case type
+#     when "either"
+#       rel_topic_score = -1
+#       rel_company_score = -1     
+#     when "company"
+#       rel_topic_score = 1
+#       rel_company_score = -1
+#     when "topic"
+#       rel_topic_score = -1
+#       rel_company_score = 1
+#     when "relevant"
+#       rel_topic_score = 1
+#       rel_company_score = 1
+#     else
+#       return result
+#     end
+#     user_id = Auth.current_id
+#     company_id, company_name = Card[company].id, Card[company].name if Card[company] and Card[company].type_id == Card::WikirateCompanyID
+#     topic_id, topic_name = Card[topic].id, Card[topic].name if Card[topic] and Card[topic].type_id == Card::WikirateTopicID
     
-    if company_id and topic_id and url and type
-      query = { url: url, 
-                user_id: user_id,
-                rel_topic_score: rel_topic_score,
-                rel_company_score: rel_company_score, 
-                company_id: company_id, 
-                company: company_name, 
-                topic_id: topic_id, 
-                topic: topic_name}
-                .to_query
-      request_url = "http://mklab.iti.gr/wikirate-sandbox/api/index.php/relevance/?#{query}"
-      uri = URI.parse(request_url)
-      http = Net::HTTP.new(uri.host, uri.port)
-      request = Net::HTTP::Get.new(uri.request_uri)
-      response = http.request(request)
-      result_from_certh = JSON.parse(response.body)
-      #TODO: log them to a file
-      result = {:result => true, :result_from_certh => result_from_certh["results"]["code"],:msg=>result_from_certh["results"]["msg"]}
-    end
-    result
-  end
-end
+#     if company_id and topic_id and url and type
+#       query = { url: url, 
+#                 user_id: user_id,
+#                 rel_topic_score: rel_topic_score,
+#                 rel_company_score: rel_company_score, 
+#                 company_id: company_id, 
+#                 company: company_name, 
+#                 topic_id: topic_id, 
+#                 topic: topic_name}
+#                 .to_query
+#       request_url = "http://mklab.iti.gr/wikirate-sandbox/api/index.php/relevance/?#{query}"
+#       uri = URI.parse(request_url)
+#       http = Net::HTTP.new(uri.host, uri.port)
+#       request = Net::HTTP::Get.new(uri.request_uri)
+#       response = http.request(request)
+#       result_from_certh = JSON.parse(response.body)
+#       #TODO: log them to a file
+#       result = {:result => true, :result_from_certh => result_from_certh["results"]["code"],:msg=>result_from_certh["results"]["msg"]}
+#     end
+#     result
+#   end
+# end
 
 class MetaData  
   attr_accessor :title,:description,:image_url,:website,:error
