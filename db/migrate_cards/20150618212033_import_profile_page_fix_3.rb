d048fc610529722463e9bc5d8f1ea2c84528e0fe
Ethan McCutchen <ethan@grasscommons.org>
2015-06-18 15:42:27 -0600

    fix constant name problem

--- db/migrate_cards/20150618212033_import_profile_page_fix_3.rb
+++ db/migrate_cards/20150618212033_import_profile_page_fix_3.rb
@@ -5,7 +5,7 @@ class ImportProfilePageFix3 < Card::Migration
     import_json "profile_page_fix_3.json"
     metric_designer_with_wrong_cardtype = {"not"=>{"type"=>["in", "company", "user"]}, "right_plus"=>[{}, {"type"=>"metric"}]}
     Card.search(metric_designer_with_wrong_cardtype).each do |card|
-      card.update_attributes! :type_id=>WikirateCompanyID
+      card.update_attributes! :type_id=>Card::WikirateCompanyID
     end
   end
 end
