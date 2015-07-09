32dc5f3f6fc9fc76498387c28e3db2a0426e3087
xithan <philipp.kuehl@gmail.com>
2015-07-09 21:52:27 +0200

    correct migration name

--- db/migrate_cards/20150709112059_update_analyses_with_articles_count_2.rb
+++ db/migrate_cards/20150709112059_update_analyses_with_articles_count_2.rb
@@ -1,6 +1,6 @@
 # -*- encoding : utf-8 -*-
 
-class UpdateAnalysesWithArticlesCount < Card::Migration
+class UpdateAnalysesWithArticlesCount2 < Card::Migration
   def up
     Card.search(:type=>'company').each do |company|
       company.fetch(:trait=>:analyses_with_articles).update_contribution_count
