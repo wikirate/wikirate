3a83aef6381a0ce71671eaf22f398d34a94d6abe
xithan <philipp.kuehl@gmail.com>
2015-07-09 18:10:15 +0200

    uncomment contribution count update

--- db/migrate_cards/20150709112058_update_analyses_with_articles_count.rb
+++ db/migrate_cards/20150709112058_update_analyses_with_articles_count.rb
@@ -3,8 +3,8 @@
 class UpdateAnalysesWithArticlesCount < Card::Migration
   def up
     import_json "company_contribution_details.json"
-    # Card.search(:type=>'company').each do |company|
-    #   company.fetch(:trait=>:analyses_with_articles).update_contribution_count
-    # end
+    Card.search(:type=>'company').each do |company|
+      company.fetch(:trait=>:analyses_with_articles).update_contribution_count
+    end
   end
 end
