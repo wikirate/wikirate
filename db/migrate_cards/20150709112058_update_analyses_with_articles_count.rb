28c708fe506a23451620b5b029f5e27e1882d6a3
xithan <philipp.kuehl@gmail.com>
2015-07-09 13:58:58 +0200

    use cached contribution count in "company contribution details"

--- db/migrate_cards/20150709112058_update_analyses_with_articles_count.rb
+++ db/migrate_cards/20150709112058_update_analyses_with_articles_count.rb
@@ -2,6 +2,7 @@
 
 class UpdateAnalysesWithArticlesCount < Card::Migration
   def up
+    import_json "company_contribution_details.json"
     Card.search(:type=>'company').each do |company|
       company.fetch(:trait=>:analyses_with_articles).update_contribution_count
     end
