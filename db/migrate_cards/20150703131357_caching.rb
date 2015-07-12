32a40b74f95273acc36b69a281862e8702ce5978
xithan <philipp.kuehl@gmail.com>
2015-07-12 14:07:59 +0200

    update import

--- db/migrate_cards/20150703131357_caching.rb
+++ db/migrate_cards/20150703131357_caching.rb
@@ -4,7 +4,7 @@ class Caching < Card::Migration
   def up
     Card.fetch("Analyses with articles").update_attributes! :codename=>'analyses_with_articles'
     Card['yinyang drag item'].update_attributes! :codename=>'yinyang_drag_item'
-    import_json "company_contribution_details.json"
+    import_json "caching.json"
     Card.create! :name=>'*cached count', :codename=>'cached_count',
       :subcards=>{'+*right+*update'=>'[[Administrator]]', '+*right+*delete'=>'[[Administrator]]'}
 
