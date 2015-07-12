2f922b799bd427e7b87cca18a8d2331a4f6b64bb
xithan <philipp.kuehl@gmail.com>
2015-07-12 14:14:07 +0200

    :bug:

--- mod/caching/set/right/yinyang_drag_item.rb
+++ mod/caching/set/right/yinyang_drag_item.rb
@@ -2,7 +2,9 @@ format :html do
   view :content do |args|
     if (metric = card[1..2]) && metric.type_code == :metric
       key = "view_content_card_#{card.key}_args_#{Card::Cache.obj_to_key args}"
-      cached_items = Card::Cache[Card::Set::Right::YinyangDragItem].fetch metric.key, {}
+      cached_items = Card::Cache[Card::Set::Right::YinyangDragItem].fetch metric.key do
+        Hash.new
+      end
       if !cached_items[key]
         cached_items[key] = super(args)
         Card::Cache[Card::Set::Right::YinyangDragItem].write metric.key, cached_items
