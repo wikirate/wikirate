9ef0bb034510d66aab5f781cdd8fa6496e040ef9
chuenlok <clt58@cam.ac.uk>
2015-08-10 19:37:41 +0100

    update file.rb

--- mod/wikirate_source/set/type_plus_right/source/file/file.rb
+++ mod/wikirate_source/set/type_plus_right/source/file/file.rb
@@ -1,5 +1,3 @@
-event :block_file_changing, :after=>:write_identifier, :on=>:update do 
-  if real? && db_content_changed? && !db_content_was.empty?
-    errors.add :file, "is not allowed to be changed."
-  end
+event :block_file_changing, :after=>:write_identifier, :on=>:update , :when=>proc{  |c| c.real? && c.db_content_changed? && !c.db_content_was.empty? } do 
+  errors.add :file, "is not allowed to be changed."
 end
