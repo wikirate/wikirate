362393b9e706171d857b016913bc6cb14f1024ce
chuenlok <clt58@cam.ac.uk>
2015-08-10 19:28:12 +0100

    remove binding.pry from file.rb

--- mod/wikirate_source/set/type_plus_right/source/file/file.rb
+++ mod/wikirate_source/set/type_plus_right/source/file/file.rb
@@ -1,5 +1,4 @@
 event :block_file_changing, :after=>:write_identifier, :on=>:update do 
-  binding.pry
   if real? && db_content_changed? && !db_content_was.empty?
     errors.add :file, "is not allowed to be changed."
   end
