adc955f06b74c18cdb8913dbeb292206df1619f5
Ethan McCutchen <ethan@grasscommons.org>
2015-08-11 15:55:40 -0600

    changed content api

--- mod/wikirate_source/set/type_plus_right/source/file/file.rb
+++ mod/wikirate_source/set/type_plus_right/source/file/file.rb
@@ -1,3 +1,4 @@
-event :block_file_changing, :after=>:write_identifier, :on=>:update , :when=>proc{  |c| c.real? && c.db_content_changed? && !c.db_content_was.empty? } do 
+event :block_file_changing, :after=>:write_identifier, :on=>:update, :changed=>:content,
+   :when=>proc{ |c| !c.db_content_was.empty? } do
   errors.add :file, "is not allowed to be changed."
 end
