2b068746d3b4c2540d88f088fea1a68e0ae575ab
chuenlok <clt58@cam.ac.uk>
2015-08-17 19:11:46 +0100

    fix updating metric value

--- mod/01_rating/set/type/metric_value.rb
+++ mod/01_rating/set/type/metric_value.rb
@@ -20,6 +20,15 @@ def company_card
   Card.fetch company_name
 end
 
+def source_exist?
+  file_card = subcards["+File"]
+  text_card = subcards["+Text"]
+  link_card = subcards["+Link"]
+  ( file_card && file_card.stringify_keys.has_key?("file") ) || 
+  ( text_card && ( text_card_content = (text_card.stringify_keys)["content"] ) && !text_card_content.empty? ) ||
+  ( link_card && ( link_card_content = (link_card.stringify_keys)["content"] ) && !link_card_content.empty? )
+
+end
 
 event :set_metric_value_name, :before=>:set_autoname do
   self.name = ['+metric', '+company', '+year'].map do |name|
@@ -31,7 +40,7 @@ event :create_source_for_metric_value, :after=>:set_metric_value_name, :on=>:cre
   create_source
 end
 
-event :create_source_for_updating_metric_value, :before=>:process_subcards, :on=>:update do
+event :create_source_for_updating_metric_value, :before=>:process_subcards, :on=>:update, :when=>proc{  |c| c.source_exist? } do
   create_source
 end
 
