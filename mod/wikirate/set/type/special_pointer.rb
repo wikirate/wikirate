a6eb9e7b01b5d5f33ce8811b988b33a2246eab86
chuenlok <clt58@cam.ac.uk>
2015-08-26 18:50:54 +0100

    initial relaese of special pointer

--- mod/wikirate/set/type/special_pointer.rb
+++ mod/wikirate/set/type/special_pointer.rb
@@ -5,5 +5,46 @@ format do
 end
 
 format :html do
-   include Card::Set::Type::Pointer::HtmlFormat
+  include Card::Set::Type::Pointer::HtmlFormat 
 end
+
+format :json do
+  def get_list_of_children_and_self c
+    [
+      nest(c),
+      Card.search(:left=>c.name).map do |ec|
+        nest(ec)
+      end
+    ]
+  end
+  view :core do |args|
+    card.item_cards.map do |c|
+      case c.type_id
+      when Card::SearchTypeID
+        [
+          nest(c),
+          c.item_names.map do |cs|
+            if search_card_item = Card[cs]
+              get_list_of_children_and_self search_card_item
+            end
+          end
+        ]
+      when  Card::SpecialPointerID
+        [
+          nest(c),
+          c.format(:json).render_core(args)
+        ]
+      when  Card::PointerID
+        [
+          nest(c),
+          c.item_cards.map do |c|
+            get_list_of_children_and_self c
+         end
+        ]
+      else
+        get_list_of_children_and_self c   
+      end
+    end.flatten.reject { |c| c.empty? }
+  end
+end
+  
