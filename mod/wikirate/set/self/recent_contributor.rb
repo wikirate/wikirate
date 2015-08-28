687d6b8fea65dc34dbdba526ca5c962dd4ec011a
chuenlok <clt58@cam.ac.uk>
2015-08-28 11:20:32 +0100

    no hardcode on id

--- mod/wikirate/set/self/recent_contributor.rb
+++ mod/wikirate/set/self/recent_contributor.rb
@@ -1,7 +1,7 @@
 
 def item_names args={}
   limit = Env::params[:limit] || 5
-  Card::Act.select(:actor_id).distinct.where("card_id is not NULL and actor_id <> 1 and actor_id <> 15").order('id DESC').limit(limit).map do |act|
+  Card::Act.select(:actor_id).distinct.where("card_id is not NULL and actor_id <> #{Card["wagn bot"]} and actor_id <> #{Card["Anonymous"]}").order('id DESC').limit(limit).map do |act|
     Card[act.actor_id].id
   end
 end
