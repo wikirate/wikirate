d30f18c8d12992c8da7d29d73f30083ed66e13b4
chuenlok <clt58@cam.ac.uk>
2015-08-28 20:45:03 +0100

    add comment and clean up a little bit on the code

--- mod/wikirate/set/self/recent_contributor.rb
+++ mod/wikirate/set/self/recent_contributor.rb
@@ -1,8 +1,15 @@
 
+
+# this is pretending as a pointer card
+# to get the recent contributor, query the ids and names from the act table to get recent editors
 def item_names args={}
+  # may need different number of editors
   limit = Env::params[:limit] || 5
-  Card::Act.select(:actor_id).distinct.where("card_id is not NULL and actor_id <> #{Card["wagn bot"]} and actor_id <> #{Card["Anonymous"]}").order('id DESC').limit(limit).map do |act|
-    Card[act.actor_id].id
+  # this where clause to get recent editors(except wagn bot and annonymous) and the acted card should not be null
+  where_clause = "card_id is not NULL and actor_id <> #{Card::WagnBotID} and actor_id <> #{Card["Anonymous"].id}"
+
+  Card::Act.select(:actor_id).distinct.where(where_clause).order('id DESC').limit(limit).map do |act|
+    Card[act.actor_id].name
   end
 end
 
