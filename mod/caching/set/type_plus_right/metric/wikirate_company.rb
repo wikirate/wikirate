0b483e59a58bcbee7895e698931bf092862a3b22
chuenlok <clt58@cam.ac.uk>
2015-09-02 21:44:25 +0100

    revert and add comment

--- mod/caching/set/type_plus_right/metric/wikirate_company.rb
+++ mod/caching/set/type_plus_right/metric/wikirate_company.rb
@@ -6,7 +6,11 @@ end
 
 expired_cached_count_cards :set=>Type::MetricValue, :on=>[:create,:delete] do |changed_card|
   [
-    changed_card.metric_card.fetch(:trait=>:wikirate_company)
+    changed_card.metric_card.fetch(:trait=>:wikirate_company),
+    # actually this is not getting cached count for `metric+company` card
+    # it is to refresh the related metric value to the company
+    # refer to ltype_rtype/metric/wikirate_company.rb
+    Card.fetch("#{changed_card.metric_name}+#{changed_card.company_name}")
   ]
 end
 
