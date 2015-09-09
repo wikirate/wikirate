7354ae80a0f050f9bef9e1f60659e232af2d92f0
chuenlok <clt58@cam.ac.uk>
2015-09-09 21:36:54 +0100

    add some comment

--- mod/wikirate/set/right/image.rb
+++ mod/wikirate/set/right/image.rb
@@ -1,6 +1,8 @@
 format :html do
 
   view :missing do |args|
+    # try to get type+missing_image_card's image to display
+    # EX: user+missing_image_card
     parent_type_name = Card[card.left.type_id].name
     missing_image_card = Card["#{parent_type_name}+missing_image_card"]
     if missing_image_card
--- mod/wikirate/set/type_plus_right/user/wikirate_logo.rb
+++ mod/wikirate/set/type_plus_right/user/wikirate_logo.rb
@@ -1,6 +1,8 @@
 format :html do 
   view :missing do |args|
     username = card.cardname.left
+    # user's image is in +image but in metric page, we are using +logo
+    # a virtual card with structure won't work as it cannot have the args from +logo
     if user_image = Card["#{username}+image"]
       subformat(user_image).render args[:home_view],:size=>args[:size]
     else
