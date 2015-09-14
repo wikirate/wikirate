5cbe9be9cedb90944bbf25eee6e91647ead44f57
chuenlok <clt58@cam.ac.uk>
2015-09-14 15:34:04 +0100

    add codename

---
+++ db/migrate_cards/20150914142151_import_missing_image_codename.rb
@@ -0,0 +1,7 @@
+# -*- encoding : utf-8 -*-
+
+class ImportMissingImageCodename < Card::Migration
+  def up
+    Card.create! :name=>"missing_image_card" ,:codename=>"missing_image_card"
+  end
+end
--- mod/wikirate/set/right/image.rb
+++ mod/wikirate/set/right/image.rb
@@ -3,8 +3,8 @@ format :html do
   view :missing do |args|
     # try to get type+missing_image_card's image to display
     # EX: user+missing_image_card
-    parent_type_name = Card[card.left.type_id].name
-    missing_image_card = Card["#{parent_type_name}+missing_image_card"]
+    parent_type_card = Card[card.left.type_id]
+    missing_image_card = parent_type_card.fetch(:trait=>:missing_image_card)
     if missing_image_card
       core = subformat( missing_image_card )._render_core args
     else
