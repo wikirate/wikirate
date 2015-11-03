12a74cec0d24f25b7130be900056b00a702c5ca6
chuenlok <clt58@cam.ac.uk>
2015-11-03 15:33:06 +0000

    update some company's names, script style

--- script/metric/step_5_import_metric_value.rb
+++ script/metric/step_5_import_metric_value.rb
@@ -4,7 +4,8 @@ require 'colorize'
 
 if ARGV.length == 0
   puts "Please include a file path".red
-  puts "EX: ruby script/import_metric_value.rb script/metric/csv/metric_to_import.csv".green
+  puts "EX: ruby script/import_metric_value.rb "\
+       "script/metric/csv/metric_to_import.csv".green
   exit
 end
 
@@ -15,7 +16,8 @@ Card::Auth.as_bot do
   source_cache = Hash.new
   csv_file_path = ARGV[0]
   puts "CSV file to import : #{csv_file_path}"
-  CSV.foreach(csv_file_path,:encoding => 'windows-1251:utf-8',:headers => true, :header_converters => :symbol, :converters => :all) do |row|
+  CSV.foreach(csv_file_path, encoding: 'windows-1251:utf-8', headers: true,
+    header_converters: :symbol, converters: :all) do |row|
     
     metric_name = row[:metric]
     company_name = row[:company_name]
@@ -42,7 +44,10 @@ Card::Auth.as_bot do
 
       subcard = {
         "+metric"=>{"content"=>metric_name},
-        "+company"=>{"content"=>"[[#{company_name}]]",:type_id=>Card::PointerID},
+        "+company"=>{
+                      "content"=>"[[#{company_name}]]",
+                      :type_id=>Card::PointerID
+                    },
         "+value"=>{"content"=>value.to_s, :type_id=>Card::PhraseID},
         "+year"=>{"content"=>year.to_s, :type_id=>Card::PointerID},
         "+source"=>{
@@ -56,7 +61,6 @@ Card::Auth.as_bot do
           }
         }
       }
-      
       metric_value = Card.new :type_id=>Card::MetricValueID,:subcards=>subcard
       if metric_value.errors.empty?
         puts "metric value card to be created #{subcard}"
@@ -65,29 +69,37 @@ Card::Auth.as_bot do
         puts metric_value.errors
         next
       end
-      # add sources to +source pointer
-      metric_value_source_card = metric_value.fetch(:trait=>:source)
-      source_cache[source_url] = metric_value_source_card.item_cards[0]
-      sources_to_be_added_to_plus_source = Array.new
-      if sources != nil
-        sources.each do |source|
-          sourcepage = source_cache[source]
-          if sourcepage == nil
-            Card::Env.params[:sourcebox] = 'true'
-            args = { :type_id=>Card::SourceID,:subcards=>{ '+Link' => {:content=> source}} }
-            puts "Source Card created #{args}"
-            sourcepage = Card.create! args
-            source_cache[source] = sourcepage
+      begin
+        # add sources to +source pointer
+        metric_value_source_card = metric_value.fetch(:trait=>:source)
+        source_cache[source_url] = metric_value_source_card.item_cards[0]
+        sources_to_be_added_to_plus_source = Array.new
+        if sources != nil
+          sources.each do |source|
+            sourcepage = source_cache[source]
+            if sourcepage == nil
+              Card::Env.params[:sourcebox] = 'true'
+              args = {
+                       :type_id=>Card::SourceID,
+                       :subcards=>{
+                         '+Link' => {:content=> source}
+                       }
+                     }
+              puts "Source Card created #{args}"
+              sourcepage = Card.create! args
+              source_cache[source] = sourcepage
+            end
+            sources_to_be_added_to_plus_source.push(sourcepage.name)
           end
-          sources_to_be_added_to_plus_source.push(sourcepage.name)
+          sources_to_be_added_to_plus_source.each do |source|
+            metric_value_source_card<<source
+          end
+          metric_value_source_card.save!
         end
-        sources_to_be_added_to_plus_source.each do |source|
-          metric_value_source_card<<source
-        end
-        metric_value_source_card.save!
-        
+      rescue => e
+        puts "Error while handling the source card of #{metric_value.name}".red
+        puts "#{e}".red
       end
-
     else
       puts "duplicated snippets #{metric_name}+#{company_name}+#{year}"
     end
