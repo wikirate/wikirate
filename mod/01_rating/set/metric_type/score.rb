4561ff437782daba7f96e4c3f0a4c780708a2f43
xithan <philipp.kuehl@gmail.com>
2015-11-26 00:45:03 +0100

    add metric type sets

---
+++ mod/01_rating/set/metric_type/score.rb
@@ -0,0 +1,8 @@
+include Set::Abstract::Calculation
+
+def normalize_value value
+  return 0 if value < 0
+  return 10 if value > 10
+  value
+end
+
---
+++ mod/01_rating/set/metric_type/wiki_rating.rb
@@ -0,0 +1,5 @@
+include Set::Abstract::Calculation
+
+
+
+
