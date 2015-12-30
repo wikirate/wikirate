d1e478a71e5326c6ba780ba92afdeb0396b29e76
xithan <philipp.kuehl@gmail.com>
2015-12-30 14:59:30 +0100

    fix permission problem

--- mod/wikirate/set/type/phrase.rb
+++ mod/wikirate/set/type/phrase.rb
@@ -1,6 +1,8 @@
 event :delayed_job_test, after: :subsequent, on: :save do
-  test_card = Card['*delayed job test'] ||
-                Card.create!(name: '*delayed job test',
-                             type_id: Card::PointerID)
- test_card.add_item! "%s: %s" % [Time.zone.now, name]
+  Auth.as_bot do
+    test_card = Card['*delayed job test'] ||
+                  Card.create!(name: '*delayed job test',
+                               type_id: Card::PointerID)
+    test_card.add_item! "%s: %s" % [Time.zone.now, name]
+  end
 end
