6fc562a9c1dbb816c87be26a91ab675fb1a021fc
xithan <philipp.kuehl@gmail.com>
2015-12-31 16:49:38 +0100

    error in wikirate head

--- mod/wikirate/set/self/head/wikirate_head.rb
+++ mod/wikirate/set/self/head/wikirate_head.rb
@@ -1,6 +1,6 @@
 format :html do
   def google_analytics_head_javascript
-    if ga_key = Card.setting("*google analytics key") #fixme.  escape this?
+    if ga_key = Card.setting('*google analytics key') #fixme.  escape this?
       %{
         <script type="text/javascript">
           var _gaq = _gaq || [];
@@ -8,7 +8,9 @@ format :html do
           _gaq.push(['_setPageGroup', '1', '#{root.card.type_name}']);
           _gaq.push(['_trackPageview']);
           (function() {
-            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            var ga = document.createElement('script');
+            ga.type = 'text/javascript';
+            ga.async = true;
             ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
             var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
           })();
@@ -21,12 +23,12 @@ format :html do
     result = super(args)
     if request
       user_agent = request.user_agent
-      if user_agent && ( user_agent=="Facebot" || user_agent.include?("facebookexternalhit/1.1"))
-        result += subformat(Card.fetch("#{Env.params["id"]}+facebook_meta"))._render_core
+      if user_agent && (user_agent == 'Facebot' ||
+         user_agent.include?('facebookexternalhit/1.1'))
+        facebook_meta = Card.fetch("#{Env.params['id']}+facebook_meta")
+        result += subformat(facebook_meta)._render_core
       end
     end
     result
   end
 end
-   
-      
