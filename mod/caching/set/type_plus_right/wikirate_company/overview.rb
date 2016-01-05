cfe0eca3f0f79ad752d73980b34072d3d2776f41
xithan <philipp.kuehl@gmail.com>
2016-01-05 17:02:15 +0100

    more WikirateArticle renaming

--- mod/caching/set/type_plus_right/wikirate_company/overview.rb
+++ mod/caching/set/type_plus_right/wikirate_company/overview.rb
@@ -3,8 +3,8 @@ include Card::CachedCount
 
 # recount overviews associated with a company
 # whenever article gets created or deleted
-ensure_set { TypePlusRight::WikirateAnalysis::WikirateArticle }
-recount_trigger(TypePlusRight::WikirateAnalysis::WikirateArticle,
+ensure_set { TypePlusRight::WikirateAnalysis::Overview }
+recount_trigger(TypePlusRight::WikirateAnalysis::Overview,
                 on: [:create, :delete]) do |changed_card|
   if (company_name = changed_card.cardname.left_name.left)
     Card.fetch company_name.to_name.trait(:analyses_with_articles)
--- mod/wikirate/set/right/showcase.rb
+++ mod/wikirate/set/right/showcase.rb
@@ -8,7 +8,7 @@ format :html do
 
 
   # view :core do |args|
-  # [CampaignID, MetricID, ClaimID, SourceID, WikirateArticleID].map do |type_id|
+  # [CampaignID, MetricID, ClaimID, SourceID, OverviewID].map do |type_id|
   #     if (search_card = card.fetch(:trait=>type_id, :new=>{:type_id=>PointerID}))
   #       nest(search_card, :slot_class=>('hidden' if search_card.item_cards.empty?), :view=>:yinyang_list)
   #     end
