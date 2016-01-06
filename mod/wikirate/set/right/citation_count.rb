49699f680d08b8fc0c65f7cd9f5a1ae2c6b54d60
xithan <philipp.kuehl@gmail.com>
2016-01-06 23:47:31 +0100

    add company=<company> to link in citation box

--- mod/wikirate/set/right/citation_count.rb
+++ mod/wikirate/set/right/citation_count.rb
@@ -1,14 +1,37 @@
 format :html do
   view :titled do |args|
-    args.merge!(:slot_class=>( "no-citations" if card.format.render_core=="0" ))
+    args.merge! slot_class: ('no-citations' if no_citations?(args))
     super(args)
   end
 
   view :related_overview_modal_box do |args|
+    content_tag :div do
+      subformat(related_overview_card)
+        ._render_modal_link(args[:link_args])
+    end
+  end
+
+  def default_related_overview_modal_box_args args
+    link_text = subformat(card).render_titled(
+             args.merge(title: "Citations", hide: 'menu')
+           )
+    link_args = args[:link_args] =
+      args.merge(
+        text: link_text,
+        html_args: { class: "#{'no-citations' if no_citations?(args)}" }
+      )
+    return unless parent && (pp = parent.parent) && (ppl = pp.card.left) &&
+                  ppl.type_id == WikirateCompanyID
+    link_args[:path_opts] ||= {}
+    link_args[:path_opts][:company] = ppl.key
+  end
+
+  def no_citations? args
     value = _render_core args
-    related_overview_card = Card.fetch card.cardname.left+"+related overview"
-    text = subformat(card).render_titled args.merge({:title=>"Citations",:hide=>"menu"})
-    related_overview_link = subformat(related_overview_card)._render_modal_link(args.merge(:text=>text,:html_args=>{:class=>"#{"no-citations" if value==""}"}))
-    content_tag(:div, related_overview_link)
+    value == '0' || value == ''
+  end
+
+  def related_overview_card
+    Card.fetch "#{card.cardname.left}+#{Card[:related_articles]}"
   end
 end
