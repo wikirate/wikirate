da4c00d0ea702610de9886ef15a61a865f3f9f89
chuenlok <clt58@cam.ac.uk>
2016-01-08 13:08:33 -0800

    disable sending email

--- script/create_gibs_users.rb
+++ script/create_gibs_users.rb
@@ -4,7 +4,7 @@ CSV.foreach('script/gibs_users.csv', encoding: 'windows-1251:utf-8',
                                      headers: true,
                                      header_converters: :symbol) do |row|
   unless Card.exists? row[:name]
-    Wagn.config.action_mailer.perform_deliveries = false
+    Card::Mailer.perform_deliveries = false
     args = {
       name: row[:name],
       type_id: Card::UserID,
@@ -18,8 +18,8 @@ CSV.foreach('script/gibs_users.csv', encoding: 'windows-1251:utf-8',
     puts "activating #{row[:name]}"
     status_card = Card["#{row[:name]}+*account+*status"]
     status_card.update_attributes! content: 'active', silent_change: true
-    
-    Wagn.config.action_mailer.perform_deliveries = true
+
+    Card::Mailer.perform_deliveries = true
     user.account.send_welcome_email
   end
 end
