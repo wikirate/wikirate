527c3482cdfa98b540281b5d818ce1a1383d78e4
chuenlok <clt58@cam.ac.uk>
2016-01-08 19:30:20 -0800

    remove welcome email

--- script/create_gibs_users.rb
+++ script/create_gibs_users.rb
@@ -1,10 +1,10 @@
 require File.expand_path('../../config/environment', __FILE__)
 Card::Auth.as_bot
+Card::Mailer.perform_deliveries = false
 CSV.foreach('script/gibs_users.csv', encoding: 'windows-1251:utf-8',
                                      headers: true,
                                      header_converters: :symbol) do |row|
   unless Card.exists? row[:name]
-    Card::Mailer.perform_deliveries = false
     args = {
       name: row[:name],
       type_id: Card::UserID,
@@ -14,12 +14,10 @@ CSV.foreach('script/gibs_users.csv', encoding: 'windows-1251:utf-8',
       }
     }
     puts "account to be created: #{args}"
-    user = Card.create! args
+    Card.create! args
     puts "activating #{row[:name]}"
     status_card = Card["#{row[:name]}+*account+*status"]
     status_card.update_attributes! content: 'active', silent_change: true
-
-    Card::Mailer.perform_deliveries = true
-    user.account.send_welcome_email
   end
 end
+Card::Mailer.perform_deliveries = true
