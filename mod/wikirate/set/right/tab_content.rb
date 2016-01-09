0734b1d4e0e2a54120f0ec4e68cef7bb9a6edb9b
chuenlok <clt58@cam.ac.uk>
2016-01-08 21:47:53 -0800

    migration and tab content

---
+++ mod/wikirate/set/right/tab_content.rb
@@ -0,0 +1,19 @@
+format :html do
+  view :core do |_args|
+    # show the content based on the url parameter
+    # tabs: metric, topic, company, note, reference, overview
+    tab = Env.params['tab']
+    left_name = card.cardname.left
+    card_tab_name =
+      if !tab.nil?
+        "#{left_name}+#{tab}_page"
+      else
+        "#{left_name}+metric_page"
+      end
+    if (content_card = Card.fetch card_tab_name)
+      subformat(content_card).render_content
+    else
+      ''
+    end
+  end
+end
