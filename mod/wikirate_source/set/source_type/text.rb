66fa9d5beb317199ddc3c8a1580241dd5974c4c9
chuenlok <clt58@cam.ac.uk>
2016-01-16 18:06:49 +0000

    commit before checking import bug

---
+++ mod/wikirate_source/set/source_type/text.rb
@@ -0,0 +1,12 @@
+card_accessor :text, type: :basic
+
+format :html do
+  view :original_link do |args|
+    card_link card.text_card, text: (args[:title] || 'Visit Text Source')
+  end
+
+  def icon
+    'pencil'
+  end
+  
+end
