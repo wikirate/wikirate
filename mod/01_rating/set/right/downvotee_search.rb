94be461212e44a2501cd81cc7729c7e134586227
Ethan McCutchen <ethan@grasscommons.org>
2016-02-01 20:35:32 -0700

    new company+metric structure

--- mod/01_rating/set/right/downvotee_search.rb
+++ mod/01_rating/set/right/downvotee_search.rb
@@ -34,33 +34,30 @@ format do
 
   alias :super_search_results :search_results
 
-  def search_results args={}
-     @search_results ||= enrich_result(get_search_result)
+  def search_results _args={}
+    @search_results ||= enrich_result(get_search_result)
   end
 
   def get_search_result
     if !Auth.signed_in?
       get_result_from_session
-    else
-      if vote_order
-        super_search_results.sort do |x,y|  # super returns array with votee cards
-          vote_order[x] <=> vote_order[y]
-        end
-      else
-        super_search_results
+    elsif vote_order
+      super_search_results.sort do |x, y| # super returns array with votee cards
+        vote_order[x] <=> vote_order[y]
       end
+    else
+      super_search_results
     end
   end
 
   def vote_order
     @vote_order ||=
-      if card.sort_by && (vote_card = Auth.current.fetch(:trait=>card.sort_by))
+      if card.sort_by && (vote_card = Auth.current.fetch trait: card.sort_by)
         votee_items = vote_card.item_names
         super_search_results.each_with_object({}) do |name, hash|
           hash[name] = votee_items.index "~#{Card.fetch_id(name)}"
         end
       end
-
   end
 
   def get_result_from_session
@@ -70,8 +67,9 @@ format do
   def list_with_session_votes
     if Env.session[card.vote_type]
       Env.session[card.vote_type].map do |votee_id|
-        found_votee_card = Card.find_by_id_and_type_id(votee_id, searched_type_id)
-        found_votee_card ? found_votee_card.name : ""
+        found_votee_card =
+          Card.find_by_id_and_type_id(votee_id, searched_type_id)
+        found_votee_card ? found_votee_card.name : ''
       end.compact.reject(&:empty?)
     else
       []
@@ -101,6 +99,7 @@ format :html do
       end.compact.join("\n").html_safe
     end.html_safe
   end
+
   # it is for type_search
   view :filter_and_sort do |args|
     res = with_filter_and_sort(args) do
