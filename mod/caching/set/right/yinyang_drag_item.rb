e9a86212b6f770de3781bd73dfd5374114276e05
Ethan McCutchen <ethan@grasscommons.org>
2016-03-01 22:38:58 -0700

    fixed cache bug

--- mod/caching/set/right/yinyang_drag_item.rb
+++ mod/caching/set/right/yinyang_drag_item.rb
@@ -1,16 +1,16 @@
 
-def cache
-  Card::Cache[Card::Set::Right::YinyangDragItem]
-end
-
 format :html do
+  def yycache
+    Card::Cache[Card::Set::Right::YinyangDragItem]
+  end
+
   view :content do |args|
     if (metric = card[1..2]) && metric.type_code == :metric
       key = "view_content_card_#{card.key}_args_#{Card::Cache.obj_to_key args}"
-      cached_items = cache.read(metric.key) || {}
-      if !cached_items[key]
+      cached_items = yycache.read(metric.key) || {}
+      unless cached_items[key]
         cached_items[key] = super(args)
-        cache.write metric.key, cached_items
+        yycache.write metric.key, cached_items
       end
       cached_items[key]
     else
@@ -18,6 +18,3 @@ format :html do
     end
   end
 end
-
-
-
