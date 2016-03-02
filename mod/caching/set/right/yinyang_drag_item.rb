503c19479e43aa4c569e4957fd175073e5a57482
Ethan McCutchen <ethan@grasscommons.org>
2016-03-01 22:06:15 -0700

    wagn tick

--- mod/caching/set/right/yinyang_drag_item.rb
+++ mod/caching/set/right/yinyang_drag_item.rb
@@ -1,13 +1,16 @@
+
+def cache
+  Card::Cache[Card::Set::Right::YinyangDragItem]
+end
+
 format :html do
   view :content do |args|
     if (metric = card[1..2]) && metric.type_code == :metric
       key = "view_content_card_#{card.key}_args_#{Card::Cache.obj_to_key args}"
-      cached_items = Card::Cache[Card::Set::Right::YinyangDragItem].fetch metric.key do
-        Hash.new
-      end
+      cached_items = cache.read(metric.key) || {}
       if !cached_items[key]
         cached_items[key] = super(args)
-        Card::Cache[Card::Set::Right::YinyangDragItem].write metric.key, cached_items
+        cache.write metric.key, cached_items
       end
       cached_items[key]
     else
