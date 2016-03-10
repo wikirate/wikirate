b7f854871e49bbf8fb93ae58cebf1acaa76f6c03
chuenlok <clt58@cam.ac.uk>
2016-03-10 23:34:59 +0000

    fix style

--- mod/wikirate/spec/set/type_plus_right/metric/value_type_spec.rb
+++ mod/wikirate/spec/set/type_plus_right/metric/value_type_spec.rb
@@ -24,7 +24,7 @@ shared_examples_for 'changing type to numeric' do |original_type, new_type|
       @metric_value = Card.create! type_id: Card::MetricValueID,
                                    subcards: subcards
       @metric.update_attributes! subcards: {
-                                   '+value_type' => "[[#{new_type}]]" }
+        '+value_type' => "[[#{new_type}]]" }
       value_type_card = @metric.fetch trait: :value_type
       expect(value_type_card.item_names[0]).to eq(new_type)
     end
@@ -64,7 +64,7 @@ describe Card::Set::TypePlusRight::Metric::ValueType do
           options_card.content = '[[65535]]'
           options_card.save!
           @metric.update_attributes! subcards: {
-                                      '+value_type' => '[[Category]]' }
+            '+value_type' => '[[Category]]' }
           value_type_card = @metric.fetch trait: :value_type
           expect(value_type_card.item_names[0]).to eq('Category')
         end
