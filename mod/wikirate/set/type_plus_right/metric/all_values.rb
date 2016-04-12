c0aad34d86960ddc9da50fdd05a39cc320a77fcf
xithan <philipp.kuehl@gmail.com>
2016-04-12 16:53:46 +0200

    fix script for value type modal

--- mod/wikirate/set/type_plus_right/metric/all_values.rb
+++ mod/wikirate/set/type_plus_right/metric/all_values.rb
@@ -185,7 +185,7 @@ format :html do
 
   view :card_list_item do |args|
     c = args[:item_card]
-    item_view = nest_defaults(c)[:view]
+    item_view = args[:item] || nest_defaults(c)[:view]
     %(
       <div class="search-result-item item-#{item_view}">
         #{nest(c, size: args[:size], view: item_view)}
