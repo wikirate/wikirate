20effca9a2af452e0a287635b30fe04908901e84
xithan <philipp.kuehl@gmail.com>
2016-04-28 15:47:24 +0100

    remove codenames for missing cards

--- db/migrate_cards/20160422220605_missing_codenames.rb
+++ db/migrate_cards/20160422220605_missing_codenames.rb
@@ -2,9 +2,10 @@
 
 class MissingCodenames < Card::Migration
   def up
-    %w(unit value_options currency category number money
+    %w(unit value_options currency number
        numeric_details monetary_details category_details
        aliases).each do |codename|
+      puts "update #{codename}"
       update_card codename, codename: codename
     end
   end
