2b62a07dd3c0dfd1ba65f4e690d32d71d0048127
Ethan McCutchen <ethan@grasscommons.org>
2016-05-16 12:29:09 -0600

    fixed bad ID ref

--- db/migrate_cards/20160514205559_add_cso_metrics.rb
+++ db/migrate_cards/20160514205559_add_cso_metrics.rb
@@ -59,7 +59,7 @@ class AddCsoMetrics < Card::Migration
   CSO = 'Center for Sustainable Organizations'.freeze
 
   def up
-    Card.create! name: CSO, type_id: WikirateCompanyID
+    Card.create! name: CSO, type_id: Card::WikirateCompanyID
     create_yearly_variables
     create_researched_metrics
     create_formula_metrics
