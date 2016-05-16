af18421787f253f558e42e2081a635c0a444406c
Ethan McCutchen <ethan@grasscommons.org>
2016-05-16 11:46:16 -0600

    update referers on industry metric rename

--- db/migrate_cards/20160516011433_update_industry_metric_name.rb
+++ db/migrate_cards/20160516011433_update_industry_metric_name.rb
@@ -4,6 +4,7 @@ class UpdateIndustryMetricName < Card::Migration
   def up
     industry_metric = Card['Richard Mills+Sector Industry']
     industry_metric.name = 'Global Reporting Initiative+Sector Industry'
+    industry_metric.update_referers = true
     industry_metric.save!
   end
 end
