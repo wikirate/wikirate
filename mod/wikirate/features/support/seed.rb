e941b8dca7e4b3da7f3000b1918ab3d9649db98b
xithan <philipp.kuehl@gmail.com>
2016-08-25 12:46:02 +0200

    seed without machine output

--- mod/wikirate/features/support/seed.rb
+++ mod/wikirate/features/support/seed.rb
@@ -3,8 +3,8 @@ class Card
     system "env RAILS_ENV=test bundle exec rake wikirate:test:seed"
   end
 end
-
-Before("@background-jobs, @delayed-jobs, @javascript") do
-  Card[:all, :script].update_machine_output
-  Card[:all, :style].update_machine_output
-end
+#
+# Before("@background-jobs, @delayed-jobs, @javascript") do
+#   Card[:all, :script].update_machine_output
+#   Card[:all, :style].update_machine_output
+# end
