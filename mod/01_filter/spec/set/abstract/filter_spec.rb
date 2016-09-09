044fa874bf6b63cd0ea3a53f86ae7fb92f948801
chuenlok <clt58@cam.ac.uk>
2016-09-09 19:38:46 +0100

    filter tests, checkedby fix and tests

--- mod/01_filter/spec/set/abstract/filter_spec.rb
+++ mod/01_filter/spec/set/abstract/filter_spec.rb
@@ -66,7 +66,6 @@ describe Card::Set::Abstract::Filter do
     end
     it "renders view industry_formgroup" do
       html = card.format.render_industry_formgroup
-
       industries = Card[card.industry_metric_name].value_options
       expect(html).to have_tag(:div,
                                with: { class: "form-group filter-input" }) do
@@ -79,44 +78,108 @@ describe Card::Set::Abstract::Filter do
         end
       end
     end
-    it "renders view importance_formgroup" do
-      html = card.format.render_importance_formgroup
-    end
     it "renders view designer_formgroup" do
+      Card.create! name: "Jedi+disturbances in the Force+Joe Admin",
+                   type_id: Card::MetricID
       html = card.format.render_designer_formgroup
+      # ensure score metric is return the third part as designer name
+      expect(html).to have_tag(:option, with: { value: "Joe Admin" },
+                                        text: "Joe Admin")
     end
-    it "renders view metric_value_formgroup" do
-      html = card.format.render_metric_value_formgroup
+    describe "view research_policy_formgroup" do
+      context "select form" do
+        it "renders select list" do
+          html = card.format.render_research_policy_formgroup select_list: true
+          expect(html).to have_tag(:select, with: { name: "research_policy" })
+        end
+      end
+      context "checkbox form" do
+        it "renders checkboxes" do
+          Card.create name: "sidan", type_id: Card::ResearchPolicyID
+          html = card.format.render_research_policy_formgroup
+          expect(html).to have_tag(:input, with: { name: "research_policy[]" })
+        end
+      end
     end
-    it "renders view research_policy_formgroup" do
-      html = card.format.render_research_policy_formgroup
-    end
-    it "renders view metric_type_formgroup" do
-      html = card.format.render_metric_type_formgroup
+    describe "view metric_type_formgroup" do
+      context "select form" do
+        it "renders select list" do
+          html = card.format.render_metric_type_formgroup select_list: true
+          expect(html).to have_tag(:select, with: { name: "metric_type" })
+        end
+      end
+      context "checkbox form" do
+        it "renders checkboxes" do
+          html = card.format.render_metric_type_formgroup
+          expect(html).to have_tag(:input, with: { name: "type[]" })
+        end
+      end
     end
     it "renders view name_formgroup" do
-      html = card.format.render_name_formgroup
+      # predefined name
+      html = card.format.render_name_formgroup name: "SiDan"
+      expect(html).to have_tag(:input, with: { name: "SiDan" })
+      # pre-defined title
+      html = card.format.render_name_formgroup title: "SiDan"
+      expect(html).to have_tag(:label, text: "SiDan")
     end
-    describe "#multiselect_filter" do
-
+    describe "#checkbox_formgroup" do
+      it "renders checkboxes" do
+        options = %w(Fancy A hTc Phone?)
+        html = card.format.checkbox_formgroup "test", options, "htc"
+        expect(html).to have_tag(:div, with: { class: "form-group" }) do
+          with_tag :label, text: "test"
+          with_tag :div, with: { class: "editor" } do
+            options.each do |opt|
+              with_tag :input, with: { type: "checkbox", name: "test[]",
+                                       value: opt.downcase }
+            end
+            with_tag :input, with: { type: "checkbox", name: "test[]",
+                                     value: "htc", checked: "checked" }
+          end
+        end
+      end
     end
     describe "#simple_multiselect_filter" do
-
+      it "renders multi select list" do
+        options = card.format.type_options :wikirate_topic
+        html = card.format.simple_multiselect_filter "Topic", options,
+                                                     nil, "Sidan"
+        expect(html).to have_tag(:div, with: { class: "form-group Topic" }) do
+          with_tag :label, text: "Sidan"
+          with_tag :select, with: { name: "Topic[]", multiple: "multiple" }
+        end
+      end
+    end
+    describe "#multiselect_filter" do
+      it "renders multi select list" do
+        html = card.format.multiselect_filter :wikirate_topic, "Whatever"
+        expect(html).to have_tag(:div, with: { class: "wikirate_topic" }) do
+          with_tag :label, text: "Whatever"
+          with_tag :select, with: { name: "wikirate_topic[]",
+                                    multiple: "multiple" }
+        end
+      end
     end
     describe "#select_filter_html" do
-
-    end
-    describe "#simple_select_filter" do
-
+      it "renders single select list" do
+        options = card.format.type_options :wikirate_topic
+        html = card.format.select_filter_html "Topic", options, nil, "Sidan"
+        expect(html).to have_tag(:div, with: { class: "form-group" }) do
+          with_tag :label, text: "Sidan"
+          with_tag :select, with: { name: "Topic" }
+        end
+      end
     end
     describe "#select_filter" do
-
-    end
-    describe "#type_options" do
-
-    end
-    describe "#text_filter" do
-
+      it "renders single select list" do
+        options = card.format.select_filter :wikirate_topic, "asc"
+        html = card.format.select_filter_html "Topic", options, nil, "Sidan"
+        expect(html).to have_tag(:div, with: { class: "form-group" }) do
+          with_tag :label, text: "Sidan"
+          with_tag :select, with: { name: "Topic" }
+        end
+      end
     end
   end
 end
