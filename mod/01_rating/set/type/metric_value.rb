0fce88be35ab4961c634150d54ca36b7baa9a36b
xithan <philipp.kuehl@gmail.com>
2016-09-21 09:48:00 +0200

    remove rename event duplicate

--- mod/01_rating/set/type/metric_value.rb
+++ mod/01_rating/set/type/metric_value.rb
@@ -15,19 +15,6 @@ event :set_metric_value_name,
   end.join "+"
 end
 
-event :rename_metric_value,
-      before: :set_autoname, when: proc { |c| c.cardname.parts.size < 4 } do
-  return if valid_value_name?
-  self.name = %w(metric company year).map do |part|
-    name_part = remove_subfield(part)
-    unless name_part
-      errors.add :name, "missing #{part} part"
-      next
-    end
-    name_part.content.gsub("[[", "").gsub("]]", "")
-  end.join "+"
-end
-
 def valid_value_name?
   cardname.parts.size >= 3 &&
     metric_card && metric_card.type_id == MetricID &&
