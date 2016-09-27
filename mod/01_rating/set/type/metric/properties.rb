c868c6fcb1d512075b24ffdea8e196e23bfc54e5
Srivigneshwar <prasadsrivi@gmail.com>
2016-09-27 16:29:11 +0100

    :dog2:

--- mod/01_rating/set/type/metric/properties.rb
+++ mod/01_rating/set/type/metric/properties.rb
@@ -1,9 +1,10 @@
 format :html do
   view :metric_properties do |args|
-    props = Hash.new
+    props = {}
     props["Designed By"] = _render_designer_info.html_safe
     props["Scored By"] = _render_scorer_info.html_safe if card.scored?
-    props["Metric Type"] = field_subformat(:metric_type)._render_content item: :name
+    props["Metric Type"] =
+      field_subformat(:metric_type)._render_content item: :name
     if card.researched?
     # value type refer: http://dev.wikirate.org/Good_Company_Index+Good_Company_Grade+numeric_detail
       props["Value Type"] = content_tag(:div, _render_value_type_detail(args))
@@ -13,12 +14,13 @@ format :html do
       props["Report Type"] = nest(card.report_type_card,
                                   view: :content,
                                   items: { view: :name })
-      props["Projects"] = nest(card.project_card,
-                               view: :content,
-                               items: { view: "content", structure: "list item" })
+      props["Projects"] =
+        nest(card.project_card, view: :content,
+                                items: {  view: "content",
+                                          structure: "list item" })
     end
-    props["Topics"] = field_subformat(:wikirate_topic)._render_content item: :link
+    props["Topics"] =
+      field_subformat(:wikirate_topic)._render_content item: :link
     table props, class: "metric-properties"
   end
-
 end
