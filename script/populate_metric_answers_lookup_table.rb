535954936161c869859a2b6e646ca9520d4cafac
xithan <philipp.kuehl@gmail.com>
2016-11-16 10:04:01 +0100

    remove breakpoint

--- script/populate_metric_answers_lookup_table.rb
+++ script/populate_metric_answers_lookup_table.rb
@@ -2,13 +2,11 @@
 
 require File.expand_path("../../config/environment",  __FILE__)
 
-require 'pry'
 Card.search(type_id: Card::MetricID, return: :id).each do |metric_id|
 
   Card.search(type_id: Card::MetricValueID,
               left: { left_id: metric_id }).each do |card|
-
-    binding.pry if card.new_card?
+    # binding.pry if card.new_card?
     MetricAnswer.create_or_update card
   end
 end
