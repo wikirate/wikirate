98fdc514b05e082f523312a202b224fb2accf0dd
xithan <philipp.kuehl@gmail.com>
2016-11-30 21:23:09 +0100

    add visualization scripts

--- db/migrate_cards/20161124101939_script_vega.rb
+++ db/migrate_cards/20161124101939_script_vega.rb
@@ -2,7 +2,13 @@
 
 class ScriptVega < Card::Migration
   def up
+    add_script 'd3',
+               type_id: Card::JavaScriptID,
+               to: 'script: wikirate scripts'
     add_script "vega", type_id: Card::JavaScriptID,
                to: "script: wikirate scripts"
+    add_script 'metric_chart',
+               type_id: Card::CoffeeScriptID,
+               to: 'script: wikirate scripts'
   end
 end
