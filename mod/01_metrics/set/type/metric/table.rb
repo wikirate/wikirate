450ce90cdd3210f41891b32552c8ae0c47b3828b
xithan <philipp.kuehl@gmail.com>
2016-12-13 23:44:09 +0100

    sort links

--- mod/01_metrics/set/type/metric/table.rb
+++ mod/01_metrics/set/type/metric/table.rb
@@ -6,8 +6,20 @@ format :html do
     items = card.fetch(trait: :all_metric_values).item_cards
     wikirate_table_with_details :company, items,
                                 [:company_thumbnail, :value_cell],
-                                header: ["Company", "Value"],
+                                header: [company_sort_link, value_sort_link],
                                 details_view: :company_details_sidebar
 
+  end
+
+  def company_sort_link
+    sort_link "Companies #{sort_icon :name}",
+              sort_by: 'name', sort_order: toggle_sort_order(:name),
+              class: 'header'
+  end
+
+  def value_sort_link
+    sort_link "Values #{sort_icon :value}",
+              sort_by: 'value', sort_order: toggle_sort_order(:value),
+              class: 'data'
   end
 end
