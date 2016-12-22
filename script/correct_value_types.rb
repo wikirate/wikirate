8d9803d2c4f27cc897e430bf5f8890344dd3de50
xithan <philipp.kuehl@gmail.com>
2016-12-22 19:13:51 +0100

    finish correct value type script

--- script/correct_value_types.rb
+++ script/correct_value_types.rb
@@ -4,6 +4,8 @@ require File.expand_path("../../config/environment", __FILE__)
 
 ZEROS = { K: 3, M: 6, B: 9, T: 12, P: 15 }
 
+UPDATER_ID = Card["Philipp Kuehl"].id
+
 def change_value_type wrong_name, opts={}
   correct_name = opts[:to]
   value_type_cards(wrong_name).each do |card|
@@ -14,6 +16,7 @@ def change_value_type wrong_name, opts={}
       Card::Cache.reset_all
     end
     yield metric_card if block_given?
+
     card.update_attributes! content: "[[#{correct_name}]]"
   end
 end
@@ -30,7 +33,7 @@ def sanitize_number_values metric_card
     old_content = vc.content
     new_content = correct_zeros old_content.tr(",", "").tr("$", "")
     puts "  changing #{old_content} to #{new_content}"
-    vc.update_attributes! content: new_content
+    vc.update_column :db_content, new_content
   end
 end
 
@@ -55,16 +58,15 @@ def change_value_type_to_category wrong_name
       return change_value_type wrong_name, to: "Free Text"
     end
     puts "  with options #{options.join ','}"
+
     metric_card.value_options_card
       .update_attributes! content: options.to_pointer_content
     Card::Cache.reset_all
   end
 end
 
-# FIXME bot should not do these changes
-Card::Auth.as_bot do
-  change_value_type_to_category "String"
-  change_value_type_to_category "Boolean"
-  change_value_type "Real", to: "Number"
-  change_value_type "Monetary", to: "Money"
-end
+Card::Auth.current_id = UPDATER_ID
+change_value_type_to_category "String"
+change_value_type_to_category "Boolean"
+change_value_type "Real", to: "Number"
+change_value_type "Monetary", to: "Money"
