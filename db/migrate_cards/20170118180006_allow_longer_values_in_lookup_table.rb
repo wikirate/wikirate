5ed628b843edd5c06eb04c50a4eca0b74cd67100
xithan <philipp.kuehl@gmail.com>
2017-01-19 22:30:47 +0100

    add index for numeric value

--- db/migrate_cards/20170118180006_allow_longer_values_in_lookup_table.rb
+++ db/migrate_cards/20170118180006_allow_longer_values_in_lookup_table.rb
@@ -4,6 +4,7 @@ class AllowLongerValuesInLookupTable < ActiveRecord::Migration
   def up
     remove_index :answers, column: :value
     add_index :answers, :value, name: "value_index", length: 100
+    add_index :answers, :numeric_value, name: "numeric_value_index"
     change_column :answers, :value, :string, limit: 1024
   end
 end
