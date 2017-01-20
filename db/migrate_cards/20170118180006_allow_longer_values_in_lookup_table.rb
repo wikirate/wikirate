45639cba713dbef954508a63a3ffd8b469f7341f
xithan <philipp.kuehl@gmail.com>
2017-01-20 12:10:04 +0100

    correct remove_index command

--- db/migrate_cards/20170118180006_allow_longer_values_in_lookup_table.rb
+++ db/migrate_cards/20170118180006_allow_longer_values_in_lookup_table.rb
@@ -2,7 +2,7 @@
 
 class AllowLongerValuesInLookupTable < ActiveRecord::Migration
   def up
-    remove_index :answers, column: :value
+    remove_index :answers, name: "value_index"
     add_index :answers, :value, name: "value_index", length: 100
     add_index :answers, :numeric_value, name: "numeric_value_index"
     change_column :answers, :value, :string, limit: 1024
