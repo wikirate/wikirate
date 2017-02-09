13055597c3d2c472e0119e6be7340bda64f19190
xithan <philipp.kuehl@gmail.com>
2017-02-09 20:04:35 +0100

    multi-category metrics

--- mod/metrics/set/type/metric.rb
+++ mod/metrics/set/type/metric.rb
@@ -62,6 +62,10 @@ def value_type
   value_type_card.item_names.first || "Free Text"
 end
 
+def value_type_code
+  ((vc = value_type_card.item_cards.first) && vc.codename.to_sym) || :free_text
+end
+
 def value_options
   value_options_card.item_names
 end
@@ -74,6 +78,10 @@ end
 # TODO: adapt to Henry's value type API
 def categorical?
   value_type == "Category"
+end
+
+def multi_categorical?
+  value_type_code == :multi_category
 end
 
 def researched?
