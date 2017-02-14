9112b560b3fe1e5ee9f3067eccf9e38d9258e642
Ethan McCutchen <ethan@grasscommons.org>
2017-02-14 16:02:09 -0500

    relationship metric migration and import

--- db/migrate_cards/20170214192232_relationship_metrics.rb
+++ db/migrate_cards/20170214192232_relationship_metrics.rb
@@ -2,9 +2,15 @@
 
 class RelationshipMetrics < Card::Migration
   def up
-    Card.create! name: "Relationship", type: "Metric Type"
-    Card.create! name: "Inverse Relationship", type: "Metric Type"
-    Card.create! name: "Relationship Answer", type: "Cardtype"
+    Card.create! name: "Relationship",
+                 type: "Metric Type",
+                 codename: :relationship
+    Card.create! name: "Inverse Relationship",
+                 type: "Metric Type",
+                 codename: :inverse_relationship
+    Card.create! name: "Relationship Answer",
+                 type: "Cardtype",
+                 codename: :relationship_answer
     Card.create! name: "Metric+inverse+*default", type: "Pointer"
     Card.create! name: "Metric Title+inverse+*default", type: "Pointer"
   end
