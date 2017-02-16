4d77a7a6f79f7aff0f41d9a95b555cda80f3e4e7
xithan <philipp.kuehl@gmail.com>
2017-02-16 16:58:49 +0100

    load environment in script
    (cherry picked from commit 90a7117)

--- script/single-use/import_sample_relationships.rb
+++ script/single-use/import_sample_relationships.rb
@@ -1,8 +1,10 @@
+require_relative "../../config/environment"
+
 require_relative "relationship_import/relationship_answers_csv"
 require_relative "relationship_import/relationship_metrics_csv"
 
-metrics_path = File.expand_path "../relationship_import/data/metrics.csv"
-answers_path = File.expand_path "../relationship_import/data/answers.csv"
+metrics_path = File.expand_path "../relationship_import/data/metrics.csv", __FILE__
+answers_path = File.expand_path "../relationship_import/data/answers.csv", __FILE__
 
 RelationshipMetricsCSV.new(metrics_path).import!
 RelationshipAnswersCSV.new(answers_path).import!
