04077e63a24013271b536f3ccd6808c49b3a77f8
xithan <philipp.kuehl@gmail.com>
2017-02-16 21:31:23 +0100

    add smartname as submodule
    (cherry picked from commit 1bdb0f7bc1c44cdc2a336f2c5c05d41eebd362e9)

--- mod/metrics/set/metric_type/relationship.rb
+++ mod/metrics/set/metric_type/relationship.rb
@@ -1,15 +1,7 @@
-include_set Abstract::Researched
-
 def inverse_card
   fetch(trait: :inverse).item_cards.first
 end
 
 def inverse
   fetch(trait: :inverse).item_names.first
-end
-
-format :html do
-  view :legend do
-    "companies"
-  end
 end
