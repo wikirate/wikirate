ca208206a13b4c47151933b080cf706e8c099f3a
Srivigneshwar <prasadsrivi@gmail.com>
2017-02-21 17:41:43 +0000

    feed hound

--- mod/wikirate/set/ltype_rtype/user/cardtype.rb
+++ mod/wikirate/set/ltype_rtype/user/cardtype.rb
@@ -67,7 +67,9 @@ format :html do
   def contribution_report_action_boxes
     wrap_with :ul, class: "nav nav-tabs" do
       [:created, :updated, :discussed, :voted_on].reverse.map do |report_action|
-        contribution_report_box report_action  if card.report_action_applies? report_action
+        if card.report_action_applies? report_action
+          contribution_report_box report_action
+        end
       end.unshift contribution_report_toggle
     end
   end
