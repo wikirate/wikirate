f21db53aa740a5c86c0c9aecc0a2c65230bc11f2
xithan <philipp.kuehl@gmail.com>
2017-02-21 11:20:00 +0100

    remove homepage rollback

--- db/migrate_cards/20170217100865_rollback_homepage.rb
+++
@@ -1,9 +0,0 @@
-# -*- encoding : utf-8 -*-
-
-class RollbackHomepage < Card::Migration
-  def up
-    merge_cards ["home", "homepage_company_item", "homepage_metric_item",
-                 "homepage_topic_item", "nav_bar_menu",
-                 "homepage_wikirate_get_it_right", "homepage_top_banner"]
-  end
-end
