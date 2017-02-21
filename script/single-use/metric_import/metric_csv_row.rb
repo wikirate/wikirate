20265ff0cd9394cb154ed27123c9cd6623467712
xithan <philipp.kuehl@gmail.com>
2017-02-22 00:19:52 +0100

    handle currency in metric imports and remove .idea config

--- script/single-use/metric_import/metric_csv_row.rb
+++ script/single-use/metric_import/metric_csv_row.rb
@@ -7,13 +7,12 @@ class MetricCSVRow < CSVRow
                  value_type: :process_value_type }
 
   def initialize row
+    @value_details = {}
     super
     @designer = @row.delete :metric_designer
     @title = @row.delete :metric_title
     @name = "#{@designer}+#{@title}"
     @row[:wikirate_topic] = @row.delete :topics if @row[:topics]
-    @row[:value_options] = @value_options if @value_options
-    @row[:unit] = @unit if @unit
   end
 
   def comma_list_to_pointer str
@@ -23,10 +22,13 @@ class MetricCSVRow < CSVRow
   def process_value_type value
     value.match /(?<type>[^(]+)\((?<options>[^)]+)/ do |match|
       new_value = match[:type].strip
+      new_value == "Category" if new_value == "Categorical"
       if new_value == "Category"
-        @value_options = comma_list_to_pointer match[:options]
+        @value_details[:value_options] = comma_list_to_pointer match[:options]
+      elsif new_value == "Money"
+        @value_details[:currency] = match[:options].strip
       else
-        @unit = match[:options].strip
+        @value_details[:unit] = match[:options].strip
       end
       new_value
     end
@@ -35,6 +37,6 @@ class MetricCSVRow < CSVRow
   def create
     create_card @designer, type: Card::ResearchGroupID unless Card.exists?(@designer)
     create_card @title, type: Card::MetricTitleID unless Card.exists?(@title)
-    create_card @name, type: Card::MetricID, subfields: @row
+    create_card @name, type: Card::MetricID, subfields: @row.merge(@value_details)
   end
 end
