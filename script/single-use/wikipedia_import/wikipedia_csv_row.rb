3939cb7d26799a973e5277f997071b64d5758084
xithan <philipp.kuehl@gmail.com>
2017-04-12 21:06:18 +0200

    fix sort order issue

--- script/single-use/wikipedia_import/wikipedia_csv_row.rb
+++ script/single-use/wikipedia_import/wikipedia_csv_row.rb
@@ -4,10 +4,16 @@ class WikipediaCSVRow < CSVRow
   @required = [:wikirate_company_name, :wikirate_id,
                :wikipedia_company_page]
 
+  def initialize row
+    super
+    @row[:wikipedia_company_page].match(%r{/([^/]+)$})
+    @wikipedia_name = Regexp.last_match(1)
+  end
+
   def create
     puts @row[:wikirate_company_name]
     ensure_card [@row[:wikirate_id].to_i, :wikipedia],
-                content: @row[:wikipedia_company_page],
+                content: @wikipedia_name,
                 type_id: Card::PhraseID
   end
 end
