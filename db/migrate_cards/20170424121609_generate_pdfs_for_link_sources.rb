097e5d6c73506fad73468f0d8b8cc4faf38c7867
xithan <philipp.kuehl@gmail.com>
2017-04-25 20:57:25 +0200

    turn transactions off

--- db/migrate_cards/20170424121609_generate_pdfs_for_link_sources.rb
+++ db/migrate_cards/20170424121609_generate_pdfs_for_link_sources.rb
@@ -1,10 +1,12 @@
 # -*- encoding : utf-8 -*-
 
 class GeneratePdfsForLinkSources < Card::Migration
+  disable_ddl_transaction!
+
   def up
-    Card.search type_id: Card::SourceID,
+    Card.search(type_id: Card::SourceID,
                 right_plus: [Card::SourceTypeID, refer_to: "Link"],
-                not: { right_plus: [Card::FileID, {}] }.each do |card|
+                not: { right_plus: [Card::FileID, {}] }).each do |card|
       card.generate_pdf
     end
   end
--- db/migrate_cards/20170425105950_update_wikipedia_mapping.rb
+++ db/migrate_cards/20170425105950_update_wikipedia_mapping.rb
@@ -4,7 +4,7 @@ require_relative "../../script/csv_import/wikipedia/wikipedia_csv_row"
 require_relative "../../script/csv_import/csv_file"
 
 class UpdateWikipediaMapping < Card::Migration
-  #disable_ddl_transaction!
+  disable_ddl_transaction!
 
   def up
     csv_path = data_path "wikirate_to_wikipedia.csv"
