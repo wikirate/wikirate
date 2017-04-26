effff3546e45f74806a9ff15dfb466777c3e19f7
xithan <philipp.kuehl@gmail.com>
2017-04-26 09:08:23 +0200

    deploy wikipedia mappings

--- db/migrate_cards/20170425105951_update_wikipedia_mapping.rb
+++ db/migrate_cards/20170425105951_update_wikipedia_mapping.rb
@@ -7,8 +7,8 @@ class UpdateWikipediaMapping < Card::Migration
   disable_ddl_transaction!
 
   def up
-    # csv_path = data_path "wikirate_to_wikipedia.csv"
-    # Card::Auth.current_id = Card.fetch_id "Vasiliki Gkatziaki"
-    # CSVFile.new(csv_path, WikipediaCSVRow).import! error_policy: :report
+    csv_path = data_path "wikirate_to_wikipedia.csv"
+    Card::Auth.current_id = Card.fetch_id "Vasiliki Gkatziaki"
+    CSVFile.new(csv_path, WikipediaCSVRow).import! error_policy: :report
   end
 end
