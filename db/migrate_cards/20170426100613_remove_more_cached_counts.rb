b7af040d14b50a54bb4ce2fada78734afe507790
xithan <philipp.kuehl@gmail.com>
2017-04-26 19:10:15 +0200

    remove more cache cards

---
+++ db/migrate_cards/20170426100613_remove_more_cached_counts.rb
@@ -0,0 +1,20 @@
+# -*- encoding : utf-8 -*-
+
+class RemoveMoreCachedCounts < Card::Migration
+  disable_ddl_transaction!
+
+  def up
+    [{ right: { codename: "contribution_count" } },
+     { right: { codename: "direct_contribution_count" } }].each do |wql|
+      Card.search(wql).each do |card|
+        card.delete
+      end
+    end
+    Card.search(left: { right: { type_id: Card::WikirateCompanyID } },
+                right: { codename: "cached_count" }).each do |card|
+      card.delete
+    end
+    Card::Cache.reset_all
+    Card.empty_trash
+  end
+end
