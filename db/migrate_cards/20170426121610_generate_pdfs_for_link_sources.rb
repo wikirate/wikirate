deb50aa1b115f287cf2ad46952e62eee0c006d49
xithan <philipp.kuehl@gmail.com>
2017-04-27 21:15:35 +0200

    fix migration

--- db/migrate_cards/20170426121610_generate_pdfs_for_link_sources.rb
+++ db/migrate_cards/20170426121610_generate_pdfs_for_link_sources.rb
@@ -8,6 +8,7 @@ class GeneratePdfsForLinkSources < Card::Migration
                 right_plus: [Card::SourceTypeID, refer_to: "Link"],
                 not: { right_plus: [Card::FileID, {}] }).each do |card|
       if card.file_link?
+        file_url = Addressable::URI.escape card.url
         ensure_card [card, :file], remote_file_url: file_url, type_id: FileID
         ensure_card [card, :source_type], content: "[[#{Card[:file].name}]]",
                                           type_id: Card::PointerID
