a0a3fb6c5a0193ec4f058f4f08052dedc9f7b00d
xithan <philipp.kuehl@gmail.com>
2017-04-27 23:00:53 +0200

    optimize migration

--- db/migrate_cards/20170401182745_remove_cached_count_cards.rb
+++ db/migrate_cards/20170401182745_remove_cached_count_cards.rb
@@ -22,13 +22,18 @@ class RemoveCachedCountCards < Card::Migration
     remove type: :wikirate_topic, plus_right: :wikirate_company
     remove type: :wikirate_analysis, plus_right: :source
     remove type: :wikirate_analysis, plus_right: :claim
+    remove type: :wikirate_analysis, plus_right: :metric
     remove_ltype_rtype :metric, :wikirate_company
     remove_contribution_counts
+    Card::Cache.reset_all
+    Card.empty_trash
+    remove_empty_analyses
     Card::Cache.reset_all
     Card.empty_trash
   end
 
   def remove type:, plus_right:
+    puts "remove #{type}+#{plus_right}+type_plus_right"
     remove_cached_counts left: { type_id: Card::Codename[type] },
                          right_id: Card::Codename[plus_right]
     card = Card[type, plus_right, :type_plus_right, :structure]
@@ -36,26 +41,40 @@ class RemoveCachedCountCards < Card::Migration
   end
 
   def remove_ltype_rtype ltype, rtype
-    Card.search(left: { left: { type_id: Card::Codename[ltype] },
-                        right: { type_id: Card::Codename[rtype] } },
-                right: { codename: "cached_count" }).each do |card|
-         card.delete!
-    end
+    trash_all left: { left: { type_id: Card::Codename[ltype] },
+                          right: { type_id: Card::Codename[rtype] } },
+              right: { codename: "cached_count" }
   end
 
   def remove_cached_counts wql
-    Card.search(wql).each do |card|
-      card.update_cached_count
-      card.delete!
+    count_ids = Card.search(left: wql, right: { codename: "cached_count" }, return: :id )
+    search_ids = count_ids.map do |id|
+      search_id = Card.quick_fetch(id).left_id
+      if (search_card = Card[search_id])
+        search_card.update_cached_count
+      end
+      search_id
     end
+    trash_ids count_ids
+    trash_ids search_ids
   end
 
   def remove_contribution_counts
-    [{ right: { codename: "contribution_count" } },
-     { right: { codename: "direct_contribution_count" } }].each do |wql|
-      Card.search(wql).each do |card|
-        card.delete
-      end
-    end
+    trash_all right: { codename: "contribution_count" }
+    trash_all right: { codename: "direct_contribution_count" }
+  end
+
+  def remove_empty_analyses
+    # find all analyses without fields (assuming there is no "+wikirate bot" field)
+    trash_all type: { codename: "wikirate_analysis" },
+              not: { right_plus: [{ not: { id: 1 } }, {}]}
+  end
+
+  def trash_all wql
+    trash_ids Card.search wql.merge(return: :id)
+  end
+
+  def trash_ids ids
+    Card.where(id: ids).update_all trash: true
   end
 end
