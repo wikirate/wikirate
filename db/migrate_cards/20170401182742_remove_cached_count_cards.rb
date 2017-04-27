357eb7f3798bef918c69f0abca7e07fd5dd35d11
xithan <philipp.kuehl@gmail.com>
2017-04-27 18:41:58 +0200

    refactor cached count removal

--- db/migrate_cards/20170401182742_remove_cached_count_cards.rb
+++ db/migrate_cards/20170401182742_remove_cached_count_cards.rb
@@ -19,18 +19,40 @@ class RemoveCachedCountCards < Card::Migration
     remove type: :wikirate_topic, plus_right: :metric
     remove type: :wikirate_topic, plus_right: :project
     remove type: :wikirate_topic, plus_right: :wikirate_company
+    remove_ltype_rtype :metric, :wikirate_company
+    remove_contribution_counts
+    Card::Cache.reset_all
+    Card.empty_trash
   end
 
-  def remove type: , plus_right:
+  def remove type:, plus_right:
     remove_cached_counts left: { type_id: Card::Codename[type] },
                          right_id: Card::Codename[plus_right]
-    Card[type, plus_right, :type_plus_right, :structure].delete
+    card = Card[type, plus_right, :type_plus_right, :structure]
+    card.delete! if card
+  end
+
+  def remove_ltype_rtype ltype, rtype
+    Card.search(left: { left: { type_id: Card::Codename[ltype] },
+                        right: { type_id: Card::Codename[rtype] } },
+                right: { codename: "cached_count" }).each do |card|
+         card.delete!
+    end
   end
 
   def remove_cached_counts wql
     Card.search(wql).each do |card|
       card.update_cached_count
-      card.delete
+      card.delete!
+    end
+  end
+
+  def remove_contribution_counts
+    [{ right: { codename: "contribution_count" } },
+     { right: { codename: "direct_contribution_count" } }].each do |wql|
+      Card.search(wql).each do |card|
+        card.delete
+      end
     end
   end
 end
--- mod/caching/set/type_plus_right/wikirate_topic/metric.rb
+++ mod/caching/set/type_plus_right/wikirate_topic/metric.rb
@@ -1,7 +1,6 @@
 # cache # of metrics tagged with this topic (=_left) via <metric>+topic
 include_set Abstract::TaggedByCachedCount, type_to_count: :metric,
                                            tag_pointer: :wikirate_topic
-
 include_set Abstract::Table
 
 def metric_ids
