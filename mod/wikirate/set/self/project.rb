6e0461bba14af994fdeef5131e8cf523564b0ed9
xithan <philipp.kuehl@gmail.com>
2017-05-17 00:00:26 +0100

    project csv

--- mod/wikirate/set/self/project.rb
+++ mod/wikirate/set/self/project.rb
@@ -1,12 +1,16 @@
 format :csv do
   def title_row
-    CSV.generate_line ["PROJECT", "METRICS", "COMPANIES", "USERS", "ANSWERS", "DESIGNER/COMMUNITY-ASSESSED"]
+    CSV.generate_line ["PROJECT", "METRICS", "COMPANIES", "ANSWERS", "USERS", "DESIGNER/COMMUNITY-ASSESSED"]
   end
 
   view :core do
+    total = [0, 0, 0]
     Card.search(type_id: card.id).map do |pc|
-      CSV.generate_line([pc.name, pc.num_metrics, pc.num_companies,
-                        pc.num_users, pc.num_answers, pc.num_policies])
-    end.unshift(title_row).join
+      cnts = [pc.num_metrics, pc.num_companies, pc.num_users, pc.num_answers]
+      3.times do |i|
+        total[i] += cnts[i]
+      end
+      CSV.generate_line([pc.name] + cnts + [pc.num_policies])
+    end.unshift(title_row).push(CSV.generate_line(["TOTAL"] + total)).join
   end
 end
