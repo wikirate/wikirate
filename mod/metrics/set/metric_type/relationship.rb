6edc49f7bcea2ea528c9afcd7db9b3d7d3b2c769
xithan <philipp.kuehl@gmail.com>
2017-05-29 11:12:20 +0200

    fix merge error

--- mod/metrics/set/metric_type/relationship.rb
+++ mod/metrics/set/metric_type/relationship.rb
@@ -1,15 +1,1 @@
-include_set Abstract::Researched
-
-def inverse_card
-  fetch(trait: :inverse).item_cards.first
-end
-
-def inverse
-  fetch(trait: :inverse).item_names.first
-end
-
-format :html do
-  view :legend do
-    "companies"
-  end
-end
+include_set Abstract::Relationship
