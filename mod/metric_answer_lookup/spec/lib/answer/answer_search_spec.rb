3b6e2bfa5d9bd64cf28bdebcc837dc87f83e844d
xithan <philipp.kuehl@gmail.com>
2017-07-21 11:27:46 +0200

    fix some tests

--- mod/metric_answer_lookup/spec/lib/answer/answer_search_spec.rb
+++ mod/metric_answer_lookup/spec/lib/answer/answer_search_spec.rb
@@ -34,17 +34,17 @@ RSpec.describe Answer, "Answer.search" do
   end
 
   it "can count" do
-    expect(search(year: "2000", return: :count)).to eq 9
+    expect(search(year: "2000", return: :count)).to eq 10
   end
 
   it "can uniquify and return count" do
     result = search year: "2000", uniq: :company_id, return: :count
-    expect(result).to eq 4
+    expect(result).to eq 5
   end
 
   it "can uniquify and return different column" do
     result = search year: "2000", uniq: :company_id, return: :company_name
-    expect(result).to eq %w[Death_Star Monster_Inc
-                            Slate_Rock_and_Gravel_Company SPECTRE]
+    expect(result).to eq ["Apple_Inc", "Death_Star", "Monster_Inc",
+                          "Slate_Rock_and_Gravel_Company", "SPECTRE"]
   end
 end
