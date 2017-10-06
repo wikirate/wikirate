8b6f588a62fc630f5b3121d83935de42f3a39567
Ethan McCutchen <ethan@grasscommons.org>
2017-10-05 23:01:09 -0400

    content_was fix

--- mod/wikirate_source/set/type_plus_right/source/file/file.rb
+++ mod/wikirate_source/set/type_plus_right/source/file/file.rb
@@ -1,6 +1,6 @@
 
 event :block_file_changing, after: :write_identifier,
                             on: :update, changed: :content,
-                            when: proc { |c| !c.db_content_was.empty? } do
+                            when: proc { |c| !c.db_content_before_act.empty? } do
   errors.add :file, "is not allowed to be changed."
 end
