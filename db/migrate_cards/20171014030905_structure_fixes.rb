de797d92a20aba279827a565df298db36db717b7
Ethan McCutchen <ethan@grasscommons.org>
2017-10-16 11:38:51 -0400

    better handling of *source type and *metric type

--- db/migrate_cards/20171014030905_structure_fixes.rb
+++ db/migrate_cards/20171014030905_structure_fixes.rb
@@ -1,6 +1,13 @@
 class StructureFixes < Card::Migration
   def up
-    merge_cards [ "research page+company", "research page+metric", "metric value source form", "source+*type+*structure",
-                  "*metric_type+*right+*structure", "*source_type+*right+*structure"]
+    merge_cards [ "research page+company",
+                  "research page+metric",
+                  "metric value source form",
+                  "source+*type+*structure",
+                  "Cardtype+*metric type+*type plus *structure",
+                  "File+*source type",
+                  "Text+*source type",
+                  "Link+*source type"
+                ]
   end
 end
