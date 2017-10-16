3cd98aae9424ae5abab8d543626f6f1494c8c9ec
Ethan McCutchen <ethan@grasscommons.org>
2017-10-16 10:31:55 -0400

    fixed broken migration and updated seed data

--- db/migrate_cards/20171014030905_structure_fixes.rb
+++ db/migrate_cards/20171014030905_structure_fixes.rb
@@ -1,6 +1,6 @@
-class StructureFixes < ActiveRecord::Migration[5.1]
+class StructureFixes < Card::Migration
   def up
-    merge_cards "research page+company", "research page+metric", "metric value source form", "source+*type+*structure",
-                "*metric_type+*right+*structure", "*source_type+*right+*structure"
+    merge_cards [ "research page+company", "research page+metric", "metric value source form", "source+*type+*structure",
+                  "*metric_type+*right+*structure", "*source_type+*right+*structure"]
   end
 end
