00c87ef6975631c303756ea07b5ed7eea86643b7
xithan <philipp.kuehl@gmail.com>
2017-10-26 10:45:17 +0200

    Remove old filter interface

--- mod/filter/set/abstract/03_filter.rb
+++ mod/filter/set/abstract/03_filter.rb
@@ -1,21 +1,8 @@
 include_set Abstract::FilterFormgroups
-include_set Abstract::FilterHelper
 include_set Abstract::Utility
 
 def advanced_filter_keys
   []
-end
-
-def filter_keys_with_values
-  (filter_keys + advanced_filter_keys).map do |key|
-    values = filter_param(key)
-    next unless values.present?
-    [key, values]
-  end.compact
-end
-
-def all_filter_keys
-  filter_keys + advanced_filter_keys
 end
 
 # def search_wql type_id, opts, params_keys, return_param=nil, &block
@@ -30,25 +17,22 @@ format :html do
   end
 
   def filter_fields
-    categories = card.filter_keys + card.advanced_filter_keys
-    cats = categories.each_with_object({}) do |cat, h|
+    filter_form filter_form_data,
+                action: filter_action_path,
+                # classes are from the old interface, probably not needed anymore
+                class: "filter-container slotter sub-content",
+                id: "_filter_container"
+  end
+
+  def filter_form_data
+    all_filter_keys.each_with_object({}) do |cat, h|
       h[cat] = { label: filter_label(cat),
                  input_field: _render("#{cat}_formgroup"),
                  active: show_filter_field?(cat) }
     end
-    filter_form cats, action: filter_action_path,
-                      class: "filter-container slotter sub-content",
-                      id: "_filter_container"
   end
-
-  delegate :filter_keys, to: :card
-  delegate :advanced_filter_keys, to: :card
 
   def show_filter_field? field
-    filter_param field
-  end
-
-  def filter_active?
-    filter_keys.any? { |key| filter_param(key).present? }
+    filter_hash[field]
   end
 end
