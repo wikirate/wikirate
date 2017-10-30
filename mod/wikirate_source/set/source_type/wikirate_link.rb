36d0d882a382ac36103e479faf8cf9cf81e44f1b
Ethan McCutchen <ethan@grasscommons.org>
2017-10-30 14:12:01 -0400

    source_type minor refactoring, mod -> card-mods, etc

--- mod/wikirate_source/set/source_type/wikirate_link.rb
+++ mod/wikirate_source/set/source_type/wikirate_link.rb
@@ -8,8 +8,7 @@ format :html do
   end
 end
 
-event :autopopulate_website,
-      :prepare_to_store, on: :create, when: proc { |c| c.populate_website? } do
+event :autopopulate_website, :prepare_to_store, on: :create, when: :populate_website? do
   link = subfield(:wikirate_link).content
   host = URI.parse(link).host
   add_subfield :wikirate_website, content: "[[#{host}]]", type_id: PointerID
@@ -21,12 +20,11 @@ event :import_linked_source, :integrate_with_delay, on: :save do
   generate_pdf if import? && html_link?
 end
 
-event :process_source_url, after: :check_source,
-                           on: :create do
+event :process_source_url, after: :check_source, on: :create do
   if !(link_card = subfield(:wikirate_link)) || link_card.content.empty?
     errors.add(:link, "does not exist.")
     return
-  end
+  ends
   link_card.content.strip!
   @url = link_card.content
 
@@ -129,7 +127,7 @@ def download_and_add_file
   file_url = Addressable::URI.escape url
   add_subfield :file, remote_file_url: file_url, type_id: FileID, content: "dummy"
   source_type = subfield(:source_type)
-  source_type.content = "[[#{Card[:file].name}]]"
+  source_type.content = "[[#{:file.cardname}]]"
   remove_subfield :wikirate_link
   reset_patterns
   include_set_modules
