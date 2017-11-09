4783e255d8ea4bc2582086bbef85d05cc6978bf5
Ethan McCutchen <ethan@grasscommons.org>
2017-11-09 14:59:24 -0500

    simplified filter sort button class

--- mod/filter/set/abstract/02_filter_formgroups.rb
+++ mod/filter/set/abstract/02_filter_formgroups.rb
@@ -63,10 +63,8 @@ format :html do
   view :sort_formgroup, cache: :never do
     selected_option = sort_param || card.default_sort_option
     options = options_for_select(sort_options, selected_option)
-    content_tag :div, class: "_filter-input" do
-      select_tag "sort", options, class: "pointer-select",
-                 "data-minimum-results-for-search"=>"Infinity"
-    end
+    select_tag "sort", options, class: "pointer-select _filter-sort",
+               "data-minimum-results-for-search"=>"Infinity"
   end
 
   def sort_options
