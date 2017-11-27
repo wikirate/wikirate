2a46adeef26a04ac881f39aab210c7525ad6cdd8
xithan <philipp.kuehl@gmail.com>
2017-11-27 18:14:52 +0100

    import page: fix redirect [Finished #153183422]

--- mod/csv_import/set/abstract/import.rb
+++ mod/csv_import/set/abstract/import.rb
@@ -42,7 +42,7 @@ format :html do
   end
 
   def new_view_hidden
-    hidden_tags success: { id: "_self", soft_redirect: false, view: :import }
+    hidden_tags success: { id: "_self", soft_redirect: false, redirect: true, view: :import }
   end
 
   view :core do
