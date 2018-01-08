f1648e40ed7cb92d2480bc81a58bcefd33fe4e1e
xithan <philipp.kuehl@gmail.com>
2018-01-08 20:08:59 +0100

    :sunny:

--- .simplecov
+++ .simplecov
@@ -1,6 +1,5 @@
 unless ENV["COVERAGE"] == "false"
-  require File.expand_path("../vendor/wagn/card/spec/support/simplecov_helper",
-                           __FILE__)
+  require File.expand_path("../vendor/decko/card/spec/support/simplecov_helper", __FILE__)
   SimpleCov.start do
     SimpleCov.merge_timeout 1200
     card_simplecov_filters
--- mod/metrics/spec/lib/formula/calculator/input_spec.rb
+++ mod/metrics/spec/lib/formula/calculator/input_spec.rb
@@ -1,5 +1,5 @@
 RSpec.describe Formula::Calculator::Input do
-  subject do
+  let :input do
     input_cards = @input.map { |i| Card.fetch i }
     described_class.new(input_cards, @year_options, &:to_f)
   end
@@ -10,19 +10,19 @@ RSpec.describe Formula::Calculator::Input do
 
   example "single input" do
     @input = ["Jedi+deadliness"]
-    expect { |b| subject.each(year: 1977, company: "Death Star", &b) }
+    expect { |b| input.each(year: 1977, company: "Death Star", &b) }
       .to yield_with_args([100.0], death_star_id, 1977)
   end
 
   example "two metrics" do
     @input = %w[Jedi+deadliness Joe_User+researched]
-    expect { |b| subject.each(year: 1977, &b) }
+    expect { |b| input.each(year: 1977, &b) }
       .to yield_with_args([100.0, 77.0], death_star_id, 1977)
   end
 
   example "yearly variable" do
     @input = ["half year", "Joe User+researched number 1"]
-    expect { |b| subject.each(year: 2015, &b) }
+    expect { |b| input.each(year: 2015, &b) }
       .to yield_successive_args([[1007.5, 5.0], samsung_id, 2015],
                                 [[1007.5, 100.0], apple_id, 2015] )
   end
@@ -31,21 +31,21 @@ RSpec.describe Formula::Calculator::Input do
     it "relative range" do
       @input = ["Joe User+researched"]
       @year_options = ["-1..0"]
-      expect { |b| subject.each(year: 2013, company: "Apple Inc", &b) }
+      expect { |b| input.each(year: 2013, company: "Apple Inc", &b) }
         .to yield_with_args([[12.0, 13.0]], apple_id, 2013)
     end
 
     it "relative year" do
       @input = ["Joe User+researched"]
       @year_options = ["-1"]
-      expect { |b| subject.each(year: 2014, company: "Apple Inc", &b) }
+      expect { |b| input.each(year: 2014, company: "Apple Inc", &b) }
         .to yield_with_args([13.0], apple_id, 2014)
     end
 
     it "fixed start range" do
       @input = ["Joe User+researched"]
       @year_options = ["2010..0"]
-      expect { |b| subject.each(year: 2013, company: "Apple Inc", &b) }
+      expect { |b| input.each(year: 2013, company: "Apple Inc", &b) }
         .to yield_with_args([[10.0, 11.0, 12.0, 13.0]], apple_id, 2013)
     end
   end
