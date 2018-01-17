b98d8a211c41eb00e951b31b90781a3d77e656cd
Ethan McCutchen <ethan@grasscommons.org>
2018-01-16 22:42:13 -0500

    trying to get migration to run

--- db/migrate_cards/20180105134604_created_at_for_answers.rb
+++ db/migrate_cards/20180105134604_created_at_for_answers.rb
@@ -3,6 +3,9 @@
 class CreatedAtForAnswers < Card::Migration
   def up
     Answer.where(created_at: nil).each do |a|
+      if a.card.type_id != Card::MetricValueID
+        a.card.update_attributes! type_id: Card::MetricValueID
+      end
       a.update_attributes! created_at: a.card.created_at
     end
   end
