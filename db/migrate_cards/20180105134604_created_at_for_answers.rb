d88a8848fc2b1c6bddccebe7b54e0b039786201e
Ethan McCutchen <ethan@grasscommons.org>
2018-01-16 23:03:20 -0500

    another go at migration fix

--- db/migrate_cards/20180105134604_created_at_for_answers.rb
+++ db/migrate_cards/20180105134604_created_at_for_answers.rb
@@ -3,10 +3,12 @@
 class CreatedAtForAnswers < Card::Migration
   def up
     Answer.where(created_at: nil).each do |a|
-      if a.card.type_id != Card::MetricValueID
-        a.card.update_attributes! type_id: Card::MetricValueID
+      if a.card.type_id == Card::MetricValueID
+        a.update_attributes! created_at: a.card.created_at
+      else
+        a.card.delete!
+        a.destroy!
       end
-      a.update_attributes! created_at: a.card.created_at
     end
   end
 end
