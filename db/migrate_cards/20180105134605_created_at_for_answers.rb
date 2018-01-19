7315f76bc0a2e9887a0c99acf8a630cda7873597
Ethan McCutchen <ethan@grasscommons.org>
2018-01-18 22:38:28 -0500

    fixed migration logic

--- db/migrate_cards/20180105134605_created_at_for_answers.rb
+++ db/migrate_cards/20180105134605_created_at_for_answers.rb
@@ -5,11 +5,11 @@ class CreatedAtForAnswers < Card::Migration
     Answer.where(created_at: nil).each do |a|
       next unless a.card
       if a.card.type_id == Card::MetricValueID
-        if a.card.calculated?
+        if a.card.researched?
+          a.update_attributes! created_at: a.card.created_at
+        else
           a.update_attributes! created_at: a.card.created_at, answer_id: nil
           a.card.delete!
-        else
-          a.update_attributes! created_at: a.card.created_at
         end
       else
         a.destroy!
