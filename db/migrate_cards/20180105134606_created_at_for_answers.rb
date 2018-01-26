9677ec961a4223c7609b595ec8446f146a4ff90f
Ethan McCutchen <ethan@grasscommons.org>
2018-01-25 21:31:11 -0500

    comment out tough migration

--- db/migrate_cards/20180105134606_created_at_for_answers.rb
+++ db/migrate_cards/20180105134606_created_at_for_answers.rb
@@ -2,25 +2,25 @@
 
 class CreatedAtForAnswers < Card::Migration
   def up
-    Answer.where(created_at: nil).find_each do |a|
-      next unless a.card
-      if a.card.type_id == Card::MetricValueID
-        begin
-          if a.card.researched?
-            a.update_attributes! created_at: a.card.created_at
-          else
-            a.created_at = a.card.created_at
-            a.answer_id = nil
-            a.card.delete!
-            a.save!
-            puts "successfully migrated calculated answer: #{a.id}"
-          end
-        rescue
-          puts "problem migrating answer: #{a.id} "
-        end
-      else
-        a.destroy!
-      end
-    end
+    # Answer.where(created_at: nil).find_each do |a|
+    #   next unless a.card
+    #   if a.card.type_id == Card::MetricValueID
+    #     begin
+    #       if a.card.researched?
+    #         a.update_attributes! created_at: a.card.created_at
+    #       else
+    #         a.created_at = a.card.created_at
+    #         a.answer_id = nil
+    #         a.card.delete!# 
+    #         a.save!
+    #         puts "successfully migrated calculated answer: #{a.id}"
+    #       end
+    #     rescue
+    #       puts "problem migrating answer: #{a.id} "
+    #     end
+    #   else
+    #     a.destroy!
+    #   end
+    # end
   end
 end
