ac03fcf60f102fd831eeeb62a8ed21a06a8e8209
xithan <philipp.kuehl@gmail.com>
2018-01-30 01:32:42 +0100

    green

--- mod/metrics/spec/set/right/checked_by_spec.rb
+++ mod/metrics/spec/set/right/checked_by_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe Card::Set::Right::CheckedBy do
     context "when nobody checked" do
       example "creator", with_user: "WikiRate Bot" do
         is_expected.to have_tag :div do
-          with_tag :h5, /Checks (0)/
+          with_tag :h5, /Checks \(0\)/
           with_text /Nobody has checked this value since it was created/
           without_text "checked this value"
           without_tag :button
@@ -27,7 +27,7 @@ RSpec.describe Card::Set::Right::CheckedBy do
 
       example "other user" do
         is_expected.to have_tag :div do
-          with_tag :h5, /Checks (0)/
+          with_tag :h5, /Checks \(0\)/
           with_text /Nobody has checked this value since it was created/
           without_text "checked this value"
           with_tag :button, "Yes, I checked"
@@ -44,7 +44,7 @@ RSpec.describe Card::Set::Right::CheckedBy do
 
         example "creator", with_user: "Wikirate Bot" do
           is_expected.to have_tag :div do
-            with_tag :h5, /Checks (0)/
+            with_tag :h5, /Checks \(0\)/
             with_text /Nobody has checked this value since it was last updated/
             without_text "checked this value"
             with_tag :button, "Yes, I checked"
@@ -54,7 +54,7 @@ RSpec.describe Card::Set::Right::CheckedBy do
 
         example "updater", with_user: "John" do
           is_expected.to have_tag :div do
-            with_tag :h5, /Checks (0)/
+            with_tag :h5, /Checks \(0\)/
             with_text /Nobody has checked this value since it was last updated/
             without_text "checked this value"
             without_tag :button
@@ -72,7 +72,7 @@ RSpec.describe Card::Set::Right::CheckedBy do
 
       example "creator", with_user: "WikiRate Bot" do
         is_expected.to have_tag :div do
-          with_tag :h5, /Checks (1)/
+          with_tag :h5, /Checks \(1\)/
           with_tag :a, "John"
           with_text /checked this value/
           without_tag :button
@@ -81,7 +81,7 @@ RSpec.describe Card::Set::Right::CheckedBy do
 
       example "checker", with_user: "John" do
         is_expected.to have_tag :div do
-          with_tag :h5, /Checks (1)/
+          with_tag :h5, /Checks \(1\)/
           with_tag :a, "John"
           with_text /checked this value/
           with_tag :button, "Uncheck"
@@ -90,7 +90,7 @@ RSpec.describe Card::Set::Right::CheckedBy do
 
       example "other user" do
         is_expected.to have_tag :div do
-          with_tag :h5, /Checks (1)/
+          with_tag :h5, /Checks \(1\)/
           with_tag :a, "John"
           with_text /checked this value/
           with_tag :button, "Yes, I checked"
@@ -110,7 +110,7 @@ RSpec.describe Card::Set::Right::CheckedBy do
 
       example "checker", with_user: "Joe Admin" do
         is_expected.to have_tag :div do
-          with_tag :h5, /Checks (4)/
+          with_tag :h5, /Checks \(4\)/
           with_tag :a, "Joe Admin"
           with_text /John, Joe User, Joe Camel, and Joe Admin/
           with_tag :button, "Uncheck"
