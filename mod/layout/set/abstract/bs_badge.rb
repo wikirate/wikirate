32a1dbf80ee69c225c2c054b8f43930df0c607a1
Ethan McCutchen <ethan@grasscommons.org>
2018-02-22 14:50:29 -0500

    starting in on badg API

--- mod/layout/set/abstract/bs_badge.rb
+++ mod/layout/set/abstract/bs_badge.rb
@@ -1,4 +1,11 @@
 format :html do
+  def bs_badge count, label, klass=nil
+    @badge_count = count
+    @badge_label = label
+    @badge_class = klass
+    haml
+  end
+
   view :bs_badge, template: :haml
   view :bs_badge_content, template: :haml
 end
