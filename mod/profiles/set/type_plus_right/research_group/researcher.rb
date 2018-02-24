afaa5173b635a9c2881aa0e313a927246d798b07
Ethan McCutchen <ethan@grasscommons.org>
2018-02-23 20:44:51 -0500

    responses to pk comments

--- mod/profiles/set/type_plus_right/research_group/researcher.rb
+++ mod/profiles/set/type_plus_right/research_group/researcher.rb
@@ -1,3 +1,10 @@
+CONTRIBUTION_CATEGORIES = %i[created updated discussed double_checked].freeze
+CONTRIBUTION_CATEGORY_HEADER = ["Answers"].concat(
+  CONTRIBUTION_CATEGORIES.map do |category|
+    Card::Set::LtypeRtype::User::Cardtype::ACTION_LABELS[category]
+  end
+)
+
 def ok_to_join?
   Auth.signed_in? && !current_user_is_member?
 end
@@ -10,9 +17,17 @@ def joining?
   Env.params[:join].present?
 end
 
+def leaving?
+  Env.params[:leave].present?
+end
+
 event :join_group, :validate, when: :joining? do
   abort :failure, "cannot join this group" unless ok_to_join?
   add_item Auth.current.name
+end
+
+event :leave_group, :validate, when: :leaving? do
+  drop_item Auth.current.name
 end
 
 format :html do
@@ -24,41 +39,42 @@ format :html do
     return "" unless card.count.positive?
     with_paging do |paging_args|
       table_content = member_contribution_content members_on_page(paging_args)
-      table table_content, header: member_contribution_header
+      table table_content, header: CONTRIBUTION_CATEGORY_HEADER
     end
   end
 
   def members_on_page paging_args
     wql = { referred_to_by: card.name, type_id: UserID, sort: :name }
-    Card.search wql.merge! paging_args.extract!(:limit, :offset)
+    Card.search wql.merge!(paging_args.extract!(:limit, :offset))
   end
 
   view :join_button, tags: :unknown_ok, denial: :blank, cache: :never,
                      perms: ->(r) { r.card.ok_to_join? } do
-    link_to "Join", path: { action: :update, join: true, success: { view: :overview } },
+    link_to "Join Group", path: { action: :update, join: true, success: { view: :overview } },
                     class: "btn btn-primary btn-sm slotter", remote: true
   end
 
+  view :leave_button, tags: :unknown_ok, denial: :blank, cache: :never,
+       perms: ->(r) { r.card.current_user_is_member? } do
+    link_to "Leave Group", path: { action: :update, leave: true, success: { view: :overview } },
+            class: "btn btn-outline-primary btn-sm slotter", remote: true
+  end
+
   view :manage_button, tags: :unknown_ok do
-    css_class = "btn btn-outline-primary btn-sm slotter"
-    link_to_view("edit", "Manage Researcher List", class: css_class)
-  end
-
-  def member_contribution_header
-    contribution_categories.map do |category|
-      Card::Set::LtypeRtype::User::Cardtype::ACTION_LABELS[category]
-    end.unshift "Answers"
-  end
-
-  def contribution_categories
-    [:created, :updated, :discussed, :double_checked]
+    link_to_view "edit",
+                 "Manage Researcher List",
+                 class: "btn btn-outline-primary btn-sm slotter"
   end
 
   def member_contribution_content members
     members.map do |member|
-      contribution_categories.map do |category|
-        card.left.contribution_count member.name, :metric_value, category
-      end.unshift nest(member, view: :thumbnail)
+      [nest(member, view: :thumbnail)].concat contribution_counts(member)
+    end
+  end
+
+  def contribution_counts member
+    CONTRIBUTION_CATEGORIES.map do |category|
+      card.left.contribution_count member.name, :metric_value, category
     end
   end
 end
