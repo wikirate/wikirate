47faf4f7ff930d33cdd34909a42ab94b630e46f8
Philipp Kühl <philipp.kuehl@gmail.com>
2018-03-20 00:40:53 +0100

    use safe_name

--- mod/widgets/lib/open_corporates/api.rb
+++ mod/widgets/lib/open_corporates/api.rb
@@ -29,11 +29,49 @@ module OpenCorporates
         end
       end
 
+      # The OC API returns an array with a structure like this:
+      #  "results":{
+      #     "industry_code_scheme":{
+      #        "id":"isic_4","name":"UN ISIC Rev 4",
+      #        "url":"http://unstats.un.org/unsd/cr/registry/isic-4.asp",
+      #        "industry_codes":[
+      #          {"industry_code":{
+      #              "code":"A","description":"Agriculture, forestry and fishing",
+      #              "uid":"isic_4-A"}},
+      #          {"industry_code":{
+      #              "code":"01","description":"Crop and animal production",
+      #          ....
+      # This method returns an array with a hash for each industry codes
+      # @return [Array<Hash>] each industry code has a code, a description and a uid key
+      #   Example:
+      #       :code=>"855",
+      #       :description=>"Educational support activities",
+      #       :uid=>"isic_4-855"
+      def fetch_industry_codes code_scheme="isic_4"
+        fetch_items(:industry_codes, code_scheme,
+                    collection_selector: [:results, :industry_code_scheme, :industry_codes],
+                    item_selector: :industry_code).each do |item|
+          item.symbolize_keys!
+        end
+      end
+
       # @example
       #  fetch_json :companies, us_ca, 3234234, sparse: true
       # @return the full json response converted to a hash
       def fetch *query_args
         JSON.parse json_response(*query_args)
+      end
+
+      def fetch_items *query_args, collection_selector:, item_selector: nil
+        collection_selector = Array.wrap(collection_selector).map(&:to_s)
+        result = pick_nested_item(*collection_selector) do
+            fetch(*query_args)
+        end
+        return result unless item_selector
+        item_selector = Array.wrap(item_selector).map(&:to_s)
+        result.map do |item|
+          item.dig(*item_selector)
+        end
       end
 
       private
