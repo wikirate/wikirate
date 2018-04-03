d46de0498e8e731b23315de455abacc2341ead03
Ethan McCutchen <ethan@grasscommons.org>
2018-04-03 17:01:27 -0400

    decko tick (and export cleanup)

--- mod/wikirate/set/type/search_type/export.rb
+++
@@ -1,8 +0,0 @@
-format :json do
-  def items_for_export
-    return [] if card.content.empty? ||
-                 card.name.include?("+*options") ||
-                 card.name.include?("+*structure")
-    card.item_cards
-  end
-end
