ae88c7edf8d3e33e8bb66ab6862f5fd87c3b5feb
Ethan McCutchen <ethan@grasscommons.org>
2018-04-06 15:06:04 -0400

    comment out relationship counts migration for now

--- db/migrate_cards/20180328142643_correct_relationship_counts.rb
+++ db/migrate_cards/20180328142643_correct_relationship_counts.rb
@@ -4,8 +4,8 @@ class CorrectRelationshipCounts < Card::Migration
   disable_ddl_transaction!
 
   def up
-    Card.search(type_id: Card::RelationshipAnswerID).each do |card|
-      card.update_counts!
-    end
+    # Card.search(type_id: Card::RelationshipAnswerID).each do |card|
+    #   card.update_counts!
+    # end
   end
 end
