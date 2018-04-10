e9c7297ba78f04ef8c86f6ebb840d1cdfeb45c25
Ethan McCutchen <ethan@grasscommons.org>
2018-04-10 17:40:55 -0400

    rescue answer validation errors

--- db/migrate_cards/20180328145810_correct_answer_names.rb
+++ db/migrate_cards/20180328145810_correct_answer_names.rb
@@ -9,7 +9,11 @@ class CorrectAnswerNames < Card::Migration
         company = Card.fetch_name(a.company_id).to_s
         metric =  Card.fetch_name(a.metric_id).to_s
         if company != a.company_name || metric != a.metric_name
-          a.update_attributes! company_name: company, metric_name: metric
+          begin
+            a.update_attributes! company_name: company, metric_name: metric
+          rescue => e
+            puts "error updating answer #{a.id}: #{e.message}"
+          end
         end
       end
     end
