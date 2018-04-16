90da39a0e32c2a571f17530d20833070bb7480b9
xithan <philipp.kuehl@gmail.com>
2018-04-16 16:54:12 +0200

    move *css content

--- db/migrate_cards/20180414102432_move_customized_skin.rb
+++ db/migrate_cards/20180414102432_move_customized_skin.rb
@@ -5,14 +5,16 @@ class MoveCustomizedSkin < Card::Migration
     $teal: #03998d !default;
     $primary: $teal !default;
   SCSS
+
   def up
     ensure_card "*stylesheets", type_id: Card::SkinID, codename: "stylesheets"
     ensure_card "wikirate skin", type_id: Card::CustomizedSkinID
     ensure_card "wikirate stylesheets",
                 type_id: Card::SkinID, codename: "wikirate_stylesheets"
     Card::Cache.reset_all
+    ensure_card "emergency style hacks", type_id: Card::ScssID
     update_card ["wikirate skin", :stylesheets],
-                content: "[[wikirate stylesheets]]"
+                content: "[[wikirate stylesheets]]\n[[emergency style hacks]]"
     Card["wikirate skin", :variables].update_column :db_content, WIKIRATE_VARIABLES
     update_card [:all, :style], content: "[[wikirate skin]]"
     Card.reset_all_machines
