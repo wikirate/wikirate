d708419f3cc05ab1de700d1425ebab66823f8986
xithan <philipp.kuehl@gmail.com>
2018-04-16 14:13:07 +0200

    reorganize wikirate style

---
+++ db/migrate_cards/20180414102432_move_customized_skin.rb
@@ -0,0 +1,20 @@
+# -*- encoding : utf-8 -*-
+
+class MoveCustomizedSkin < Card::Migration
+  WIKIRATE_VARIABLES = <<-SCSS.strip_heredoc
+    $teal: #03998d !default;
+    $primary: $teal !default;
+  SCSS
+  def up
+    ensure_card "*stylesheets", type_id: Card::SkinID, codename: "stylesheets"
+    ensure_card "wikirate skin", type_id: Card::CustomizedSkinID
+    ensure_card "wikirate stylesheets",
+                type_id: Card::SkinID, codename: "wikirate_stylesheets"
+    Card::Cache.reset_all
+    update_card ["wikirate skin", :stylesheets],
+                content: "[[wikirate stylesheets]]"
+    Card["wikirate skin", :variables].update_column :db_content, WIKIRATE_VARIABLES
+    update_card [:all, :style], content: "[[wikirate skin]]"
+    Card.reset_all_machines
+  end
+end
