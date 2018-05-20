7ab5edaf6e9d88b21773ba67b7aeec8c89c7c846
Ethan McCutchen <ethan@grasscommons.org>
2018-05-20 15:39:25 -0400

    decribe_views fixes and vendor ticks

--- mod/profiles/spec/set/type/research_group_spec.rb
+++ mod/profiles/spec/set/type/research_group_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Card::Set::Type::ResearchGroup do
   let(:research_group) { Card["Jedi"] }
 
   describe_views :open_content, :listing, :edit,
-                 :researcher_tab, :metric_tab, :project_tab do |view|
+                 :researcher_tab, :metric_tab, :project_tab do
     it "has no errors" do
       expect(research_group.format.render(view)).to lack_errors
     end
