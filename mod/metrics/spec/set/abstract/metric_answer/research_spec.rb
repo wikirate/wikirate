ba4e66178f85568dff0b678eeb097c3fea23f09f
Philipp Kühl <philipp.kuehl@gmail.com>
2018-05-29 09:24:37 +0200

    fix spec

--- mod/metrics/spec/set/abstract/metric_answer/research_spec.rb
+++ mod/metrics/spec/set/abstract/metric_answer/research_spec.rb
@@ -4,8 +4,10 @@ RSpec.describe Card::Set::Abstract::MetricAnswer::Research do
   specify "non-researchable metrics are not editable" do
     rendered = view :edit, card: "Jedi+friendliness+Death Star+1977"
     aggregate_failures "no edit form" do
-      expect(rendered).not_to have_tag ("input")
-      expect(rendered).and include "metric not editable"
+      expect(rendered).to have_tag("div.card-slot.left_research_side-view") do
+        without_tag :input
+      end
+      expect(rendered).to include "Answers to this metric cannot be researched directly."
     end
   end
 end
