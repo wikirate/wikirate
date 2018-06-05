5c10191ee30ef86b56ce8e2a9cfebbbe4442cbb8
Ethan McCutchen <ethan@grasscommons.org>
2018-06-05 15:53:48 -0400

    wikirating and formula features (kinda) working

---
+++ mod/metrics/features/step_definitions/metric_steps.rb
@@ -0,0 +1,31 @@
+When(/^the submit button (should|should not) be disabled$/) do |should|
+  if should =~ /not/
+    expect(submit_button).not_to be_disabled
+  else
+    expect(submit_button).to be_disabled
+  end
+end
+
+When(/^the weight total should be "(\d+)"$/) do |total|
+  expect(weight_total).to eq(total)
+end
+
+When(/I set weight for "([^"]*)" to "(\d+)"$/) do |metric, weight|
+  assign_weight metric, weight
+end
+
+# assign metric weight via jQuery
+# (would be more idiomatic to use xpath)
+def assign_weight metric, weight
+  selector = "$('[data-card-name=\"#{metric}\"]').closest('tr').find('[name=\"pair_value\"]')"
+  page.execute_script "#{selector}.val(#{weight});"
+  page.execute_script "#{selector}.trigger('input');"
+end
+
+def weight_total
+  find("#weight_sum").value.to_i
+end
+
+def submit_button
+  find(".submit-button")
+end
