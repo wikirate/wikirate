fe286011c068cd088e8ccf2a82be0095526157b6
xithan <philipp.kuehl@gmail.com>
2018-06-26 09:54:31 -0400

    try to fix badge problem

--- mod/badges/set/type_plus_right/metric_answer/checked_by/award_badges.rb
+++ mod/badges/set/type_plus_right/metric_answer/checked_by/award_badges.rb
@@ -1,6 +1,6 @@
 include_set Abstract::AwardBadges, squad_type: :metric_answer
 
 event :award_answer_check_badges, :finalize,
-      on: :save do
+      on: :save, changed: :content do
   award_badge_if_earned :check
 end
