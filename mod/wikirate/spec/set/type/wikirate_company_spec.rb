9f548bdd86c3b028d8fcd020eeb0b8e6dba5d2b5
xithan <philipp.kuehl@gmail.com>
2018-07-19 20:29:51 +0200

    :rainbow:

--- mod/wikirate/spec/set/type/wikirate_company_spec.rb
+++ mod/wikirate/spec/set/type/wikirate_company_spec.rb
@@ -1,26 +1,28 @@
-describe Card::Set::Type::WikirateCompany do
+RSpec.describe Card::Set::Type::WikirateCompany do
   it "shows the link for view \"missing\"" do
     html = render_card :missing, type_id: Card::WikirateCompanyID, name: "non-existing-card"
     expect(html).to eq(render_card(:link, type_id: Card::WikirateCompanyID, name: "non-existing-card"))
   end
 
-
   describe "creating company with post request", type: :controller do
     routes { Decko::Engine.routes }
-    before do
-      @controller = CardController.new
-      #login_as "joe_user"
-    end
+    before { @controller = CardController.new }
 
-    let(:token) { Card["Joe Admin",:account, :token] }
+    let(:token) do
+      Card::Auth.as_bot do
+        Card["Joe Admin", :account, :token].update_attributes! content: "abcd"
+        "abcd"
+      end
+    end
 
     it "creates company" do
       post :create, params: { card: { name: "new company",
                                       subcards: { "+:open_corporates" => "C0806592",
                                                   "+:headquarters" => "us_ca" } },
                               success: { format: :json },
-                              token: token.content }
-      expect_card("new company").to exist
+                              token: token }
+      expect_card("new company")
+        .to exist
         .and have_a_field(:open_corporates).with_content("C0806592")
         .and have_a_field(:headquarters).with_content("[[California (United States)]]")
     end
