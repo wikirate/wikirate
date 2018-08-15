36455a53b1f6a6546df5043d67d8c64d8a926029
Ethan McCutchen <ethan@grasscommons.org>
2018-08-15 13:08:33 -0400

    remove binding.pry

--- mod/metrics/set/abstract/value/unknown.rb
+++ mod/metrics/set/abstract/value/unknown.rb
@@ -2,7 +2,6 @@ UNKNOWN = "Unknown".freeze
 
 event :unknown_value, :initialize,
       when: ->(c) { c.subfield(:unknown) } do
-  binding.pry
   self.content = UNKNOWN if subfield(:unknown).checked?
   detach_subfield :unknown
 end
