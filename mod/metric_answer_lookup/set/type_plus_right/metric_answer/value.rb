154f7ac0305099e49ed0f9911bfd5a188d7a3a89
Ethan McCutchen <ethan@grasscommons.org>
2018-08-15 13:55:56 -0400

    more debugging removal

--- mod/metric_answer_lookup/set/type_plus_right/metric_answer/value.rb
+++ mod/metric_answer_lookup/set/type_plus_right/metric_answer/value.rb
@@ -8,7 +8,6 @@ event :delete_answer_lookup_table_entry_due_to_value_change, :finalize, on: :del
 end
 
 event :create_answer_lookup_entry_due_to_value_change, :finalize, on: :create do
-  raise Card::Error
   if hybrid? && (ans = left&.answer)
     update_answer id: ans.id
   else
