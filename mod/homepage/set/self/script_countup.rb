c2d4f1b451a8d074d0c9c1fc17d399cf05237b48
dcastroeyss <daniel.castro@eyss-dev.com>
2018-08-16 14:23:24 +0200

    fix error

--- mod/homepage/set/self/script_countup.rb
+++ mod/homepage/set/self/script_countup.rb
@@ -2,7 +2,7 @@ include_set Abstract::CodeFile
 Self::ScriptLibraries.add_item :script_countup
 
 def source_files
-  "vendor/CountUp/dist/countUp.js"
+  "vendor/countUp/dist/countUp.js"
 end
 
 def source_dir
