32d5158dc60197fb05ea521fa99d92de2ef51572
dcastroeyss <daniel.castro@eyss-dev.com>
2018-08-22 14:00:12 +0200

    minor fixes

--- mod/projects/spec/set/self/project.rb
+++
@@ -1,8 +0,0 @@
-describe Card::Set::Self::Project do
-  specify "view core" do
-    expect_view(:core).to have_tag("div.card") do
-      with_tag "div.card-header"
-      with_tag "div.card-body"
-    end
-  end
-end
