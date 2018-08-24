d6607c3654e95148d26776858c30d85b71299cb0
dcastroeyss <daniel.castro@eyss-dev.com>
2018-08-24 15:54:59 +0200

    minor fix in test rspec

--- mod/projects/spec/set/self/project_spec.rb
+++ mod/projects/spec/set/self/project_spec.rb
@@ -1,8 +1,8 @@
 describe Card::Set::Self::Project do
   specify "view core" do
-    expect_view(:core).to have_tag("div.card") do
-      with_tag "div.card-header"
-      with_tag "div.card-body"
+    expect_view(:core).to have_tag("div.card-slot") do
+      with_tag "div.tabbable"
+      with_tag "div.tab-content"
     end
   end
 end
