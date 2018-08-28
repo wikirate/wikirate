0661a9bcaa0895308172ebaaacbb02eb4e5caf67
Ethan McCutchen <ethan@grasscommons.org>
2018-08-27 21:42:07 -0400

    first go at prme migration

---
+++ db/migrate_cards/20180827205903_fix_prme_years.rb
@@ -0,0 +1,34 @@
+# -*- encoding : utf-8 -*-
+
+class FixPrmeYears < Card::Migration
+  def up
+    duplicate_list = []
+    each_broken_answer_id do |answer_id|
+      answer = Card.fetch answer_id
+      update_answer_or_track_duplicate answer, duplicate_list
+    end
+    store_duplicates duplicate_list
+    raise "blah"
+  end
+
+  def update_answer_or_track_duplicate answer, duplicate_list
+    new_name = answer.name.gsub /7$/, "6"
+    if Card[new_name]
+      duplicate_list << answer.name
+    else
+      answer.update_attributes! name: new_name
+    end
+  end
+
+  def store_duplicates duplicates
+    return if duplicates.empty?
+    Card.create name: "PRME duplicates", type_id: Card::PointerID, content: duplicates
+  end
+
+  def each_broken_answer_id
+    path = File.expand_path "../csv/gri_data_to_clean.csv", __FILE__
+    csv = File.read path
+    rows = CSV.parse csv
+    rows.each { |row| yield row.first.to_i }
+  end
+end
