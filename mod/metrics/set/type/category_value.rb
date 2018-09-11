7eed892437f1420d60127761f46e2615ef484442
Ethan McCutchen <ethan@grasscommons.org>
2018-09-11 11:52:20 +0200

    track supercards in new +values

--- mod/metrics/set/type/category_value.rb
+++ mod/metrics/set/type/category_value.rb
@@ -1,12 +1,27 @@
 include_set Abstract::Value
 include_set Abstract::Pointer
 
-def option_card
+def raw_value
+  json_options? ? raw_values_from_hash : super
+end
+
+def inverted_options_hash
+  options_hash.each_with_object({}) do |(k, v), h|
+    h[v] = k
+  end
+end
+
+def raw_values_from_hash
+  hash = inverted_options_hash
+  item_names.map { |item| hash[item] }
+end
+
+def options_card
   Card.fetch metric, :value_options, new: {}
 end
 
 def option_names
-  option_card.item_names context: :raw
+  options_card.item_names context: :raw
 end
 
 format :html do
