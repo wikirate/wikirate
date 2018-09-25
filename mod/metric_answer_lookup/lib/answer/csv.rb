9a12bde4f9e7d5a494ed3ae6ce2a7edf8cd2dca7
Ethan McCutchen <ethan@grasscommons.org>
2018-09-25 16:53:13 -0400

    rolled back accidental csv change

--- mod/metric_answer_lookup/lib/answer/csv.rb
+++ mod/metric_answer_lookup/lib/answer/csv.rb
@@ -9,14 +9,10 @@ class Answer
       CSV.generate_line [answer_id, metric_name, company_name, year, value]
     end
 
-    def answer_link
-      card_url
-    end
-
     # class methods for {Answer}
     module ClassMethods
       def csv_title
-        CSV.generate_line ["ANSWER ID", "ANSWER_LINK", "METRIC NAME", "COMPANY NAME", "YEAR", "VALUE", "FIRST_SOURCE", "NUM_SOURCES"]
+        CSV.generate_line ["ANSWER ID", "METRIC NAME", "COMPANY NAME", "YEAR", "VALUE"]
       end
     end
   end
