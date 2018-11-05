ccfa5a244ba8b92f2dc3283b99bc93901a956137
Ethan McCutchen <ethan@grasscommons.org>
2018-11-05 16:02:09 -0500

    fix value type / unknown bug

--- db/migrate_cards/20180328142644_correct_relationship_counts.rb
+++ db/migrate_cards/20180328142644_correct_relationship_counts.rb
@@ -4,9 +4,14 @@ class CorrectRelationshipCounts < Card::Migration
   disable_ddl_transaction!
 
   def up
-    Card.search(type_id: Card::RelationshipAnswerID).each do |card|
-      puts card.name
-      card.update_counts!
+    Card.where(type_id: Card::RelationshipAnswerID).find_each do |card|
+      card.include_set_modules
+      begin
+        puts "updated counts for #{card.name}"
+        card.update_counts!
+      rescue
+        puts "error updating counts for #{card.name}"
+      end
     end
   end
 end
