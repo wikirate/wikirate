109e042e387fd51cb34efa090f043403838a104d
Philipp Kühl <philipp.kuehl@gmail.com>
2018-11-27 23:46:34 +0100

    working on network aware parser

--- mod/calculated_metrics/lib/formula/calculator/input/company_option_parser.rb
+++ mod/calculated_metrics/lib/formula/calculator/input/company_option_parser.rb
@@ -2,9 +2,67 @@ module Formula
   class Calculator
     class Input
       class CompanyOptionParser
-        #   Related[Jedi+more evil>=6 && Commons+Supplied by=Tier 1 Supplier]
-        def initialize string
+
+        class Expression
+          attr_reader :metric_card, :value, :metric_id
+
+          class Error < StandardError
+          end
+
+          SEPARATORS = %w[&& ||].freeze
+          SPLIT_REGEX = SEPARATORS.map { |sep| /\)*\s#{Regexp.quote sep}\s*\(*/ }.join "|"
+
+          def initialize string
+            @metric, @value =
+                          string.split Card::Query::OPERATORS.values.join("|")
+            validate
+            @metric_id = @metric_card.id
+          end
+
+          def validate
+            if @metric.blank? || @value.blank?
+              raise Error, "invalid expression \"#{string}\""
+            end
+            @metric_card = Card.fetch @metric
+            if @metric_card.nil? || @metric_card.type_id != Card::MetricID
+              raise Error, "no a metric: \"#{@metric}\""
+            end
+          end
         end
+
+
+        #   Related[Jedi+more evil >= 6 && Commons+Supplied by=Tier 1 Supplier]
+        def initialize string
+          @string = string
+        end
+
+        def parse
+          case @string
+          when /^\s*Related\[([^]]+)\]\s*$/
+            related_parse $1
+          when /^[\w\s+\d]/
+
+          end
+        end
+
+        def related_parse string
+          expr =
+            string.split(Expression::SPLIT_REGEX).map do |part|
+              Expression.new part.strip.gsub(/^\(*/).gsub(/\)*$/)
+            end
+          Relation.where(metric_id AND value >= 6)
+        rescue Expression::Error => e
+
+        end
+
+        def query
+
+        end
+
+        def companies_and_years
+
+        end
+
       end
     end
   end
--- mod/calculated_metrics/spec/lib/formula/calculator/company_option_parser_spec.rb
+++ mod/calculated_metrics/spec/lib/formula/calculator/company_option_parser_spec.rb
@@ -1,9 +1,4 @@
 RSpec.describe Formula::Calculator::Input::CompanyOptionParser do
-  let :input do
-    @requirement ||= :all
-    input_cards = @input.map { |i| Card.fetch i }
-    described_class.new(input_cards, @requirement, @year_options, &:to_f)
-  end
 
   let(:death_star_id) { Card.fetch_id "Death Star" }
   let(:apple_id) { Card.fetch_id "Apple Inc" }
