aa538f24e9eb82a35a2ab12394a88bb112ec0bac
Srivigneshwar R Prasad <prasadsrivi@gmail.com>
2018-11-27 15:30:26 +0000

    fresh source copy button

--- mod/sources/set/type/source/citation.rb
+++ mod/sources/set/type/source/citation.rb
@@ -8,17 +8,17 @@ format :html do
   view :wikirate_copy_message, template: :haml
 
   view :bar_and_preview, cache: :never do
-    wrap { [
-            render_close_icon,
-            render_mini_bar,
-            render_wikirate_copy_message,
-            render_preview
-        ] }
+    wrap do
+      [render_close_icon,
+       render_mini_bar, render_wikirate_copy_message, render_preview]
+    end
   end
 
   view :cite_button, template: :haml
 
   view :uncite_button, template: :haml
+
+  view :freshen_button, template: :haml
 
   def hidden_item_input
     tag :input, type: "hidden", class: "_pointer-item", value: card.name
