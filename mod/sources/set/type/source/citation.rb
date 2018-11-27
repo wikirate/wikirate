e1fd711d989b73637464e974e5d0a9ed863ae264
Srivigneshwar R Prasad <prasadsrivi@gmail.com>
2018-11-27 14:14:33 +0000

    research page source preview ui fixes

--- mod/sources/set/type/source/citation.rb
+++ mod/sources/set/type/source/citation.rb
@@ -3,8 +3,17 @@ format :html do
 
   view :preview_bar, template: :haml
 
+  view :close_icon, template: :haml
+
+  view :wikirate_copy_message, template: :haml
+
   view :bar_and_preview, cache: :never do
-    wrap { [render_mini_bar, render_preview] }
+    wrap { [
+            render_close_icon,
+            render_mini_bar,
+            render_wikirate_copy_message,
+            render_preview
+        ] }
   end
 
   view :cite_button, template: :haml
