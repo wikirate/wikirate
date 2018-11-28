138d8e0c0aa18a53dc2d58c0cbad0714605ff052
Ethan McCutchen <ethan@grasscommons.org>
2018-11-27 22:43:31 -0500

    handle wikirate urls in citation search

--- mod/metrics/set/abstract/metric_answer/source_views.rb
+++ mod/metrics/set/abstract/metric_answer/source_views.rb
@@ -109,7 +109,13 @@ format :html do
 
   def sources_found_by_url
     return unless source_search_term.url?
-    Self::Source.find_duplicates source_search_term
+    wikirate_source_from_url || Self::Source.find_duplicates(source_search_term)
+  end
+
+  def wikirate_source_from_url
+    mdata = source_search_term.match(/\/\/wikirate\.org\/(.*)$/)
+    return unless mdata && (source_card = Card[mdata[1]])
+    [source_card]
   end
 
   def sources_found_by_keyword
