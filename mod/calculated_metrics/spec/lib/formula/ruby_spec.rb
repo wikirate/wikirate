1d39c3bda2f30002a9d81351e945f25bdbd615f9
Philipp Kühl <philipp.kuehl@gmail.com>
2018-12-22 21:55:55 +0100

    fix unknown bug

--- mod/calculated_metrics/spec/lib/formula/ruby_spec.rb
+++ mod/calculated_metrics/spec/lib/formula/ruby_spec.rb
@@ -69,16 +69,16 @@ RSpec.describe Formula::Ruby do
   end
 
   describe "formula with unknown option" do
-    xspecify "Zeros with unknown and year option" do
+    specify "Zeros with unknown and year option" do
       result = calculate "Zeros[{{Joe User+RM|year:-2..0; unknown: 0}}]"
            expect(result).to include 2002 => { apple_id => 3 },
                                      2012 => { apple_id => 0 }
     end
 
-    xspecify "Total with unknown and year option" do
+    specify "Total with unknown and year option" do
       result = calculate "Total[{{Joe User+RM|year:-2..0; unknown:1}}]"
-           expect(result).to include 2002 => { apple_id => 3 },
-                                     2012 => { apple_id => 0 }
+           expect(result).to include 2002 => { apple_id => 2.0 },
+                                     2012 => { apple_id => 33.0 }
     end
   end
 
