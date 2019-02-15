7bae06174d50aaa91816c404d4c757caa6c3bf4c
Ethan McCutchen <ethan@grasscommons.org>
2019-02-15 15:51:07 +0100

    cleaner follow views and decko tick

--- mod/wikirate/set/right/follow.rb
+++ mod/wikirate/set/right/follow.rb
@@ -1,17 +1,15 @@
 format :html do
   view :title do
-    voo.hide :more_link
-    res = super()
-    res + render_more_link
+    super() + render_more_link(optional: :hide)
   end
 
   view :more_link do
     link_to_card card, "more..."
   end
 
-  view :profile, tags: :unknown_ok do
-    voo.show :more_link
-    return "" unless card.left.present? && card.left.account
-    frame { _render_following_list }
-  end
+  #  view :profile, tags: :unknown_ok do
+  #    voo.show :more_link
+  #    return "" unless card.left.present? && card.left.account
+  #    frame { _render_following_list }
+  #  end
 end
