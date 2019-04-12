936c17a940d4edd81059d0f61bf84064224a1501
Ethan McCutchen <ethan@grasscommons.org>
2019-04-12 15:11:52 -0400

    added wikirate team role codename to test seed data

--- mod/researched_metrics/spec/set/type_plus_right/metric/value_options_spec.rb
+++ mod/researched_metrics/spec/set/type_plus_right/metric/value_options_spec.rb
@@ -2,7 +2,7 @@
 
 RSpec.describe Card::Set::TypePlusRight::Metric::ValueOptions do
   it "doesn't contextualize item names" do
-    card = Card.new name: "options+value options", content: "[[A+]]\n[[+B]]"
+    card = Card.new name: "Joe User+RM+value options", content: "[[A+]]\n[[+B]]"
     expect(card.item_names).to contain_exactly "A+", "+B"
   end
 end
