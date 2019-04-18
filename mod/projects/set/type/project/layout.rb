d506e57c74ad4a4fb7e660905db520fbf4c22ee5
Ethan McCutchen <ethan@grasscommons.org>
2019-04-18 11:55:03 -0400

    fix parent project listing

--- mod/projects/set/type/project/layout.rb
+++ mod/projects/set/type/project/layout.rb
@@ -29,7 +29,7 @@ format :html do
   end
 
   def with_parent
-    parent = link_to_card(card.superproject_card)
+    parent = link_to_card(card.parent_project)
     "SUB-#{card.type.upcase} (of #{parent})"
   end
 
