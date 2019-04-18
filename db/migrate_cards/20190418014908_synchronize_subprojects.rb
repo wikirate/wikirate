1fb24ca5418cce967be5439f74521e506ab0888c
Ethan McCutchen <ethan@grasscommons.org>
2019-04-18 09:25:35 -0400

    subproject synchronization migration

--- db/migrate_cards/20190418014908_synchronize_subprojects.rb
+++ db/migrate_cards/20190418014908_synchronize_subprojects.rb
@@ -2,6 +2,12 @@
 
 class SynchronizeSubprojects < Card::Migration
   def up
-
+    Card.search(type_id: Card::ProjectID) do |project|
+      %i[metric wikirate_company].each do |trait|
+        trait_card = project.send "#{trait}_card"
+        all_items = trait_card.item_names | trait_card.subproject_item_names
+        trait_card.update_attributes! all_items.to_pointer_content
+      end
+    end
   end
 end
