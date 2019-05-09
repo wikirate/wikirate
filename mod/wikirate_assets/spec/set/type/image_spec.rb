ffd21bec1c1ea9c093fb791bad59e2a599fb1fa0
Ethan McCutchen <ethan@grasscommons.org>
2019-05-09 16:32:06 -0400

    code climate fixes

--- mod/wikirate_assets/spec/set/type/image_spec.rb
+++ mod/wikirate_assets/spec/set/type/image_spec.rb
@@ -5,15 +5,15 @@ RSpec.describe Card::Set::Type::Image do
     end
 
     it "is NOT wrapped when rendered from core" do
-      expect(new_image_view :core).not_to match(/missing-view/)
+      expect(new_image_view(:core)).not_to match(/missing-view/)
     end
 
     it "is wrapped when rendered directly" do
-      expect(new_image_view :missing).to match(/missing-view/)
+      expect(new_image_view(:missing)).to match(/missing-view/)
     end
 
     it "is wrapped when rendered from content" do
-      expect(new_image_view :content).to match(/missing-view/)
+      expect(new_image_view(:content)).to match(/missing-view/)
     end
   end
 end
