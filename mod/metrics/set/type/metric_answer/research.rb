76e8bb655d6eb0b98dd613e747b03f0d88062b75
Ethan McCutchen <ethan@grasscommons.org>
2019-06-06 13:07:55 -0400

    fix caching bug on research page

--- mod/metrics/set/type/metric_answer/research.rb
+++ mod/metrics/set/type/metric_answer/research.rb
@@ -8,7 +8,7 @@ format :html do
   # AS RESEARCH PAGE
 
   # NOCACHE because slot machine manipulates instance variables
-  view :open do
+  view :open, cache: :never do
     wrap do
       subformat(:research_page).slot_machine metric: card.metric,
                                              company: card.company,
