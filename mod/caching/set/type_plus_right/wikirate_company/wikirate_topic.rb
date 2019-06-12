bc34a47b86941f3dfcb4c234e7f59aeee1d329c8
Ethan McCutchen <ethan@grasscommons.org>
2019-06-12 11:48:48 -0400

    added filter and details cypress specs

--- mod/caching/set/type_plus_right/wikirate_company/wikirate_topic.rb
+++ mod/caching/set/type_plus_right/wikirate_company/wikirate_topic.rb
@@ -21,7 +21,7 @@ recount_trigger :type_plus_right, :metric, :wikirate_topic do |changed_card|
 end
 
 def wql_from_content
-  { type_id: WikirateTopicID, sort: :name,
+  { type_id: WikirateTopicID,
     referred_to_by: { left_id: [:in] + metric_ids,
                       right_id: WikirateTopicID } }
 end
