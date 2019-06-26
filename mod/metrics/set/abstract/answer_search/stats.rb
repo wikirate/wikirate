b1b308ea09dfd94bfeef5b9ad5871d22e0e11b01
Ethan McCutchen <ethan@grasscommons.org>
2019-06-26 16:29:18 -0400

    progress bar on company answer results

--- mod/metrics/set/abstract/answer_search/stats.rb
+++ mod/metrics/set/abstract/answer_search/stats.rb
@@ -6,21 +6,36 @@ format :html do
   LABELS = { known: "Known", unknown: "Unknown", none: "Not Researched",
              total: "Total" }.freeze
 
-  def stat_rows
-    rows = card.query.count_by_status
-    more_than_one = rows.keys.size > 1
+  def count_by_status
+    @count_by_status ||= card.query.count_by_status
+  end
+
+  def map_status skip_total=false
     LABELS.keys.map do |status|
-      next unless (count = rows[status])
-      stat_row status, count, more_than_one
+      next unless (count = count_by_status[status])
+      next if skip_total && status == :total
+      yield status, count
     end.compact
   end
 
-  def stat_row cat, count, more_than_one
+  def stat_rows
+    map_status { |status, count| stat_row status, count }
+  end
+
+  def stat_row cat, count
     cat = cat.to_sym
-    cells = more_than_one ? [{ content: operand(cat), class: "text-right" }] : []
+    cells = multicount? ? [{ content: operand(cat), class: "text-right" }] : []
     cells << { content: badge_tag(count, class: cat), class: "text-right" }
     cells << LABELS[cat]
     { content: cells, class: cat }
+  end
+
+  def total_results
+    @total_results = count_by_status[:total] || count_by_status.values.first
+  end
+
+  def multicount?
+    count_by_status.key? :total
   end
 
   def operand cat
@@ -29,5 +44,15 @@ format :html do
     when :total then "="
     else             "+"
     end
+  end
+
+  view :progress_bar do
+    sections = map_status(true) do |status, count|
+      { value: (count / total_results.to_f * 100),
+        body: count,
+        title: "#{count} #{LABELS[status]} Answers",
+        class: "progress-#{status}" }
+    end
+    progress_bar *sections
   end
 end
