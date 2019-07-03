767f7cf9b3f9705c2dc0c10e6ba68b6dfd235969
Philipp Kühl <philipp.kuehl@gmail.com>
2019-07-03 17:16:49 +0200

    Fix chunk processing bug

--- mod/calculated_metrics/chunk/formula_input.rb
+++ mod/calculated_metrics/chunk/formula_input.rb
@@ -10,7 +10,7 @@ class Card
 
         Card::Content::Chunk.register_class(
           self, prefix_re: '\\{\\{',
-                full_re:    /^\{\{([^\}]*)\}\}/,
+                full_re:    /\A\{\{([^\}]*)\}\}/,
                 idx_char:  "{"
         )
       end
