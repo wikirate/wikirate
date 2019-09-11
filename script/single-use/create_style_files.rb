377e39b338dbcf8f6b103acfe7c9831cec5d76d6
Ethan McCutchen <ethan@grasscommons.org>
2019-09-11 16:30:55 -0400

    fix popover styling

--- script/single-use/create_style_files.rb
+++ script/single-use/create_style_files.rb
@@ -1,6 +1,7 @@
 names = %w[style_top_bar style_slick style_overview_item style_company_and_topic_item
-           style_homepage_layout style_note style_user_following_list style_wikirate_bootstrap_navbar
-           style_profile_page style_company_header style_wikirate_layout style_wikirate_responsiveness
+           style_homepage_layout style_note style_user_following_list
+           style_wikirate_bootstrap_navbar style_profile_page style_company_header
+           style_wikirate_layout style_wikirate_responsiveness
            style_fakeloader style_wikirate_font_icon style_bootstrap_modal_fix]
 
 names.each do |name|
