4a8bab316e36072baf8faf89bcd884e3cae66fcb
Ethan McCutchen <ethan@grasscommons.org>
2019-11-19 15:13:18 -0500

    starting in on company/metric merging

--- mod/metrics/set/type/metric/answers.rb
+++ mod/metrics/set/type/metric/answers.rb
@@ -1,5 +1,3 @@
-# ANSWERS
-
 def researched_answers
   Answer.where metric_id: id
 end
@@ -8,112 +6,11 @@ def answer_ids
   researched_answers.pluck(:id)
 end
 
-def answer company, year
-  company = Card.fetch_id(company) unless company.is_a? Integer
-  Answer.where(metric_id: id, company_id: company, year: year.to_i).take
-end
-
-def distinct_values
-  researched_answers.select(:value).distinct.pluck(:value)
-end
-
-def companies_with_years_and_values
-  Answer.search(metric_id: id, return: [:company, :year, :value]).map do |c, y, v|
-    [c, y.to_s, v]
-  end
-end
-
-def random_value_card
+def random_answer_card
   Answer.search(metric_id: id, limit: 1).first
-end
-
-def random_valued_company_card
-  Answer.search(metric_id: id, return: :company_card, limit: 1).first
-end
-
-def metric_answer_cards cached: true
-  cached ? Answer.search(metric_id: id) : Card.search(metric_answer_query)
-end
-
-def value_cards _opts={}
-  Answer.search metric_id: id, return: :value_card
 end
 
 def metric_answer_name company, year
   Card::Name[name, Card.fetch_name(company), year.to_s]
 end
 
-def metric_answer_query
-  { left: { left_id: id }, type_id: MetricAnswerID }
-end
-
-def researchable?
-  researched? || hybrid?
-end
-
-# note: can return True for anonymous user if answer is generally researchable
-def user_can_answer?
-  return false unless researchable?
-
-  # TODO: add metric designer respresentative logic here
-  is_admin = Auth.always_ok?
-  is_owner = Auth.current.id == creator&.id
-  (is_admin || is_owner) || !designer_assessed?
-end
-
-# VALUE TYPES
-
-# Currently we have a slightly redundant value type system.
-#
-# For any given type, there is a card (with a codename) for both
-# (a) short name: the value type as it appears to users, eg Number(:number), and
-# (b) long name: the cardtype card, eg NumberValue(:number_value)
-#
-# These could and should clearly be unified.
-
-# Value Type, short name:
-# ~~~~
-
-# @return String
-def value_type
-  value_type_card&.value_type || default_value_type_code.cardname
-end
-
-# @return Symbol
-def value_type_code
-  value_type_card&.value_type_code || default_value_type_code
-end
-
-# @return Symbol
-def default_value_type_code
-  calculated? ? :number : :free_text
-end
-
-# Value Type, long name:
-# ~~~~
-# @return Symbol
-def value_cardtype_code
-  :"#{value_type_code}_value"
-end
-
-# @return Integer
-def value_cardtype_id
-  Card::Codename.id value_cardtype_code
-end
-
-def value_options
-  value_options_card.item_names
-end
-
-def numeric?
-  ten_scale? || value_type_code.in?(%i[number money])
-end
-
-# TODO: adapt to Henry's value type API
-def categorical?
-  value_type_code.in? %i[category multi_category]
-end
-
-def multi_categorical?
-  value_type_code == :multi_category
-end
--- mod/wikirate_company/set/type/wikirate_company/merge.rb
+++ mod/wikirate_company/set/type/wikirate_company/merge.rb
@@ -9,20 +9,6 @@ end
 def move_all_answers_to target_company
   researched_answers.each do |answer|
     next unless answer.real?
-    target_company = Card.cardish(target_company).name
-    target_name = Card::Name[answer.metric.to_s, target_company, answer.year.to_s]
-    move_answer answer.card, target_name
+    answer.move company: target_company
   end
-end
-
-def move_answer old_answer, new_answer_name
-  return duplicate_answer!(new_answer_name) if Card.exists? new_answer_name
-  puts "renaming #{old_answer.name} to #{new_answer_name}"
-  old_answer.update! name: new_answer_name, update_referers: true, silent_change: true
-rescue
-  puts "RENAME FAILURE: #{old_answer.name}"
-end
-
-def duplicate_answer! answer_name
-  puts "DUPLICATE: #{answer_name}"
 end
