04840fea8d652f61e914b6e6bd8baf94130d2b3f
Ethan McCutchen <ethan@grasscommons.org>
2019-12-12 11:32:56 -0500

    sdg filtering on browse metric page

--- mod/filter/set/right/browse_metric_filter.rb
+++ mod/filter/set/right/browse_metric_filter.rb
@@ -1,6 +1,10 @@
 include_set Abstract::BrowseFilterForm
 include_set Abstract::BookmarkFiltering
 
+SDG_TOPIC_IDS =
+  [1094740, 1094743, 1094744, 1094771, 1094774, 1094777, 1094780, 1094784, 1094787,
+   1099746, 1099750, 1099750, 1104669, 1104672, 1104675, 1104678, 1104681].freeze
+
 def default_filter_hash
   { name: "" }
 end
@@ -58,5 +62,14 @@ format :html do
                 return: :name,
                 sort: "name",
                 dir: order
+  end
+
+  def custom_quick_filters
+    haml :sdg_quick_filters, topic_ids: SDG_TOPIC_IDS
+  end
+
+  def sdg_help_text
+    'The Sustainable Development Goals (SDGs) are a group of 17 global goals <br/>' \
+    'conceived as a "blueprint to achieve a better and more sustainable future".'
   end
 end
