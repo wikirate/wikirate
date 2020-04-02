2b0b01ea78fa14ad92456f79bfbe86108eca7599
Ethan McCutchen <ethan@grasscommons.org>
2020-04-02 19:23:24 -0400

    fashion import fixes

--- script/csv_import/metric/import_fashion_metrics_2020.rb
+++ script/csv_import/metric/import_fashion_metrics_2020.rb
@@ -1,6 +1,6 @@
 require_relative "../../../config/environment"
 require_relative "metric_csv_row"
-import_manager_dir = "../../../vendor/card-mods/csv_import/lib/import_manager"
+import_manager_dir = "../../../vendor/card-mods/csv_import/lib"
 require_relative "#{import_manager_dir}/script_import_manager.rb"
 
 csv_path = File.expand_path "../data/fti_2020_import.csv", __FILE__
