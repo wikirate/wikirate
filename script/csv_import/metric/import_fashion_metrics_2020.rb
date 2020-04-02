bbb32c7c8819ee35022237fd3f68ada1f179f6a2
Ethan McCutchen <ethan@grasscommons.org>
2020-04-02 12:12:39 -0400

    fashion 2020 import

---
+++ script/csv_import/metric/import_fashion_metrics_2020.rb
@@ -0,0 +1,10 @@
+require_relative "../../../config/environment"
+require_relative "metric_csv_row"
+import_manager_dir = "../../../vendor/card-mods/csv_import/lib/import_manager"
+require_relative "#{import_manager_dir}/script_import_manager.rb"
+
+csv_path = File.expand_path "../data/fti_2020_import.csv", __FILE__
+
+file = CsvFile.new(csv_path, MetricCsvRow, col_sep: ",", headers: true)
+
+ScriptImportManager.new(file, user: "Fashion Revolution", error_policy: :report).import
