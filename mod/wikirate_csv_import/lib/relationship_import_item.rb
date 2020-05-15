7e92b3ecd19fd1cfe745b62febfb9158eafdaa07
Ethan McCutchen <ethan@grasscommons.org>
2020-05-15 00:27:50 -0400

    auto adding in map ui

--- mod/wikirate_csv_import/lib/relationship_import_item.rb
+++ mod/wikirate_csv_import/lib/relationship_import_item.rb
@@ -4,11 +4,11 @@
 # This class provides an interface to import relationship answers
 class RelationshipImportItem < AnswerImportItem
   @columns = { metric: { map: true },
-               subject_company: { map: true, type: :wikirate_company },
-               object_company: { map: true, type: :wikirate_company },
+               subject_company: { map: true, type: :wikirate_company, auto_add: true },
+               object_company: { map: true, type: :wikirate_company, auto_add: true },
                year: { map: true },
                value: {},
-               source: { map: true, separator: ";" },
+               source: { map: true, separator: ";", auto_add: true },
                comment: { optional: true } }
 
   def translate_company_args item
