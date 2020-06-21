529f5cd721b3fa3cf8e10328eb5b6e1a52bb6b54
Ethan McCutchen <ethan@grasscommons.org>
2020-06-20 23:53:14 -0400

    double doh

--- mod/profiles/set/type/wikirate_company/supplier_info.rb
+++ mod/profiles/set/type/wikirate_company/supplier_info.rb
@@ -7,7 +7,7 @@ end
 def supplier_info
   data = supplier_info_data
   hash = { name: name, country_name: country_name }.merge data
-  hash[:present] = supplier_data_present? hash
+  hash[:present] = supplier_data_present? data
   hash
 end
 
