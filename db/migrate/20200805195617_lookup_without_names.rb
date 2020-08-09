0aebe2ec025be30688edb417dbb877dd9fb62327
Ethan McCutchen <ethan@grasscommons.org>
2020-08-09 14:33:20 -0400

    stop lookup migrations for now

--- db/migrate/20200805195617_lookup_without_names.rb
+++ db/migrate/20200805195617_lookup_without_names.rb
@@ -1,14 +1,14 @@
 class LookupWithoutNames < ActiveRecord::Migration[6.0]
   def change
-    add_column :answers, :title_id, :integer
-
-    remove_column :answers, :metric_name
-    remove_column :answers, :designer_name
-    remove_column :answers, :title_name
-    remove_column :answers, :company_name
-    remove_column :answers, :record_name
-
-    remove_column :relationships, :subject_company_name
-    remove_column :relationships, :object_company_name
+    # add_column :answers, :title_id, :integer
+    #
+    # remove_column :answers, :metric_name
+    # remove_column :answers, :designer_name
+    # remove_column :answers, :title_name
+    # remove_column :answers, :company_name
+    # remove_column :answers, :record_name
+    #
+    # remove_column :relationships, :subject_company_name
+    # remove_column :relationships, :object_company_name
   end
 end
