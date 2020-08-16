2ce7ca347fd33b1aa5e1cd7a640582eb862fce13
Ethan McCutchen <ethan@grasscommons.org>
2020-08-15 20:44:58 -0400

    metric spec fix

--- script/single-use/remove_left_right_duplicates.rb
+++ script/single-use/remove_left_right_duplicates.rb
@@ -1,13 +1,36 @@
-#require File.expand_path "../../../config/environment", __FILE__
+require File.expand_path "../../../config/environment", __FILE__
 
-# Card::Auth.signin "Ethan McCutchen"
+Card::Auth.signin "Ethan McCutchen"
+require "csv"
 
 FILENAME = File.expand_path "data/left_right_duplicates.csv"
 
 # include Card::Model::SaveHelper
 
+def csv
+  CSV.new raw, headers: :true
+end
+
 def raw
   File.read FILENAME
 end
 
-puts raw
+def fetch id
+  Card.fetch id.to_i, look_in_trash: true
+end
+
+def trash_test card
+  puts "#{card.name} is in trash" if card.trash
+
+end
+
+csv.each do |r|
+  c1 = fetch r["id1"]
+  c2 = fetch r["id2"]
+
+  trash_test c1
+  trash_test c2
+end
+
+
+puts "done."
