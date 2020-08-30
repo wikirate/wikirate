81ebfbdc2213aafc2147718ae446c9cea5550ff8
Ethan McCutchen <ethan@grasscommons.org>
2020-08-30 11:54:18 -0400

    silly code cleanup in single-use script

--- script/single-use/award_badges.rb
+++ script/single-use/award_badges.rb
@@ -30,18 +30,17 @@ class AwardBadges
     def award_create_badges type_code, opts={}
       puts "create badges for #{type_code}"
 
-      type_id = Card::Codename.id type_code
-      where ||= opts[:where] || "type_id = #{type_id}"
-      where = "WHERE (#{where})" unless where.empty?
-      min = min ? "HAVING COUNT(*) > #{min}" : ""
-      from ||= opts[:from] || "cards"
-      query(
-        "SELECT creator_id, COUNT(*) FROM #{from} #{where} GROUP BY creator_id "\
-      "#{min}"
-      ).each do |user_id, count|
+      query(count_statement(type_code, opts[:where], opts[:from])).each do |user_id, count|
         next unless user_id
         award_badges_if_earned! count, user_id, type_code, opts[:affinity]
       end
+    end
+
+    def count_statement type_code, where, from
+      where ||= opts[:where] || "type_id = #{Card::Codename.id type_code}"
+      where = "WHERE (#{where})" unless where.empty?
+      from ||= "cards"
+      "SELECT creator_id, COUNT(*) FROM #{from} #{where} GROUP BY creator_id "
     end
 
     def award_badges_by_user
@@ -65,24 +64,14 @@ class AwardBadges
 
     def award_affinity_answer_badges affinity_type
       return award_project_affinity_answer_badges if affinity_type == :project
-      badge_line = Card::Set::Type::MetricAnswer::BadgeSquad
-                   .badge_line(:create, affinity_type)
-      badge_levels = [:bronze, :silver, :gold].map do |level, _h|
-        [badge_line.badge(level).threshold, badge_line.badge(level).name]
-      end
-      min_thresh = badge_line.threshold(:bronze)
+      badge_line = badge_line affinity_type
+      badge_levels = badge_levels badge_line
+      min_thresh = badge_line.threshold :bronze
 
       user_ids.each do |user_id|
-        affinity_names =
-          query("SELECT #{affinity_type}_name, COUNT(*) FROM answers "\
-                "WHERE creator_id = #{user_id} "\
-                "GROUP BY #{affinity_type}_id HAVING COUNT(*) >= #{min_thresh}")
-
-        badge_levels.each do |threshold, name|
-          badge_names = affinity_names.map do |an, count|
-            next if count < threshold
-            "#{an}+#{name}+#{affinity_type} badge"
-          end.compact
+        affinities = answer_affinities_for_user affinity_type, user_id, min_thresh
+        each_answer_badge_level
+        each_affinity_badge affinities, affinity_type, badge_levels do |badge_names|
           award_badges! user_id, :metric_answer, badge_names, true
         end
       end
@@ -95,18 +84,52 @@ class AwardBadges
       # end
     end
 
+    def each_affinity_badge affinities, affinity_type, badge_levels
+      badge_levels.each do |threshold, name|
+        badge_names = affinities.map do |an, count|
+          next if count < threshold
+          "#{an}+#{name}+#{affinity_type} badge"
+        end.compact
+        yield badge_names
+      end
+    end
+
+    def badge_line affinity_type
+      Card::Set::Type::MetricAnswer::BadgeSquad.badge_line :create, affinity_type
+    end
+
+    def badge_levels badge_line
+      [:bronze, :silver, :gold].map do |level|
+        [badge_line.badge(level).threshold, badge_line.badge(level).name]
+      end
+    end
+
+    def answer_affinities_for_user affinity_type, user_id, min_thresh
+      query("SELECT #{affinity_type}_name, COUNT(*) FROM answers "\
+                "WHERE creator_id = #{user_id} "\
+                "GROUP BY #{affinity_type}_id HAVING COUNT(*) >= #{min_thresh}")
+    end
+
     def award_project_affinity_answer_badges
       Card.search(type_id: Card::ProjectID).each do |project|
-        next if project.company_ids.empty? || project.metric_ids.empty?
-        query("SELECT creator_id, COUNT(*) FROM answers "\
-      "WHERE company_id IN (#{project.company_ids.join(',')}) AND"\
-      "       metric_id IN (#{project.metric_ids.join(',')})"\
-      "GROUP BY creator_id").each do |user_id, count|
+        next unless (statement = project_affinity_answer_badges_statement project)
+        query(statement).each do |user_id, count|
           next unless user_id
           award_affinity_answer_badges_if_earned! count, user_id,
                                                   :project, project.name
         end
       end
+    end
+
+    def project_affinity_answer_badges_statement project
+      company_ids = project.company_ids.join ","
+      metric_ids = project.metric_ids.join ","
+      return unless company_ids.present? && metric_ids.present?
+
+      "SELECT creator_id, COUNT(*) FROM answers "\
+      "WHERE company_id IN (#{company_ids}) AND"\
+      "       metric_id IN (#{metric_ids}) "\
+      "GROUP BY creator_id"
     end
 
     def award_affinity_answer_badges_if_earned! count, user_id,
