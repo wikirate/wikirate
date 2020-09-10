1cdf6ae2753e0191ca10bb893f7f258bbd99b90c
Ethan McCutchen <ethan@grasscommons.org>
2020-09-10 17:09:17 -0400

    spec cleanup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 require "decko/mods_spec_helper"
 require_relative "source_helper"
+require_relative "answer_helper"
 
 Spork.prefork do
   RSpec.configure do |config|
@@ -12,59 +13,8 @@ Spork.prefork do
   end
 end
 
-include SourceHelper
-include SharedData::Samples
+RSpec::Core::ExampleGroup.include SourceHelper
+RSpec::Core::ExampleGroup.include AnswerHelper
+RSpec::Core::ExampleGroup.include SharedData::Samples
+RSpec::Core::ExampleGroup.extend SharedData::Samples::ClassMethods
 
-def create_answer args
-  with_user(args.delete(:user) || "Joe User") do
-    Card.create answer_args(args)
-  end
-end
-
-def build_answer args
-  Card.new answer_args(args)
-end
-
-def answer_args(metric: sample_metric.name,
-                company: sample_company.name,
-                year: "2015",
-                value: "sample value",
-                source: sample_source.name)
-  { type_id: Card::MetricAnswerID,
-    "+metric" => metric,
-    "+company" => company,
-    "+value" => value,
-    "+year" => year,
-    "+source" => source
-  }
-end
-
-# Usage:
-# create_metric type: :researched do
-#   Siemens 2015: 4, 2014: 3
-#   Apple   2105: 7
-# end
-def create_metric opts={}, &block
-  Card::Auth.as_bot do
-    if opts[:name] && opts[:name].to_name.parts.size == 1
-      opts[:name] = "#{Card::Auth.current.name}+#{opts[:name]}"
-    end
-    opts[:name] ||= "TestDesigner+TestMetric"
-    Card::Metric.create opts, &block
-  end
-end
-
-def have_badge_count num, klass, label
-  have_tag "span.#{klass}" do
-    with_tag "span.badge", text: /#{num}/
-    with_tag "label", text: /#{label}/
-  end
-end
-
-def html_trim str
-  s = str.dup
-  s.delete!("\r\n")
-  s.delete!("\n")
-  s.delete!("  ")
-  s
-end
