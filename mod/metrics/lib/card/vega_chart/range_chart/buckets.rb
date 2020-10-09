eb8c1091f9d84c40dfd6104c30691cd067ac61bf
Ethan McCutchen <ethan@decko.org>
2020-10-08 23:56:43 -0400

    run specs in mods

--- mod/metrics/lib/card/vega_chart/range_chart/buckets.rb
+++ mod/metrics/lib/card/vega_chart/range_chart/buckets.rb
@@ -36,7 +36,7 @@ class Card
         # end
 
         def bucket_size
-          @bucket_size ||= nice(raw_bucket_size) { |b| b * 1.05 }
+          @bucket_size ||= nice(raw_bucket_size) { |b| b * 1.03 }
         end
 
         def raw_bucket_size
