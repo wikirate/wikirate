a056c1d69df2cf23057334c585a4b975a88e9510
Ethan McCutchen <ethan@decko.org>
2020-10-08 20:44:50 -0400

    code :rainbow: :zap:

--- initializers/delayed_job_web.rb
+++ initializers/delayed_job_web.rb
@@ -2,7 +2,7 @@
 # shared/config.  So long as that is the case, any changes here will need
 # to be copied over manually to shared/config.
 
-require 'delayed_job_web'
+require "delayed_job_web"
 
 DelayedJobWeb.use Rack::Auth::Basic do |email, password|
   account = Card::Auth.authenticate email, password
