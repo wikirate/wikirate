44e119cc5012643d5274e2d67527b7acd1a47015
Ethan McCutchen <ethan@decko.org>
2020-11-30 19:42:23 -0500

    hooking up timeline charts

--- mod/vega/lib/card/vega_chart/grid.rb
+++ mod/vega/lib/card/vega_chart/grid.rb
@@ -1,17 +1,38 @@
 class Card
   class VegaChart
-    # Grid of answer values
+    # Metric/Company Grid of answer values
     class Grid < VegaChart
+
+      def initialize format, opts
+        @invert = opts[:invert] = true
+        super
+      end
+
       def hash
-        with_values(company_list: 0, metric_list: 1, answer_list: 2) do
+        with_values(company_list: 0, metric_list: 1, answer_list: 4) do
           super.tap do |h|
             h[:scales] << builtin(:ten_scale_color)
+            invert h
           end
         end
       end
 
       def layout
         super.merge builtin(:grid)
+      end
+
+      private
+
+      def invert hash
+        return unless @invert
+        hash[:signals].first[:value] = "companies"
+        map_data hash, 2, :companies
+        map_data hash, 3, :metrics
+        hash
+      end
+
+      def map_data hash, index, set
+        hash[:data][index][:source] = set
       end
     end
   end
---
+++ mod/vega/lib/card/vega_chart/timeline.rb
@@ -0,0 +1,24 @@
+class Card
+  class VegaChart
+    # Timeline of answer values
+    class Timeline < VegaChart
+      include Helper::Axes
+
+      def hash
+        with_values(year_list: 0) { super }
+      end
+
+      def layout
+        super.merge builtin(:timeline)
+      end
+
+      def x_axis
+        super.merge offset: 10
+      end
+
+      def y_axis
+        super.merge tickMinStep: 1
+      end
+    end
+  end
+end
