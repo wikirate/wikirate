a5552cd46a654c1992d56f420c131e5f93c6eae2
Ethan McCutchen <ethan@decko.org>
2020-12-02 14:32:36 -0500

    beginning of the life of pie chart

---
+++ mod/vega/lib/card/vega_chart/pie.rb
@@ -0,0 +1,32 @@
+class Card
+  class VegaChart
+    # Timeline of answer values
+    class Pie < VegaChart
+      include Helper::Axes
+
+      def hash
+        with_values(year_list: 0) { super }
+      end
+
+      def layout
+        super.merge builtin(:pie)
+      end
+
+      def x_axis
+        super.tap do |h|
+          h[:offset] = 10
+          h[:encode][:labels][:update].merge!(
+            fontWeight: [{ test: "datum.value == tooltip.year", value: 900 },
+                         { "value": 400 }],
+            fill: [{ test: "datum.value == tooltip.year", value:"#000" },
+                   { value: "#888" }]
+          )
+        end
+      end
+
+      def y_axis
+        super.merge tickMinStep: 1
+      end
+    end
+  end
+end
