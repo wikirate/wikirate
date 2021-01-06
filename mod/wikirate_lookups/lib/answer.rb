c87744dfacfa56ad476bbb2fd765c42ae1167b34
Ethan McCutchen <ethan@decko.org>
2021-01-05 22:58:33 -0500

    missing comments in answer fetchers

--- mod/wikirate_lookups/lib/answer.rb
+++ mod/wikirate_lookups/lib/answer.rb
@@ -22,7 +22,8 @@ class Answer < ApplicationRecord
   after_destroy :latest_to_true
 
   fetcher :metric_id, :company_id, :record_id, :source_count, :source_url, :imported,
-          :value, :numeric_value, :checkers, :check_requester, :overridden_value
+          :value, :numeric_value, :checkers, :check_requester, :overridden_value,
+          :comments
 
   def card
     return @card if @card
