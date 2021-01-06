7dc70674d49515906fd31c24c9676cf0669622f3
Ethan McCutchen <ethan@decko.org>
2021-01-05 19:27:09 -0500

    missing comma in answer fetchers

--- mod/wikirate_lookups/lib/answer.rb
+++ mod/wikirate_lookups/lib/answer.rb
@@ -21,7 +21,7 @@ class Answer < ApplicationRecord
 
   after_destroy :latest_to_true
 
-  fetcher :metric_id, :company_id, :record_id, :source_count, :source_url, :imported
+  fetcher :metric_id, :company_id, :record_id, :source_count, :source_url, :imported,
           :value, :numeric_value, :checkers, :check_requester
 
   def card
