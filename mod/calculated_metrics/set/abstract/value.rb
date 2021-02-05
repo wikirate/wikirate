40ba31c380c19135b2df76ef31deaacb63c8bc42
Ethan McCutchen <ethan@decko.org>
2021-02-05 17:16:32 -0500

    restore update_related_calculations

--- mod/calculated_metrics/set/abstract/value.rb
+++ mod/calculated_metrics/set/abstract/value.rb
@@ -1,7 +1,6 @@
 event :update_related_calculations, :after_integrate, skip: :allowed do
   ensure_metric(metric_card).each_depender_metric do |metric|
-    metric.update_value_for! company: company_id # , year: year
-    # I commented out year so that this will not break when year is specified in the
-    # formula. But this means every year is recalculated.
+    metric.update_value_for! company: company_id, year: year
+    # FIXME: this will break when year is specified in the formula.
   end
 end
