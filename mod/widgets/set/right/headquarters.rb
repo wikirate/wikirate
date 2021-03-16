35a6f50ea406bac9a4bc89d1edadcff4ed2477ad
Ethan McCutchen <ethan@decko.org>
2021-03-16 10:13:22 -0400

    stop running MappingApi calls

--- mod/widgets/set/right/headquarters.rb
+++ mod/widgets/set/right/headquarters.rb
@@ -12,13 +12,16 @@ end
 
 # if we're assuming left is a company, this should arguably be in a type_plus_right set
 def needs_oc_mapping?
+  return false
+  # Not doing mapping for now
+  # We realized the old CERTH server was not actually handling these requests any more.
+
   (l = left) && l.open_corporates.blank?
 end
 
 event :update_oc_mapping_due_to_headquarters_entry, :integrate,
-      on: :save, when: :needs_oc_mapping?, trigger: :required do
-      # This was previously "skip: :allowed", but then we realized the old CERTH
-      # server was not actually handling these requests any more.
+      on: :save, when: :needs_oc_mapping?, skip: :allowed do
+
   prefixed_oc_code = oc_code.start_with?("oc_") ? oc_code : "oc_#{oc_code}"
   oc = ::OpenCorporates::MappingApi
        .fetch_oc_company_number company_name: name.left,
