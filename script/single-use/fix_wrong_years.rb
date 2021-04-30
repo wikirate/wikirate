d94eabee27a3e7a2abd0008089bce21b38e4e4a2
Ethan McCutchen <ethan@decko.org>
2021-04-30 16:41:24 -0400

    further answer fixer fixing

--- script/single-use/fix_wrong_years.rb
+++ script/single-use/fix_wrong_years.rb
@@ -6,48 +6,51 @@ require "csv"
 
 FILENAME = File.expand_path "script/single-use/data/wrong_year.csv"
 
-def csv
-  CSV.new raw, headers: true
+module AnswerFixer
+  class << self
+    def csv
+      CSV.new raw, headers: true
+    end
+
+    def raw
+      File.read FILENAME
+    end
+
+    def card_ok id
+      card = Card[id.to_i]
+      return card if card
+
+      puts "could not find card with id #{id}"
+      false
+    end
+
+    def year_ok year, card
+      return year unless year == card.year
+
+      puts "year is already correct: #{card.name}"
+      false
+    end
+
+    def name_ok year, card
+      new_name = Card::Name[card.name.left, year]
+      return new_name unless Card[new_name]
+
+      puts "card already exists: #{new_name}"
+      false
+    end
+
+    def run!
+      csv.each do |r|
+        next unless
+          (card = card_ok r["Answer ID"]) &&
+          (year = year_ok r["Correct Year"], card) &&
+          (name = name_ok year, card)
+        puts "renaming: #{card.name}\n      to: #{name}"
+        card.update! name: name
+      end
+    end
+  end
 end
 
-def raw
-  File.read FILENAME
-end
-
-def fetch id
-  Card.fetch id.to_i
-end
-
-def card_ok id
-  card = Card[id]
-  return card if card
-
-  puts "could not find card with id #{id}"
-  false
-end
-
-def year_ok year, card
-  return year unless year == card.year
-
-  puts "year is already correct: #{card.name}"
-  false
-end
-
-def name_ok year, card
-  new_name = Card::Name[card.name.left, year]
-  return new_name unless Card[new_name]
-
-  puts "card already exists: #{new_name}"
-  false
-end
-
-csv.each do |r|
-  next unless
-    (card = card_ok r["Answer ID"]) &&
-    (year = year_ok r["Correct Year"], card) &&
-    (name = name_ok year, card)
-  puts "renaming: #{card.name}\n     to: #{name}"
-  card.update! name: name
-end
-
+AnswerFixer.run!
 puts "done."
