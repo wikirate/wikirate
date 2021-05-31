94c5fcb5b0ff1700eef085b0c98a33b748769aab
Ethan McCutchen <ethan@decko.org>
2021-05-31 17:08:13 -0400

    add unpublished flag to metrics table

---
+++ mod/metrics/set/abstract/publishable.rb
@@ -0,0 +1,18 @@
+card_accessor :unpublished
+
+# TODO: this should work without explicit trash handling.
+def unpublished
+  return false if unpublished_card.trash
+
+  unpublished_card.content == "1"
+end
+
+def check_published
+  return true unless unpublished && !steward?
+
+  deny_because "not yet published"
+end
+
+def ok_to_read
+  super && check_published
+end
