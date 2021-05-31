06195edeeb2f4ddbc1e16ef3f3ec6397dcf5cb22
Ethan McCutchen <ethan@decko.org>
2021-05-31 16:04:20 -0400

    spent WAY too long figuring out cypress configuration error

--- config/initializers/cypress_dev.rb
+++ config/initializers/cypress_dev.rb
@@ -1,6 +1,6 @@
 if defined?(CypressDev)
   CypressDev.configure do |c|
-    c.cypress_folder = File.join Decko.gem_root, "decko/spec/cypress"
+    c.cypress_folder = File.join Decko.gem_root, "spec/cypress"
     c.use_middleware = Rails.env.test? || ENV["CYPRESS_DEV"]
     c.logger = Rails.logger
   end
