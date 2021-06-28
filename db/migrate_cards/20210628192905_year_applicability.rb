b225ddc8510958076573974d0326b991c532f912
Ethan McCutchen <ethan@decko.org>
2021-06-28 16:59:43 -0400

    seeded with applicability migration

---
+++ db/migrate_cards/20210628192905_year_applicability.rb
@@ -0,0 +1,22 @@
+# -*- encoding : utf-8 -*-
+
+class YearApplicability < Cardio::Migration
+  def up
+    clean_up_list_card
+
+    ensure_card %i[metric year type_plus_right default], content: :list.cardname
+    ensure_card %i[metric year type_plus_right input_type], content: "multiselect"
+
+    ensure_card %i[company_group year type_plus_right default], content: :list.cardname
+    ensure_card %i[company_group right content_options],
+                content: '{"type":"Company Group","sort":"name","dir":"desc","limit":"0"}'
+  end
+
+  def clean_up_list_card
+    card_named_list = Card["List"]
+    return if card_named_list&.codename == :list
+
+    card_named_list.delete!
+    Card[:list].update! name: "List"
+  end
+end
