bd47ac9e4fc1fd26daeec91b5cc92e115125b435
Ethan McCutchen <ethan@decko.org>
2021-06-29 16:32:22 -0400

    remove applicability update conditions that don't consider false negatives.

--- mod/calculated_metrics/set/type_plus_right/metric/company_group.rb
+++ mod/calculated_metrics/set/type_plus_right/metric/company_group.rb
@@ -2,11 +2,12 @@ include_set Abstract::MetricChild, generation: 1
 
 event :enforce_company_applicability_to_calculations, :after_integrate, skip: :allowed do
   return if metric_card.action.in?(%i[create delete]) || !metric_card.calculated?
-  return unless wrong_companied_answers?
+  # return unless wrong_companied_answers?
 
   metric_card.deep_answer_update
 end
 
+# this finds false positives but not false negatives
 def wrong_companied_answers?
   metric_card.answers.where("company_id not in (#{company_ids.join ', '})").any?
 end
