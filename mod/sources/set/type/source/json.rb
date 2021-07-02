f99c417b834dffcb7cea82a964845f07fc6efbeb
Ethan McCutchen <ethan@decko.org>
2021-07-02 11:58:02 -0400

    added sources to relationship answer json

--- mod/sources/set/type/source/json.rb
+++ mod/sources/set/type/source/json.rb
@@ -1,16 +1,22 @@
 format :json do
   def atom
-    nucleus.merge fields_with_view(:content)
+    nucleus.merge fields_with_view(atom_fields, :content)
   end
 
   def molecule
-    super.merge fields_with_view(:atom)
+    super.merge fields_with_view(molecule_fields, :atom)
   end
 
-  def fields_with_view view
-    %i[file wikirate_link report_type title wikirate_company year description discussion
-       metric metric_answer].each_with_object({}) do |codename, hash|
+  def atom_fields
+    %i[file wikirate_link report_type wikirate_title year]
+  end
 
+  def molecule_fields
+    atom_fields + %i[description discussion wikirate_company metric metric_answer]
+  end
+
+  def fields_with_view fields, view
+    fields.each_with_object({}) do |codename, hash|
       key = codename.cardname.downcase.tr(" ", "_").to_sym
       hash[key] = field_nest codename, view: view
     end
