1a8a7f97c8acfbfe85dd1bbca6e8b6ae6bcd2b03
Ethan McCutchen <ethan@decko.org>
2021-07-13 11:43:02 -0400

    restore old fashionchecker API (for now)

--- mod/profiles/set/self/wikirate_company/brands.rb
+++ mod/profiles/set/self/wikirate_company/brands.rb
@@ -56,4 +56,51 @@ format :json do
   def name_query
     Env.params[:q] if Env.params[:q].present?
   end
+
+
+  view :search_brands, cache: :never do
+    keyword ? search_with_keyword : "{}"
+  end
+
+  def search_with_keyword
+    ids = search_by_company_name
+    company_traits(ids).sort_by { |a| a[:name] }
+                       .to_json
+  end
+
+  def country_search
+    return [].to_json unless region_id
+    company_traits(brand_ids).to_json
+  end
+
+  # TODO: if the data grows we might want sql sorting instead of ruby sorting
+  # but that involves serious SQL work because we have to join the queries of
+  # search_by_company_name and search_by_address
+  def company_traits ids
+    return [] unless ids.present?
+    ids.map do |id|
+      company_name = Card.fetch_name(id)
+      { name: company_name, id: id, url_key: company_name.url_key }
+    end
+  end
+
+  def search_by_company_name
+    return [] unless brand_ids.present?
+    Card.search type_id: Card::WikirateCompanyID,
+                name: ["match", keyword],
+                id: ["in"].concat(brand_ids),
+                return: :id
+  end
+
+  def brand_ids
+    @brand_ids ||= search_brand_ids
+  end
+
+  def search_brand_ids
+    ["Google Inc".card_id]
+  end
+
+  def keyword
+    params[:keyword] if params[:keyword].present?
+  end
 end
