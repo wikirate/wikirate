8181ea891b538bdfe9d82a5de520951990109670
Ethan McCutchen <ethan@decko.org>
2021-07-30 15:47:03 -0400

    fix wolfram instant api generation

--- script/wolfram_instant_api.rb
+++ script/wolfram_instant_api.rb
@@ -5,7 +5,8 @@
 # 1. we run this script on a copy of the site with current geo data (using bundle exec)
 #
 # 2. we cut and paste the API string into Wolfram Cloud:
-#    https://www.wolframcloud.com/env/philipp.kuehl/wikirate.nb
+#    https://www.wolframcloud.com/
+#    (click new Notebook)
 #
 # 3. we update the server configuration with the new object identification:
 #     config.wolfram_api_key = "adfsadf-asdfsad-asdfsa-dfsdfds"
@@ -17,20 +18,22 @@ require File.expand_path "../../config/environment", __FILE__
 Card::Auth.signin "Ethan McCutchen"
 
 def region_fields field
-  Card::Set::Self::Region.region_fields(field).map do |card|
+  Card::Region.field_cards(field).map do |card|
     yield card
   end
 end
 
 def region_association field
-  key_vals = region_fields(field) { |f| %("#{f.name.left}" ->  "#{f.content}") }
+  key_vals = region_fields(field) do |f|
+    %("#{f.name.left}" ->  "#{f.content.gsub(/[\[\]]/, '')}")
+  end
   "<| #{key_vals.join ', '} |>"
 end
 
 puts <<~WOLFRAM
 
-  ILORegion = #{region_association 'ILO Region'};
-  Country = #{region_association 'Country'};
+  ILORegion = #{region_association :ilo_region};
+  Country = #{region_association :country};
   Zeros = Function[a, Count[a, 0]];
   Unknowns = Function[a, Count[a, "Unknown"]];
 
