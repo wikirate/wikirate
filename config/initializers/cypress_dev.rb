403836e5740c13a2cfd409b02cf3048506871cc8
Ethan McCutchen <ethan@decko.org>
2021-08-05 14:30:10 -0400

    new init dirs

--- config/initializers/cypress_dev.rb
+++
@@ -1,7 +0,0 @@
-# if defined?(CypressDev)
-#   CypressDev.configure do |c|
-#     c.cypress_folder = File.join Decko.gem_root, "spec/cypress"
-#     c.use_middleware = Rails.env.cypress?
-#     c.logger = Rails.logger
-#   end
-# end
