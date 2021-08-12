8af652dd11c6c67197dcabc9243bf85d30f0280f
Ethan McCutchen <ethan@decko.org>
2021-08-12 16:18:35 -0400

    still trying to get coffeescript working for industry

--- mod/calculated_metrics/set/type_plus_right/metric/formula/input.rb
+++ mod/calculated_metrics/set/type_plus_right/metric/formula/input.rb
@@ -7,7 +7,7 @@ def clean_formula
 end
 
 def standard_formula
-  content.gsub(/[\r\n]+/m, "")
+  calculator_class == Formula::JavaScript ? content : content.gsub(/[\r\n]+/m, "")
 end
 
 def parser
--- mod/isic/set/self/commons_industry.rb
+++ mod/isic/set/self/commons_industry.rb
@@ -1,8 +1,3 @@
 def calculator_class
   Formula::JavaScript
 end
-
-# don't get rid of line breaks
-def standard_formula
-  content
-end
