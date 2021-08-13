07adbc7167c09b0107a3639b5ecc2b1faeaabe97
Ethan McCutchen <ethan@decko.org>
2021-08-13 14:13:11 -0400

    simplify bar_graph improvements (move more invariable code to json)

--- mod/vega/lib/card/vega_chart/bar_graph.rb
+++ mod/vega/lib/card/vega_chart/bar_graph.rb
@@ -8,8 +8,7 @@ class Card
         with_values(answer_list: 0) do
           super.tap do |h|
             transform_multi_values h[:data]
-            insert_value_options_map h[:data]
-            lookup_option_labels h[:data]
+            translate_value_options h[:data]
           end
         end
       end
@@ -25,28 +24,9 @@ class Card
         ]
       end
 
-      def insert_value_options_map data
-        data.insert 1, name: "options", url: value_options_url
-      end
-
-      def value_options_url
-        metric_card.value_options_card.format(:json)
-                   .path view: :option_list, format: :json
-      end
-
-      def lookup_option_labels data
-        # this is rawCounts
-        # TODO: lookup dataset by name
-        # don't rely on index staying the same!
-        # should add support library
-        data[2][:transform] << {
-          type: "lookup",
-          from: "options",
-          key: "key",
-          fields: ["value"],
-          as: ["label"],
-          values: ["name"]
-        }
+      def translate_value_options data
+        data[1][:url] = metric_card.value_options_card.format(:json)
+                                   .path view: :option_list, format: :json
       end
 
       def layout
