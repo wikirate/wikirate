0172fc760d6670cbe9d5c087a57d16036c4ce8c6
Ethan McCutchen <ethan@decko.org>
2021-08-13 19:03:21 -0400

    javascript calculator code comments

--- mod/calculated_metrics/lib/formula/java_script.rb
+++ mod/calculated_metrics/lib/formula/java_script.rb
@@ -35,6 +35,7 @@ module Formula
       "#{year}-#{company_id}"
     end
 
+    # all inputs in the form of { year-company_id => values }
     def input_hash
       {}.tap do |hash|
         each_input do |values, company_id, year|
@@ -43,12 +44,19 @@ module Formula
       end
     end
 
+    # adds spaces before each coffeescript line so the initial indentation is correct
     def prepended_coffee_formula
       coffee_formula.split(/[\r\n]+/).map { |l| "  #{l}" }.join "\n"
     end
 
+    # replaces nests with inputs (which are actually array lookups, eg iN[0])
     def coffee_formula
       replace_nests { |index| input_name index }
+    end
+
+    # just weird enough that users aren't likely to use it...
+    def input_name index
+      "iN[#{index}]"
     end
 
     # def prepare_values values
@@ -72,15 +80,10 @@ module Formula
     #   end
     # end
 
-    def numeric? index
-      @numeric ||= {}
-      return @numeric[index] unless @numeric[index].nil?
-      @numeric[index] = input.type(index).in? %i[number yearly_value]
-    end
-
-    # just weird enough that users aren't likely to use it...
-    def input_name index
-      "iN[#{index}]"
-    end
+    # def numeric? index
+    #   @numeric ||= {}
+    #   return @numeric[index] unless @numeric[index].nil?
+    #   @numeric[index] = input.type(index).in? %i[number yearly_value]
+    # end
   end
 end
