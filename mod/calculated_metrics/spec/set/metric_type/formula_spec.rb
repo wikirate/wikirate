b1db6b473ee5f58ebbc451366f2421bc76fd7d34
Ethan McCutchen <ethan@decko.org>
2021-08-16 19:43:15 -0400

    climate changes

--- mod/calculated_metrics/spec/set/metric_type/formula_spec.rb
+++ mod/calculated_metrics/spec/set/metric_type/formula_spec.rb
@@ -5,10 +5,10 @@ require_relative "../../../spec/support/formula.rb"
 RSpec.describe Card::Set::MetricType::Formula do
   include_context "formula"
 
-  let (:metric_name) { "Joe User+RM"}
-  let (:metric_name1) { "Joe User+researched number 1" }
-  let (:metric_name2) { "Joe User+researched number 2" }
-  let (:metric_name3) { "Joe User+researched number 3" }
+  let(:metric_name) { "Joe User+RM" }
+  let(:metric_name1) { "Joe User+researched number 1" }
+  let(:metric_name2) { "Joe User+researched number 2" }
+  let(:metric_name3) { "Joe User+researched number 3" }
 
   def build_formula formula
     format formula, metric_name1, metric_name2, metric_name3
