360ad662f431fe0895f40f2ed6c285487d55a407
Ethan McCutchen <ethan@decko.org>
2021-08-26 17:09:16 -0400

    wagn reference clean up / remigrated with codename for wikirate_layout

--- mod/guides/db/migrate_cards/20210826200444_add_guide_types.rb
+++ mod/guides/db/migrate_cards/20210826200444_add_guide_types.rb
@@ -5,5 +5,6 @@ class AddGuideTypes < Cardio::Migration
     ensure_card "Guide", type: :cardtype, codename: :guide_type
     ensure_card "Reference", type: :cardtype, codename: :reference
     ensure_card "Guide Layout", type: :layout_type, codename: :guide_layout
+    ensure_card "Wikirate Layout", type: :layout, codename: :wikirate_layout
   end
 end
