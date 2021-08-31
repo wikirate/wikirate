90a74c3dabe77373d50fb946f20cdb7d5c5236bc
Ethan McCutchen <ethan@decko.org>
2021-08-30 21:58:35 -0400

    guide sidebar mostly working

--- mod/guides/set/all/guide_views.rb
+++ mod/guides/set/all/guide_views.rb
@@ -4,4 +4,25 @@ format :html do
       layout_nest
     end
   end
+
+  view :content_with_anchored_headers do
+    wrap { [render_menu, render_core_with_anchored_headers] }
+  end
+
+  view :core_with_anchored_headers, cache: :never do
+    # start new format so we get fully rendered html (no nest stubs)
+    n = Nokogiri::HTML render_core
+    n.css("h1, h2, h3, h4, h5, h6").each { |h| h.wrap header_anchor(h) }
+    n.to_html
+  end
+
+  private
+
+  def header_anchor header
+    "<a class='header-anchor' name='#{header.text.to_name.key}'></a>"
+  end
+
+  view :infobox do
+    wrap { wrap_with("div", class: "wr-infobox") { render_core } }
+  end
 end
--- mod/guides/set/type/guide_type.rb
+++ mod/guides/set/type/guide_type.rb
@@ -1,13 +1,12 @@
+card_accessor :body
+
 format :html do
   def layout_name_from_rule
     :guide_layout
   end
 
   before :content_formgroups do
-    voo.edit_structure = [
-      :description,
-      :body
-    ]
+    voo.edit_structure = %i[description body]
   end
 
   view :box_middle do
@@ -19,5 +18,17 @@ format :html do
   end
 
   view :guide_page, template: :haml, wrap: :slot
-  view :sidebar_nav, template: :haml
+  view :sidebar_nav, template: :haml, cache: :never
+  view :guide_paging do
+    "hello world"
+  end
+
+  def expanded?
+    parent&.parent&.card == card
+  end
+
+  def subheaders
+    # start new format so we get fully rendered html (no nest stubs)
+    Nokogiri::HTML(card.body_card.format.render_core).css("h2").sort
+  end
 end
