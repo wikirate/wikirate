d53a57a3e4462f611ad73004617b4ae3a95702db
Ethan McCutchen <ethan@decko.org>
2021-09-01 21:39:24 -0400

    project -> dataset fixes to get specs running

--- mod/wikirate_caching/spec/set/type_plus_right/dataset/data_subset_spec.rb
+++ mod/wikirate_caching/spec/set/type_plus_right/dataset/data_subset_spec.rb
@@ -1,6 +1,6 @@
 # -*- encoding : utf-8 -*-
 
-RSpec.describe Card::Set::TypePlusRight::Project::Subproject do
+RSpec.describe Card::Set::TypePlusRight::Dataset::DataSubset do
   describe "subprojects" do
     def create_subproject subfields={}
       subfields.reverse_merge! parent: "Evil Project"
