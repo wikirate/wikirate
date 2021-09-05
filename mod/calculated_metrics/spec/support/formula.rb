6729e2272d2a0bacd050d67fed4a7489d2016c0c
Ethan McCutchen <ethan@decko.org>
2021-09-04 21:50:58 -0400

    wikirate_table with fewer arguments

--- mod/calculated_metrics/spec/support/formula.rb
+++ mod/calculated_metrics/spec/support/formula.rb
@@ -1,14 +1,15 @@
 shared_context "formula" do
   def formula_str options: nil, method: nil, year: nil, unknown: nil, company: nil,
               metric: "Joe User+RM", related: nil, add: nil
-    res = "{{#{metric}#{nest_options options, year, unknown, company, related}}}"
+    nest_values = [year, unknown, company, related]
+    res = "{{#{metric}#{nest_options options, nest_values}}}"
     res = "#{method}[#{res}]" if method
     res = "#{res}+#{formula_str(**add)}" if add
     res
   end
 
-  def nest_options options, year, unknown, company, related
-    options ||= implicit_nest_options(year, unknown, company, related)
+  def nest_options options, nest_values
+    options ||= implicit_nest_options(*nest_values)
     format_nest_options options.compact
   end
 
