f169fabf781251613945d8df10c7b1ab10586eba
Ethan McCutchen <ethan@decko.org>
2021-09-09 14:01:38 -0400

    graphiql hello world

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,13 @@ require 'decko/engine'
 require 'delayed_job_web'
 
 Rails.application.routes.draw do
-  post "/graphql", to: "graphql#execute"
-  options "/graphql", to: "graphql#execute"
+  post "api/graphql", to: "graphql#execute"
+  # options "api/graphql", to: "graphql#execute"
 
-  # if Rails.env.development?
-  #   mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
-  # end
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: "api/graphiql", graphql_path: "graphql"
+  end
+
   mount DelayedJobWeb => "/*admin/delayed_job"
   mount Decko::Engine => '/'
 end
