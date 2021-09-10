1b227d0107d7d422cfcbb77497ffa0035ee154ab
Ethan McCutchen <ethan@decko.org>
2021-09-10 14:01:00 -0400

    source filter fixes

--- mod/wikirate_company/set/type_plus_right/wikirate_company/source.rb
+++ mod/wikirate_company/set/type_plus_right/wikirate_company/source.rb
@@ -1,8 +1,8 @@
+include_set Abstract::SourceFilter
+
 # cache # of sources tagged with this company (=left) via <source>+company
 include_set Abstract::TaggedByCachedCount,
             type_to_count: :source, tag_pointer: :wikirate_company
-
-include_set Abstract::SourceFilter
 
 format do
   # don't show answer sort option, because that means "total answers"
