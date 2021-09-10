4feb28fa8e2aac97536d745a85d079ae61130d47
Ethan McCutchen <ethan@decko.org>
2021-09-09 22:47:10 -0400

    filter_list.rb

--- mod/datasets/set/type_plus_right/dataset/wikirate_company.rb
+++ mod/datasets/set/type_plus_right/dataset/wikirate_company.rb
@@ -5,30 +5,13 @@ include_set Abstract::SearchViews
 include_set Right::BrowseCompanyFilter
 include_set Abstract::DatasetScope
 include_set Abstract::IdPointer
+include_set Abstract::FilterList
 
 def item_cards_for_validation
   item_cards.sort_by(&:key)
 end
 
-def cql_content
-  { referred_to_by: "_"}
-end
-
-def item_cards args={}
-  return item_cards_search(args) if args[:complete]
-  return known_item_cards(args) if args[:known_only]
-
-  all_item_cards args
-end
-
-def count
-  item_strings.size
-end
-
-
 format :html do
-
-
   def filter_field_code
     :browse_company_filter
   end
