fe9d7184d2179d9f5b3a71b657815834c8ac76e1
Ethan McCutchen <ethan@decko.org>
2021-09-14 10:00:24 -0400

    dashboard company context progressing...

--- mod/researched_metrics/set/type/record/research.rb
+++ mod/researched_metrics/set/type/record/research.rb
@@ -1,4 +1,8 @@
 format :html do
+  def project_card
+    @project_card || params[:project]&.card
+  end
+
   layout :research_layout, view: :research do
     wikirate_layout "wikirate-one-full-column-layout research-layout px-2" do
       layout_nest
@@ -10,4 +14,8 @@ format :html do
   end
 
   view :research, template: :haml
+
+  view :research_progress_bar do
+    nest Card.fetch([card.company_name, project_card]), view: :research_dashboard_progress
+  end
 end
