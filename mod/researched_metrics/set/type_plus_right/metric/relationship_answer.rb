1be456a2102ca47fe4a6ba9e7ec91e0fdac2d671
Ethan McCutchen <ethan@decko.org>
2021-09-17 15:56:28 -0400

    +relationship answer fix for ccc widget

--- mod/researched_metrics/set/type_plus_right/metric/relationship_answer.rb
+++ mod/researched_metrics/set/type_plus_right/metric/relationship_answer.rb
@@ -12,7 +12,7 @@ format do
   # only handles subject company and year for now.
   def relationship_query
     card.query.tap do |query|
-      filter_by_subject_companies query
+      filter_by_companies query
       filter_by_year query
     end
   end
@@ -37,13 +37,14 @@ format do
     end
   end
 
-  def filter_by_subject_companies query
-    return unless subject_company_ids.present?
+  def filter_by_companies query
+    return unless company_ids.present?
 
-    query[:subject_company_id] = subject_company_ids.unshift("in")
+    company_field = metric_card.inverse? ? :object_company_id : :subject_company_id
+    query[company_field] = company_ids.unshift("in")
   end
 
-  def subject_company_ids
-    @subject_company_ids ||= Env.params[:filter] ? filtered_company_ids : []
+  def company_ids
+    @company_ids ||= params[:filter] ? filtered_company_ids : []
   end
 end
