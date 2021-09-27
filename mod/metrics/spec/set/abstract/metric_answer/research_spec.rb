1ffff2775005704ee7053b6072d1bb9035a139e7
Ethan McCutchen <ethan@decko.org>
2021-09-27 11:53:36 -0400

    pause research spec

--- mod/metrics/spec/set/abstract/metric_answer/research_spec.rb
+++ mod/metrics/spec/set/abstract/metric_answer/research_spec.rb
@@ -1,7 +1,7 @@
 # -*- encoding : utf-8 -*-
 
 RSpec.describe Card::Set::Abstract::MetricAnswer::Research do
-  specify "non-researchable metrics are not editable" do
+  xspecify "non-researchable metrics are not editable" do
     rendered = view :edit,
                     card: "Jedi+deadliness average+Slate Rock and Gravel Company+2005"
     aggregate_failures "no edit form" do
