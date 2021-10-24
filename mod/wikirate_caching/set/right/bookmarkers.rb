10e964bfaba9013bc6d5a9804ae183738a8c5288
Ethan McCutchen <ethan@decko.org>
2021-10-24 02:36:47 -0400

    bookmarkers conflict fix

--- mod/wikirate_caching/set/right/bookmarkers.rb
+++ mod/wikirate_caching/set/right/bookmarkers.rb
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-# TODO: abstract and put in bookmarks mod
-
-# cache # of users who have bookmarked this metric/topic/whatever(=left)
-include_set Abstract::ListRefCachedCount,
-            type_to_count: :user,
-            list_field: :bookmarks,
-            count_trait: :bookmarkers
-
-=======
->>>>>>> main
 format :html do
   def bookmark_status_class
     card.active? ? "#{card.left.type_name.key}-color" : "inactive-bookmark"
