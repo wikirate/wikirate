c2eb2ecb2730e11f6e5215bda2bcf300dbe287de
Ethan McCutchen <ethan@decko.org>
2021-11-11 11:29:44 -0500

    graph_q_l tweaks

---
+++ mod/wikirate_graphql/lib/graph_q_l/types/research_group.rb
@@ -0,0 +1,15 @@
+module GraphQL
+  module Types
+    # CompanyGroup type for GraphQL
+    class ResearchGroup < Card
+      # field :companies, [Company], null: false do
+      #   # argument :limit, Integer, required: false
+      #   # argument :offset, Integer, required: false
+      # end
+      #
+      # def companies
+      #   object.wikirate_company_card.item_cards # limit: limit, offset: offset
+      # end
+    end
+  end
+end
