265b06dfe87a6b9f6e762fdfdfe4200f14b2b1e2
Ethan McCutchen <ethan@decko.org>
2021-11-23 20:10:01 -0500

    integration failure fixes

--- mod/deckorate_calculations/set/type_plus_right/metric/formula/calculations.rb
+++ mod/deckorate_calculations/set/type_plus_right/metric/formula/calculations.rb
@@ -9,6 +9,8 @@ def calculator_class
   end
 end
 
-event :update_metric_answers, :integrate, on: :save, changed: :content, when: :content? do
+event :update_metric_answers, :integrate_with_delay,
+      on: :save, changed: :content, when: :content? do
+  puts "CALCULATING".yellow
   metric_card.deep_answer_update
 end
