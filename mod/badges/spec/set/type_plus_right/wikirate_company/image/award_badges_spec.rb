5d9a22dfc34e0eec2809720be516352b57dc6fee
Ethan McCutchen <ethan@decko.org>
2021-11-29 19:51:41 -0500

    rollback graphql gem

--- mod/badges/spec/set/type_plus_right/wikirate_company/image/award_badges_spec.rb
+++ mod/badges/spec/set/type_plus_right/wikirate_company/image/award_badges_spec.rb
@@ -1,6 +1,6 @@
 # -*- encoding : utf-8 -*-
 
-describe Card::Set::TypePlusRight::WikirateCompany::Image::AwardBadges do
+RSpec.describe Card::Set::TypePlusRight::WikirateCompany::Image::AwardBadges do
   describe "logo badges" do
     let(:badge_action) { :logo }
     let(:badge_type) { :wikirate_company }
