fd3eb220f7ec23e6a2cd15da859149e16f3b7ad9
Ethan McCutchen <ethan@decko.org>
2021-12-08 22:30:35 -0500

    code climate fixes for invalid answer script

--- script/find_and_log_invalid_answer_values.rb
+++ script/find_and_log_invalid_answer_values.rb
@@ -2,23 +2,25 @@ require_relative "../config/environment"
 
 FILENAME = "/tmp/invalid_answer_values.csv".freeze
 SITE = "https://wikirate.org".freeze
-VALUE_TYPES = %i[multi_category]
-METRIC_TYPES = %i[researched]
+VALUE_TYPES = %i[multi_category].freeze
+METRIC_TYPES = %i[researched].freeze
 OFFSET = 0
 
 Card::Auth.as_bot
 
-def validity answer_id
+def validate answer_id
   answer = answer_id.card
   return if answer.relationship? || answer.calculated?
 
-  val = answer.value_card
-  if !Answer.unknown?(val.content) && val.illegal_items.present?
-    record_invalid answer_id, "INVALID", val.content
-  end
-    #  record_invalid val, "INVALID", val.content unless val.valid?
+  validate_value answer.value_card
 rescue StandardError => e
   record_invalid answer_id, "ERROR", e.message
+end
+
+def validate_value val
+  return if Answer.unknown?(val.content) || !val.illegal_items.present?
+
+  record_invalid answer_id, "INVALID", val.content
 end
 
 def milestones seq
@@ -40,6 +42,6 @@ Metric.joins("join answers on metrics.metric_id = answers.metric_id")
       .select(:answer_id).offset(OFFSET)
       .pluck(:answer_id).each_with_index do |answer_id, index|
 
-  validity answer_id
+  validate answer_id
   milestones index
 end
