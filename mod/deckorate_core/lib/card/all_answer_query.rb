5a1700409b1fd19d0415c1fce9e4d3befeeae5e0
Ethan McCutchen <ethan@decko.org>
2021-12-15 15:20:31 -0500

    all answer query

--- mod/deckorate_core/lib/card/all_answer_query.rb
+++ mod/deckorate_core/lib/card/all_answer_query.rb
@@ -18,8 +18,7 @@ class Card
     end
 
     def process_filters
-      require_partner!
-      add_card_condition "#{@partner}.type_id = ?", PARTNER_TYPE_ID[@partner]
+      add_card_condition "#{partner}.type_id = ?", PARTNER_TYPE_ID[partner]
       filter_applicability
       super
     end
@@ -31,14 +30,14 @@ class Card
     private
 
     def partner_joins
-      @card_joins.unshift("AS #{@partner}").push answer_join
+      @card_joins.unshift("AS #{partner}").push answer_join
     end
 
     # Currently these queries only work with a fixed company or metric
     # it is not yet possible to handle not-researched answers for multiple companies and
     # metrics in one query
-    def require_partner!
-      @partner =
+    def partner
+      partner ||=
         if single_metric?
           :company
         elsif single_company?
@@ -51,11 +50,11 @@ class Card
     # This left join is the essence of the search strategy.
     def answer_join
       "LEFT JOIN answers " \
-      "ON #{@partner}.id = answers.#{@partner}_id AND #{lookup_conditions}"
+      "ON #{partner}.id = answers.#{partner}_id AND #{lookup_conditions}"
     end
 
     def partner_where
-      "#{@partner}.trash is false AND #{card_conditions} "
+      "#{partner}.trash is false AND #{card_conditions} "
     end
 
     def researched_card id
@@ -63,13 +62,14 @@ class Card
     end
 
     def main_results
+      puts "all answer query sql: #{main_results_sql}"
       Card.find_by_sql(main_results_sql).map do |rec|
         rec.id ? researched_card(rec.id) : not_researched_card(rec.name)
       end
     end
 
     def main_results_sql
-      p = @partner
+      p = partner
       sort_and_page do
         main_query.select "answers.id, #{p}.name, #{p}.left_id, #{p}.right_id"
       end.to_sql
