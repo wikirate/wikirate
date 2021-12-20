b6d1af2b66fbc6315f340dc2fc3b28a3a35f0ebd
Ethan McCutchen <ethan@decko.org>
2021-12-20 15:27:37 -0500

    optimize topic / company counts

--- mod/deckorate_core/set/type_plus_right/metric/wikirate_company.rb
+++ mod/deckorate_core/set/type_plus_right/metric/wikirate_company.rb
@@ -1,6 +1,10 @@
 # cache # of companies with values for metric (=_left)
 include_set Abstract::AnswerLookupCachedCount, target_type: :company
 
+def query_hash
+  { metric_id: left_id }
+end
+
 # recount number of companies for a given metric when a Metric Value card is
 # created or deleted
 recount_trigger :type, :metric_answer, on: %i[create delete] do |changed_card|
@@ -12,8 +16,4 @@ recount_trigger :type_plus_right, :metric_answer, :unpublished do |changed_card|
   field_recount changed_card do
     changed_card.left.metric_card.fetch :wikirate_company
   end
-end
-
-def search_anchor
-  { metric_id: left.id }
 end
