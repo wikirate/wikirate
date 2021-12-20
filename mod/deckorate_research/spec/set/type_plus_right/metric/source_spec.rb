bc32fa1fc819f641138209ae76398ab76de9e2a9
Ethan McCutchen <ethan@decko.org>
2021-12-20 12:18:58 -0500

    new metric+source counting

---
+++ mod/deckorate_research/spec/set/type_plus_right/metric/source_spec.rb
@@ -0,0 +1,12 @@
+# RSpec.describe Card::Set::TypePlusRight::Metric::Source do
+#   it_behaves_like "cached count", "#{Card::Name[:star_wars_source]}+metric", 4, 1 do
+#     let :add_one do
+#       Card["Jedi+Weapons"].create_answers do
+#         Samsung "1977" => { value: "hand", source: Card[:star_wars_source] }
+#       end
+#     end
+#     let :delete_one do
+#       Card["Jedi+cost of planets destroyed+Death Star+1977"].delete
+#     end
+#   end
+# end
