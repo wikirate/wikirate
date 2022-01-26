0602df52ff8e6090003ef439668c73485c4268b7
Ethan McCutchen <ethan@decko.org>
2022-01-26 12:16:31 -0500

    priorities for integrate_with_delay events

--- mod/deckorate_calculations/set/type_plus_right/metric/formula/calculations.rb
+++ mod/deckorate_calculations/set/type_plus_right/metric/formula/calculations.rb
@@ -9,7 +9,7 @@ def calculator_class
   end
 end
 
-event :update_calculated_answers, :integrate_with_delay,
+event :update_calculated_answers, :integrate_with_delay, priority: 5,
       on: :save, changed: :content, when: :content? do
   metric_card.deep_answer_update
 end
