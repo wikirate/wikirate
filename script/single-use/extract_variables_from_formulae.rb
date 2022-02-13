fc1241dcc3a79b464bf99fdf045dc9dc95d0c1b3
Ethan McCutchen <ethan@decko.org>
2022-02-13 17:09:20 -0500

    codeclimate fix and semaphore tweak

--- script/single-use/extract_variables_from_formulae.rb
+++ script/single-use/extract_variables_from_formulae.rb
@@ -4,30 +4,58 @@ Card::Auth.signin "Ethan McCutchen"
 
 NEST_REGEXP = /^\s*(?<variable>\w+)\s*=\s*\{\{(?<nest>[^}]*)\}\}\s*$/
 
-
 def inspect_inputs inputs, formula
   puts "#{inputs.keys.size} found variables for #{formula.name.left}: " \
        "#{inputs.keys.join ', '}".green
-  if inputs.keys.size != formula.nest_chunks.size
-    puts "expected #{formula.nest_chunks.size} variables from " \
-    "/#{formula.name.url_key} :\n#{formula.content}".red
-  end
+
+  chunks = formula.nest_chunks
+  inspect_nest_count inputs, chunks.size, formula
+  inspect_nest_options chunks, formula
+end
+
+def inspect_nest_count inputs, nest_count, formula
+  return if inputs.keys.size == nest_count
+
+  puts "expected #{formula.nest_chunks.size} variables from " \
+       "/#{formula.name.url_key} :\n#{formula.content}".red
+end
+
+def inspect_nest_options chunks, formula
+  opts = chunks.map { |n| n.options.except :nest_name, :nest_syntax }.uniq
+  puts opts
+
+  @option_groups[opts] ||= []
+  @option_groups[opts] << formula
+
+  return unless opts.size > 1
+
+  puts "nest options vary! " # ":\n#{formula.content}".yellow
 end
 
 def strip_coffeescript_comment fcontent
   fcontent.sub /^#\s*CoffeeScript\s*/m, ""
 end
 
-Card.search left: { type: :metric }, right: :formula do |formula|
-  next unless formula.calculator_class == Calculate::JavaScript
+def each_javascript_formula
+  Card.search left: { type: :metric }, right: :formula do |formula|
+    yield formula if formula.calculator_class == Calculate::JavaScript
+  end
+end
 
-  fcontent = strip_coffeescript_comment formula.content
+def inputs_from_formula fcontent
   inputs = {}
-
   fcontent.each_line do |line|
     break unless (match = line.match NEST_REGEXP)
     inputs[match[:variable]] = match[:nest]
   end
+  inputs
+end
 
-  inspect_inputs inputs, formula
+def inspect_all
+  @option_groups = {}
+  each_javascript_formula do |formula|
+    fcontent = strip_coffeescript_comment formula.content
+    inputs = inputs_from_formula fcontent
+    inspect_inputs inputs, formula
+  end
 end
