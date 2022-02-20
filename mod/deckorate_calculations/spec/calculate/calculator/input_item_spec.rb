bbe8c4b977ba4024df7083a3d0c8a0e17c6e777a
Ethan McCutchen <ethan@decko.org>
2022-02-20 12:51:16 -0500

    input_item_spec green

--- mod/deckorate_calculations/spec/calculate/calculator/input_item_spec.rb
+++ mod/deckorate_calculations/spec/calculate/calculator/input_item_spec.rb
@@ -20,6 +20,12 @@ RSpec.describe Calculate::Calculator::InputItem do
       ii.answer_for company, year
     end
 
+    def answer_value_hash *args
+      answer_for(*args).each_with_object({}) do |(year, answer), hash|
+        hash[year] = answer.value
+      end
+    end
+
     context "no options" do
       example "no year specified" do
         expect(answer_for(death_star, nil).values.first.value).to eq("0.31")
@@ -41,82 +47,76 @@ RSpec.describe Calculate::Calculator::InputItem do
         expect(answer_for(death_star, nil, year: "-1")[1978].value).to eq("0.31")
       end
 
-      example "metric with relative year list" do
-        expect(answer_for(death_star, nil, year: "-23, 0, 1")[2000].value)
-          .to eq %w[yes yes yes]
+      context "with disturbance" do
+        let(:mark) { "Jedi+disturbances in the Force "}
+
+        example "metric with relative year list" do
+          expect(answer_for(death_star, nil, year: "-23, 0, 1")[2000].value)
+            .to eq %w[yes yes yes]
+        end
+
+        example "metric with fixed year list" do
+          expect(answer_for(death_star, nil, year: "1977, 2000").values.map(&:value).uniq)
+            .to eq [%w[yes yes]]
+        end
       end
 
-      example "metric with fixed year list" do
-        expect(answer_for(death_star, nil, year: "1977, 2000").values.map(&:value).uniq)
-          .to eq %w[yes yes]
-      end
+      context "with researched" do
+        let(:mark) { "Joe User+researched number 1"}
 
-      example "metric with relative year range" do
-        expect(answer_for(samsung, nil, year: "-1..0"))
-          .to eq 2015 => %w[10 5]
-      end
+        example "metric with relative year range" do
+          expect(answer_for(samsung, nil, year: "-1..0")[2015].value).to eq(%w[10 5])
+        end
 
-      example "metric with year range with fixed start and relative stop " do
-        expect(answer_value_hash(samsung, nil, year: "2014..0"))
-          .to eq 2015 => %w[10 5], 2014 => ["10"]
-      end
+        example "metric with year range with fixed start and relative stop " do
+          expect(answer_value_hash(samsung, nil, year: "2014..0"))
+            .to eq 2015 => %w[10 5], 2014 => ["10"]
+        end
 
-      example "metric with year range with relative start and fixed stop " do
-        ii, = input_items "{{Joe User+researched number 1|year:0..2015}}"
-        expect(answer_value_hash(ii, samsung, nil))
-          .to eq 2014 => %w[10 5], 2015 => ["5"]
-      end
+        example "metric with year range with relative start and fixed stop " do
+          expect(answer_value_hash(samsung, nil, year: "0..2015"))
+            .to eq 2014 => %w[10 5], 2015 => ["5"]
+        end
 
-      example "metric with year fixed range" do
-        ii, = input_items "{{Joe User+researched number 1|year:2014..2015}}"
-        expect(ii.answer_for(samsung, 2000).value)
-          .to eq %w[10 5]
-      end
+        example "metric with year fixed range" do
+          expect(answer_for(samsung, 2000, year: "2014..2015").value).to eq %w[10 5]
+        end
 
-      example "metric with latest year" do
-        ii, = input_items "{{Joe User+researched number 1|year:latest}}"
-        expect(ii.answer_for(samsung, 2000).value)
-          .to eq "5"
+        example "metric with latest year" do
+          expect(answer_for(samsung, 2000, year: "latest").value).to eq "5"
+        end
       end
     end
 
     describe "company options" do
+      let(:mark) { "Jedi+deadliness" }
+
+      example "metric with fixed single company option" do
+        expect(answer_value_hash(death_star, nil, company: "Death Star"))
+          .to eq(1977 => "100")
+      end
+
+      example "metric with fixed list company option" do
+        expect(answer_value_hash(samsung, nil, company: "Death Star, SPECTRE"))
+          .to eq(1977 => %w[100 50])
+      end
+
       example "metric with related company option" do
-        ii, = input_items "{{Jedi+deadliness|company:Related[Jedi+more evil=yes]}}"
-        expect(answer_value_hash(ii, death_star, nil))
+        expect(answer_value_hash(death_star, nil, company: "Related[Jedi+more evil=yes]"))
           .to eq(1977 => %w[40 50])
       end
 
       example "metric with related company options with 2 conditions" do
-        ii, = input_items "{{Jedi+deadliness|company:Related[Jedi+more evil = yes && "\
-                        "Commons+Supplied by = Tier 1 Supplier]}}"
-        expect(answer_value_hash(ii, spectre, nil))
+        condition = "Jedi+more evil = yes && Commons+Supplied by = Tier 1 Supplier"
+        expect(answer_value_hash(spectre, nil, company: "Related[#{condition}]"))
           .to eq(1977 => %w[40])
       end
 
-      example "metric with fixed single company option" do
-        ii, ii2 = input_items "{{Jedi+deadliness}} / {{Jedi+deadliness|company:Death Star}}"
-        aggregate_failures do
-          expect(answer_value_hash(ii, death_star, nil)).to eq(1977 => "100")
-          expect(answer_value_hash(ii2, death_star, nil)).to eq(1977 => "100")
-        end
+      example "metric with company and year options" do
+        expect(answer_value_hash(death_star, nil, year: "-1",
+                                                  company: "Related[Jedi+more evil=yes]"))
+          .to eq(1978 => %w[40 50])
       end
-
-      example "metric with fixed list company option" do
-        ii, ii2 = input_items(
-          "{{Jedi+deadliness}} / {{Jedi+deadliness|company:Death Star, SPECTRE}}"
-        )
-        aggregate_failures do
-          expect(answer_value_hash(ii, samsung, nil)).to eq(1977 => "Unknown")
-          expect(answer_value_hash(ii2, samsung, nil)).to eq(1977 => %w[100 50])
-        end
-      end
-    end
-
-    example "metric with company and year options" do
-      ii, = input_items "{{Jedi+deadliness|company:Related[Jedi+more evil=yes]; year: -1}}"
-      expect(answer_value_hash(ii, death_star, nil))
-        .to eq(1978 => %w[40 50])
     end
   end
 end
