97280be98f7124f4b759fba6a35416c4089d476e
Ethan McCutchen <ethan@decko.org>
2022-06-22 13:43:31 -0400

    open search handles autocomplete

--- mod/deckorate_search/set/self/search/open_search.rb
+++ mod/deckorate_search/set/self/search/open_search.rb
@@ -11,14 +11,14 @@ end
 # @result [Hash] ruby translation of JSON results
 # note: options configured in config/application.rb
 def search query
+  puts "OPEN SEARCH QUERY =\n#{os_query}".yellow
   open_search_client.search body: { query: query },
                             index: Cardio.config.open_search_index
 end
 
 format do
   def search_with_params
-    # puts "BOOL = #{os_query}".yellow
-    os_results_to_cards { card.search bool: os_query }
+    @search_with_params ||= os_results_to_cards { card.search bool: os_query }
   end
 
   private
@@ -45,5 +45,11 @@ format do
     return yield unless type_param.present?
 
     yield[:filter] = { term: { type_id: type_param.card_id } }
+  end
+end
+
+format :json do
+  def complete_or_match_search *_args
+    os_results_to_cards { card.search bool: os_query }.compact.map &:name
   end
 end
