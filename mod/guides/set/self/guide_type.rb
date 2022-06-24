633ebdac1677300b50d28bd649022f69a2a309a6
Ethan McCutchen <ethan@decko.org>
2022-06-24 14:07:24 -0400

    more jumbotron usage. guide behavior

--- mod/guides/set/self/guide_type.rb
+++ mod/guides/set/self/guide_type.rb
@@ -1,19 +1,17 @@
+include_set Abstract::Jumbotron
+
+SUPPORT_CARDS = ["FAQ", "Glossary", "Talk to us"].freeze
+
 format :html do
-  SUPPORT_CARDS = ["FAQ", "Glossary", "Talk to us"].freeze
-
-  def layout_name_from_rule
-    :deckorate_jumbotron_layout
-  end
-
   before :content_formgroups do
     voo.edit_structure = %i[title description list]
   end
 
   before(:page) { voo.title = "Guides" }
 
+  view :page, template: :haml, wrap: :slot
+
   def support_cards
     SUPPORT_CARDS
   end
-
-  view :page, template: :haml, wrap: :slot
 end
--- mod/guides/set/type/guide_type.rb
+++ mod/guides/set/type/guide_type.rb
@@ -1,3 +1,5 @@
+include_set Abstract::Jumbotron
+
 card_accessor :body
 
 format :html do
