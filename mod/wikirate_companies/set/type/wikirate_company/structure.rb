917c5d4e8a623fae7570a51f24a4b10cc5f6e7be
Ethan McCutchen <ethan@decko.org>
2022-07-01 11:14:18 -0400

    homepage styling fix, company structure test

--- mod/wikirate_companies/set/type/wikirate_company/structure.rb
+++ mod/wikirate_companies/set/type/wikirate_company/structure.rb
@@ -41,18 +41,6 @@ format :html do
       company_group: { label: "Groups" } }
   end
 
-  # def answer_filtering
-  #   filtering(".RIGHT-answer ._compact-filter") do
-  #     yield view: :bar, show: :full_page_link, hide: %i[company_header edit_link]
-  #   end
-  # end
-
-  # view :wikirate_topic_tab do
-  #   answer_filtering do |items|
-  #     field_nest :wikirate_topic, view: :filtered_content, items: items
-  #   end
-  # end
-
   view :metric_answer_tab do
     field_nest :metric_answer, view: :filtered_content
   end
