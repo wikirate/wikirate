8fe76a21a504c76994a7e20dd1b8d12aaa000448
Ethan McCutchen <ethan@decko.org>
2022-07-05 19:31:22 -0400

    dataset filterable list fix

--- mod/deckorate_datasets/set/type_plus_right/dataset/metric.rb
+++ mod/deckorate_datasets/set/type_plus_right/dataset/metric.rb
@@ -1,6 +1,7 @@
 # These Dataset+Metric (type plus right) cards refer to the list of
 # all companies on a given dataset.
 
+include_set Abstract::FilterableList
 include_set Abstract::MetricSearch
 include_set Abstract::DatasetScope
 include_set Abstract::IdPointer
--- mod/deckorate_datasets/set/type_plus_right/dataset/wikirate_company.rb
+++ mod/deckorate_datasets/set/type_plus_right/dataset/wikirate_company.rb
@@ -1,5 +1,7 @@
 # These Dataset+Company (type plus right) cards refer to the list of
 # all companies on a given dataset.
+
+include_set Abstract::FilterableList
 include_set Abstract::CompanySearch
 include_set Abstract::DatasetScope
 include_set Abstract::IdPointer
