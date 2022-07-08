9f2daf99c6fd880018277888eb119e7cc4df58e0
Ethan McCutchen <ethan@decko.org>
2022-07-08 11:33:14 -0400

    dataset metric list fix

--- mod/deckorate_datasets/set/type_plus_right/dataset/wikirate_company.rb
+++ mod/deckorate_datasets/set/type_plus_right/dataset/wikirate_company.rb
@@ -2,7 +2,7 @@
 # all companies on a given dataset.
 
 include_set Abstract::CompanySearch
-include_set Abstract::FilterableList
+include_set Abstract::FilterableList # must come after CompanySearch to override cql
 include_set Abstract::DatasetScope
 include_set Abstract::IdPointer
 
