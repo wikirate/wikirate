56ccb06d7ec00b97857990fcf363757135d8793e
Ethan McCutchen <ethan@decko.org>
2022-07-12 09:30:46 -0400

    popover fixes, etc

--- mod/deckorate_utility/set/abstract/export.rb
+++ mod/deckorate_utility/set/abstract/export.rb
@@ -1,3 +1,5 @@
+EXPORT_LIMIT_OPTIONS = [50, 100, 500, 1000, 5000].freeze
+
 format do
   def export_filename
     "WikiRate-#{export_timestamp}-#{export_title}"
@@ -5,5 +7,30 @@ format do
 end
 
 format :html do
-  view :export_panel, cache: :never, template: :haml
+  view :export_panel, cache: :never, template: :haml, wrap: :slot
+
+  view :export_button, cache: :never, template: :haml
+
+  def export_limit_options
+    options = EXPORT_LIMIT_OPTIONS.map { |num|  export_limit_option_label num }
+    options_for_select options,
+                       disabled: (Auth.signed_in? ? [] : [1000, 5000]),
+                       selected: (Auth.signed_in? ? 5000 : 500)
+  end
+
+  def export_modal_link text, opts={}
+    opts[:path] = filter_and_sort_hash.merge(mark: card.name, view: :export_panel)
+    modal_link text, opts
+  end
+
+  # localize
+  def export_limit_option_label num
+    ["up to #{num}", num]
+  end
+
+  # localize
+  def export_item_limit_label
+    type_name = card.item_type_name
+    type_name.present? ? type_name&.vary(:plural) : "Items"
+  end
 end
