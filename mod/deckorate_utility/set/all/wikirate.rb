7f38a6f855273ea88c74be674a84f405faa57d02
Ethan McCutchen <ethan@decko.org>
2022-07-13 09:45:26 -0400

    export panel in dropdown

--- mod/deckorate_utility/set/all/wikirate.rb
+++ mod/deckorate_utility/set/all/wikirate.rb
@@ -1,6 +1,20 @@
 require "net/https"
 require "uri"
 
+# for now, no notifications on acts using API key
+def silent_change?
+  Card::Auth.api_act? || super
+end
+
+def as_wikirate_team?
+  Card::Auth.as_id == Card::WikirateTeamID
+end
+
+def as_moderator?
+  Card::Auth.always_ok? || as_wikirate_team?
+end
+
+
 format do
   def rate_subject
     @wikirate_subject ||= Card.fetch_name(:wikirate_company)
@@ -16,5 +30,26 @@ format :html do
     # force showing help text
     voo.help ||= true
     super()
+  end
+
+  # TODO: refactor away
+  view :menued do
+    render_titled hide: [:title, :toggle], show: :menu
+  end
+
+  view :type_link, template: :haml do
+    @type_card = card.type_card
+  end
+
+  def default_nest_view
+    :content
+  end
+
+  def type_link_label
+    @type_card.name
+  end
+
+  def type_link_icon
+    mapped_icon_tag @type_card.codename
   end
 end
