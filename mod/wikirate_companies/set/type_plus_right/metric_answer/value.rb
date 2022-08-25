2547a4fb9bd0ef6d3ea1706c3e49b8ccf1b7dc2f
Ethan McCutchen <ethan@decko.org>
2022-08-25 08:50:10 -0400

    use delayed jobs with company group updates

--- mod/wikirate_companies/set/type_plus_right/metric_answer/value.rb
+++ mod/wikirate_companies/set/type_plus_right/metric_answer/value.rb
@@ -1,11 +1,10 @@
-# FIXME: temporarily disabled for testing
-
-# event :update_company_group_lists_based_on_metric, :integrate do
-#   company_group_lists_for_metric(metric_id).each do |grouplist|
-#     grouplist.update_content_from_spec
-#     grouplist.save!
-#   end
-# end
+event :update_company_group_lists_based_on_metric,
+      :integrate_with_delay, priority: 15 do
+  company_group_lists_for_metric(metric_id).each do |grouplist|
+    grouplist.update_content_from_spec
+    grouplist.save!
+  end
+end
 
 def company_group_lists_for_metric metric_id
   Card.search type: :company_group,
