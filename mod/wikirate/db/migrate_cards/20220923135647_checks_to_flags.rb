08b4e703ed5f6ef3190e9b6033154a175fc7afc1
Ethan McCutchen <ethan@decko.org>
2022-09-23 11:45:57 -0400

    fix migrations permissions issue

--- mod/wikirate/db/migrate_cards/20220923135647_checks_to_flags.rb
+++ mod/wikirate/db/migrate_cards/20220923135647_checks_to_flags.rb
@@ -6,34 +6,41 @@ class ChecksToFlags < Cardio::Migration
   def up
     each_check_request do |answer, checked_by|
       if answer.value_card.unknown_value?
-        checked_by.delete!
+        delete_card checked_by
       elsif answer.discussion.present?
         add_flag answer, checked_by
-        checked_by.delete!
+        delete_card checked_by
       end
     end
   end
-end
 
-def add_flag answer, checked_by
-  with_request_context checked_by do
-    Card.create! type: :flag,
-                 fields: {
-                   flag_type: "Wrong Value",
-                   discussion: answer.discussion,
-                   subject: answer.name
-                 }
+  private
+
+  def add_flag answer, checked_by
+    with_request_context checked_by do
+      Card.create! type: :flag,
+                   fields: {
+                     flag_type: "Wrong Value",
+                     discussion: answer.discussion,
+                     subject: answer.name
+                   }
+    end
   end
-end
 
-def with_request_context checked_by, &block
-  Card::Auth.signin checked_by.updater
-  Timecop.freeze checked_by.updated_at &block
-end
+  def delete_card checked_by
+    Card::Auth.signin "Ethan McCutchen"
+    checked_by.delete!
+  end
 
-# note: check requests on an answer set the content of its +checked_by card to "request"
-def each_check_request
-  Card.search left: { type: "Answer" }, right: :checked_by, eq: "request" do |checked_by|
-    yield checked_by.left, checked_by
+  def with_request_context checked_by, &block
+    Card::Auth.signin checked_by.updater
+    Timecop.freeze checked_by.updated_at &block
+  end
+
+  # note: check requests on an answer set the content of its +checked_by card to "request"
+  def each_check_request
+    Card.search left: { type: "Answer" }, right: :checked_by, eq: "request" do |checked_by|
+      yield checked_by.left, checked_by
+    end
   end
 end
