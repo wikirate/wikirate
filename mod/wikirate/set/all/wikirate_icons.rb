b0357c525c2eb97b38d5cba6436aa337350f0c3e
Ethan McCutchen <ethan@decko.org>
2023-03-11 21:47:42 -0500

    try overriding outputs

--- mod/wikirate/set/all/wikirate_icons.rb
+++ mod/wikirate/set/all/wikirate_icons.rb
@@ -1,6 +1,8 @@
-basket[:icons][:wikirate] =
+icon_basket = basket[:icons][:wikirate] =
   %i[wikirate_company wikirate_topic company_group project dataset research_group
      metric metric_answer source].each_with_object({}) { |key, hash| hash[key] = key }
+
+icon_basket[:data_subset] = :dataset
 
 format :html do
   def wikirate_icon_tag icon, _opts={}
