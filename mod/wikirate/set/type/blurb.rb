eae1eae5b28eabf28ee2056f199e428b32d0b6a8
Ethan McCutchen <ethan@decko.org>
2023-03-14 10:12:49 -0400

    blurb fix

--- mod/wikirate/set/type/blurb.rb
+++ mod/wikirate/set/type/blurb.rb
@@ -6,7 +6,7 @@ format :html do
     homepage_action2: "Come meet us",
     homepage_action4: "Get in touch",
     community_action1: "Join the conversation",
-    community_action2: "Become a steward",
+    community_action2: "Get in touch",
     community_action3: "Subscribe for updates",
     community_action4: "Join the Slack community",
     community_action5: "Get in touch",
