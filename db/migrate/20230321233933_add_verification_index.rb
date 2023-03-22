2a17205ebebc3b49338f2ec42fecabf4472d1677
Ethan McCutchen <ethan@decko.org>
2023-03-21 20:00:02 -0400

    removed debugging

--- db/migrate/20230321233933_add_verification_index.rb
+++ db/migrate/20230321233933_add_verification_index.rb
@@ -1,8 +1,7 @@
 # -*- encoding : utf-8 -*-
-require "pry"
+
 class AddVerificationIndex < Cardio::Migration::DeckStructure
   def change
-    # binding.pry
     add_index :answers, :verification #, name: "verification_index"
   end
 end
