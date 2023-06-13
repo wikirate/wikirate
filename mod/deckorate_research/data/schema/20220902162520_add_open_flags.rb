22614dc056d1182e8f25fae859314c3e383fd3ae
Ethan McCutchen <ethan@decko.org>
2023-06-12 22:49:52 -0400

    migration handling updates

--- mod/deckorate_research/data/schema/20220902162520_add_open_flags.rb
+++ mod/deckorate_research/data/schema/20220902162520_add_open_flags.rb
@@ -1,7 +1,7 @@
 # -*- encoding : utf-8 -*-
 
 # answer lookup fields for new flagging pattern
-class AddOpenFlags < ActiveRecord::Migration[6.1]
+class AddOpenFlags < Cardio::Migration::Schema
   def up
     add_column :answers, :open_flags, :integer, if_not_exists: true
     remove_column :answers, :check_requester, if_exists: true
