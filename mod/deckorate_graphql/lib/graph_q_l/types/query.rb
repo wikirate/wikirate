9184be5371a9721f239b272ee922fc0b49a8e8bc
vasgat <vasgat@gmail.com>
2023-07-06 14:56:02 +0200

    modifying Query class to support filtering

--- mod/deckorate_graphql/lib/graph_q_l/types/query.rb
+++ mod/deckorate_graphql/lib/graph_q_l/types/query.rb
@@ -3,16 +3,27 @@ module GraphQL
     # Root Query for GraphQL
     class Query < BaseObject
       class << self
-        def cardtype_field fieldname, type, codename=nil
+        def cardtype_field fieldname, type, codename = nil
           codename ||= fieldname
           plural_fieldname = fieldname.to_s.to_name.vary(:plural).to_sym
 
           singular_field fieldname, type
-          plural_field plural_fieldname, type
+          plural_field plural_fieldname, codename, type
 
           define_method(fieldname) { |**mark| ok_card codename, **mark }
-          define_method plural_fieldname do |name: nil, limit: 10, offset: 0|
-            card_search name, codename, limit, offset
+          define_method plural_fieldname do |limit: 10, offset: 0, **filter|
+            card_search codename, limit, offset, filter
+          end
+        end
+
+        def subcardtype_field fieldname, type, codename = nil
+          codename ||= fieldname
+          plural_fieldname = fieldname.to_s.to_name.vary(:plural).to_sym
+
+          plural_field plural_fieldname, codename, type
+
+          define_method plural_fieldname do |limit: 10, offset: 0, **filter|
+            card_search codename, limit, offset, filter
           end
         end
 
@@ -23,11 +34,14 @@ module GraphQL
           end
         end
 
-        def plural_field fieldname, type
+        def plural_field fieldname, codename, type
           field fieldname, [type], null: false do
-            argument :id, Integer, required: false
             argument :limit, Integer, required: false
             argument :offset, Integer, required: false
+
+            codename.card.format.filter_keys.each do |filter|
+              argument filter, String, required: false
+            end
           end
         end
 
@@ -35,67 +49,45 @@ module GraphQL
           10
         end
 
-        # def lookup_field fieldname, type, filter_query_class
-        #
-        # end
+        def default_offset
+          0
+        end
+
       end
 
       cardtype_field :research_group, ResearchGroup
       cardtype_field :company_group, CompanyGroup
       cardtype_field :company, Company, :wikirate_company
+      cardtype_field :metric, Metric, :metric
+      cardtype_field :answer, Answer, :metric_answer
+      cardtype_field :relationship, Relationship, :relationship_answer
       cardtype_field :topic, Topic, :wikirate_topic
       cardtype_field :dataset, Dataset
       cardtype_field :source, Source
-
-      field :metric, Metric, null: true do
-        argument :name, String, required: false
-        argument :id, Integer, required: false
-      end
-      field :metrics, [Metric], null: false do
-        argument :limit, Integer, required: false
-      end
-
-      field :answer, Answer, null: true do
-        argument :id, Integer, required: false
-      end
-      field :answers, [Answer], null: false do
-        argument :metric, String, required: false
-        argument :limit, Integer, required: false
-        # argument :sort_by, String, required: false
-        ::Card::AnswerQuery.card_id_filters.each do |filter|
-          argument filter, String, required: false
-        end
-      end
-
-      field :relationship, Relationship, null: true do
-        argument :id, Integer, required: false
-        argument :limit, Integer, required: false
-      end
-      field :relationships, [Relationship], null: false
 
       def metric **mark
         ok_card(:metric, **mark)&.lookup
       end
 
-      def metrics limit: Query.default_limit
-        ::Metric.limit(limit).all
+      def metrics limit: Query.default_limit, offset: Query.default_offset, **filter
+        ::Card::MetricQuery.new(filter, {}, limit: limit, offset: offset).lookup_relation.all
       end
 
       def answer **mark
         ok_card :metric_answer, **mark
       end
 
-      def answers metric: nil, limit: Query.default_limit, **filter
+      def answers metric: nil, limit: Query.default_limit, offset: Query.default_offset, **filter
         filter[:metric_id] = metric.card_id if metric
-        ::Card::AnswerQuery.new(filter, {}, limit: limit).lookup_relation.all
+        ::Card::AnswerQuery.new(filter, {}, limit: limit, offset: offset).lookup_relation.all
       end
 
       def relationship **mark
         ok_card :relationship_answer, **mark
       end
 
-      def relationships limit: Query.default_limit
-        ::Relationship.limit(limit).all
+      def relationships limit: Query.default_limit, offset: Query.default_offset, **filter
+        ::Relationship.limit(limit).offset(offset).all
       end
     end
   end
