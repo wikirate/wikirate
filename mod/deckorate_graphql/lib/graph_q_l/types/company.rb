155c80a1446a7376cb75cd008579d4a6b9f2cc52
vasgat <vasgat@gmail.com>
2023-07-18 19:37:44 +0200

    refactor Company to extend WikirateCard and use methods to create different types of fields

--- mod/deckorate_graphql/lib/graph_q_l/types/company.rb
+++ mod/deckorate_graphql/lib/graph_q_l/types/company.rb
@@ -1,18 +1,12 @@
 module GraphQL
   module Types
     # Company type for GraphQL
-    class Company < Card
-      subcardtype_field :answer, Answer, :metric_answer
+    class Company < WikirateCard
+      lookup_field :answer, Answer, :metric_answer
       field :relationships, [Relationship],
             "relationships of which company is either subject or object", null: false
       field :datasets, [Dataset], null: false
       field :logo_url, String, "url for company logo image", null: true
-
-      def answers metric_id: nil, limit: Card.default_limit, offset: Card.default_offset, **filter
-        filter[:company_id] = object.card_id
-        filter[:metric_id] = metric_id if metric_id
-        ::Card::AnswerQuery.new(filter, {}, limit: limit, offset: offset).lookup_relation.all
-      end
 
       def relationships
         ::Relationship.where(
