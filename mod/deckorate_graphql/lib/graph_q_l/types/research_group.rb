294dd65eeb432ba29619e90298f53b0a34970d0b
vasgat <vasgat@gmail.com>
2023-07-18 19:36:41 +0200

    refactor ResearchGroup to extend WikirateCard and use methods to create different types of fields

--- mod/deckorate_graphql/lib/graph_q_l/types/research_group.rb
+++ mod/deckorate_graphql/lib/graph_q_l/types/research_group.rb
@@ -1,12 +1,8 @@
 module GraphQL
   module Types
     # ResearchGroup type for GraphQL
-    class ResearchGroup < Card
-      subcardtype_field :metric, Metric
-      def metrics limit: Card.default_limit, offset: Card.default_offset, **filter
-        filter[:research_group] = object.name
-        ::Card::MetricQuery.new(filter, {}, limit: limit, offset: offset).lookup_relation.all
-      end
+    class ResearchGroup < WikirateCard
+      lookup_field :metric, Metric
 
     end
   end
