b7ef4b5b51d3538f062025f56eb2cf74acdaa826
vasgat <vasgat@gmail.com>
2023-07-18 19:37:24 +0200

    refactor Datasets to extend WikirateCard and use methods to create different types of fields

--- mod/deckorate_graphql/lib/graph_q_l/types/dataset.rb
+++ mod/deckorate_graphql/lib/graph_q_l/types/dataset.rb
@@ -1,27 +1,16 @@
 module GraphQL
   module Types
     # Data Set type for GraphQL
-    class Dataset < Card
+    class Dataset < WikirateCard
 
       field :years, [Integer], null: false
       field :description, String, null: false
-      subcardtype_field :company, Company, :wikirate_company
-      subcardtype_field :metric, Metric
-      subcardtype_field :answer, Answer, :metric_answer
+      cardtype_field :company, Company, :wikirate_company
+      lookup_field :metric, Metric
+      lookup_field :answer, Answer, :metric_answer
 
       def years
         object.year_card.item_names.map(&:to_i)
-      end
-
-      def metrics limit: Card.default_limit, offset: Card.default_offset, **filter
-        filter[:dataset] = object.name
-        ::Card::MetricQuery.new(filter, {}, limit: limit, offset: offset).lookup_relation.all
-      end
-
-      def answers metric_id: nil, limit: Card.default_limit, offset: Card.default_offset, **filter
-        filter[:dataset] = object.name
-        filter[:metric_id] = metric_id if metric_id
-        ::Card::AnswerQuery.new(filter, {}, limit: limit, offset: offset).lookup_relation.all
       end
     end
   end
