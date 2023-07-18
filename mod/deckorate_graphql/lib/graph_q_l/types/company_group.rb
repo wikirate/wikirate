820dbb398fef21513485febe0ae7c15f7e0cc411
vasgat <vasgat@gmail.com>
2023-07-18 19:37:35 +0200

    refactor CompanyGroup to extend WikirateCard and use methods to create different types of fields

--- mod/deckorate_graphql/lib/graph_q_l/types/company_group.rb
+++ mod/deckorate_graphql/lib/graph_q_l/types/company_group.rb
@@ -1,8 +1,8 @@
 module GraphQL
   module Types
     # CompanyGroup type for GraphQL
-    class CompanyGroup < Card
-      subcardtype_field :company, Company, :wikirate_company
+    class CompanyGroup < WikirateCard
+      cardtype_field :company, Company, :wikirate_company
     end
   end
 end
