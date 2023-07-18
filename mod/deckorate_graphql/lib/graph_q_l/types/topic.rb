aa0a4cee09b4f8ffe802583dd0e5d0cf463fb726
vasgat <vasgat@gmail.com>
2023-07-18 19:36:14 +0200

    refactor Topics to extend WikirateCard and use methods to create different types of fields

--- mod/deckorate_graphql/lib/graph_q_l/types/topic.rb
+++ mod/deckorate_graphql/lib/graph_q_l/types/topic.rb
@@ -1,15 +1,10 @@
 module GraphQL
   module Types
     # Topic type for GraphQL
-    class Topic < Card
-      subcardtype_field :metric, Metric
-      subcardtype_field :dataset, Dataset
+    class Topic < WikirateCard
+      lookup_field :metric, Metric
+      cardtype_field :dataset, Dataset
       field :description, String, null: true
-
-      def metrics limit: Card.default_limit, offset: Card.default_offset, **filter
-        filter[:wikirate_topic] = object.name
-        ::Card::MetricQuery.new(filter, {}, limit: limit, offset: offset).lookup_relation.all
-      end
     end
   end
 end
