d71dd3d75b303d8fdf0f3f3f287887670dafaf7a
vasgat <vasgat@gmail.com>
2023-07-24 13:25:41 +0200

    updating fields definition on ResearchGroup

--- mod/deckorate_graphql/lib/graph_q_l/types/research_group.rb
+++ mod/deckorate_graphql/lib/graph_q_l/types/research_group.rb
@@ -2,8 +2,7 @@ module GraphQL
   module Types
     # ResearchGroup type for GraphQL
     class ResearchGroup < WikirateCard
-      lookup_field :metric, Metric
-
+      lookup_field :metric, Metric, nil, true
     end
   end
 end
