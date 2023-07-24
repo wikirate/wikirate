7af9d8cd3e10ba3d94a7ece99220ca79149f5dfd
vasgat <vasgat@gmail.com>
2023-07-24 13:26:00 +0200

    updating fields definition on Topic

--- mod/deckorate_graphql/lib/graph_q_l/types/topic.rb
+++ mod/deckorate_graphql/lib/graph_q_l/types/topic.rb
@@ -2,8 +2,8 @@ module GraphQL
   module Types
     # Topic type for GraphQL
     class Topic < WikirateCard
-      lookup_field :metric, Metric
-      cardtype_field :dataset, Dataset
+      lookup_field :metric, Metric, nil, true
+      cardtype_field :dataset, Dataset, nil, true
       field :description, String, null: true
     end
   end
