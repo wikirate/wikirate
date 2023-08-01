b39fb312ac9664ec515fb8605164c59082b89ac9
Ethan McCutchen <ethan@decko.org>
2023-08-01 11:50:56 -0400

    reseeded, started in on new menu handling

---
+++ mod/deckorate_utility/set/all/wikirate/menu.rb
@@ -0,0 +1,11 @@
+format :html do
+  def bar_menu_items
+    [
+      full_page_link(text: "Open in full page"),
+      new_window_link(text: "Open in new window"),
+      # modal_page_link(text: "modal"),
+      edit_link(:edit, text: card.new? ? "Create" : "Edit"),
+      board_link(text: "Advanced")
+    ]
+  end
+end
