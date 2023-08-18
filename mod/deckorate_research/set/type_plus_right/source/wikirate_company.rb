7895862ee956d956eb46c1d4c6d6cc4db63cf9d6
Ethan McCutchen <ethan@decko.org>
2023-08-18 14:43:50 -0400

    stop filtering companies on sources for now

--- mod/deckorate_research/set/type_plus_right/source/wikirate_company.rb
+++ mod/deckorate_research/set/type_plus_right/source/wikirate_company.rb
@@ -1,5 +1,11 @@
-include_set Abstract::CompanySearch
-include_set Abstract::FilterableList
+# TODO: make companies filterable on sources
+# (The code below was breaking several specs)
+#
+# (see also mod/deckorate_graphql/lib/graph_q_l/types/source.rb)
+
+# include_set Abstract::CompanySearch
+# include_set Abstract::FilterableList
+
 def ok_item_types
   :wikirate_company
 end
