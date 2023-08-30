ce5afd85549d69e0af8691020eb4bd6e0ecea3bf
Christina Schweipert <44006691+cschweipert@users.noreply.github.com>
2023-08-30 12:05:39 -0600

    convert alert banner closing script into coffee script
    and add to manifest.yml

--- mod/wikirate/assets/script/alert_banner.js
+++
@@ -1,8 +0,0 @@
-// slides up the alert banner on click
-$(document).ready(function() {
-    if ($('#homepage-alert').length) {
-        $('#close-alert-button').click(function() {
-          $('#homepage-alert').slideUp("slow");
-        });
-    }
-});
