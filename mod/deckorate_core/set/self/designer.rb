63eb589e39f4b6c01e319544d9fb99bf72a10ef5
Ethan McCutchen <ethan@decko.org>
2023-09-21 15:38:37 -0400

    simple designer view

--- mod/deckorate_core/set/self/designer.rb
+++ mod/deckorate_core/set/self/designer.rb
@@ -1,4 +1,5 @@
-include_set Abstract::Search
+# include_set Abstract::Search
+# include_set Abstract::SearchViews
 
 def count
   ::Metric.select(:designer_id).distinct.count
@@ -6,5 +7,27 @@ end
 
 def count_label
   "Metric designers"
+end
+
+def designers_by_count
+  ::Metric.connection.exec_query(
+    "select designer_id, count(*) as count from metrics
+     group by designer_id order by count desc"
+  ).rows
+end
+
+
+# def search _args
+#   designers_by_count.map do |designer_id, _count|
+#     designer_id.card
+#   end
+# end
+
+format :html do
+  view :core, template: :haml
+  #
+  # def paging_needed?
+  #   false
+  # end
 end
 
