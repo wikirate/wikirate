875f573b5d4645733a804fb68b8c3eaabd997ef8
vasgat <vasgat@gmail.com>
2023-09-26 16:23:43 +0200

    rename card_search to wikirate_card_search

--- mod/deckorate_graphql/lib/graph_q_l/types/deckorate_search.rb
+++ mod/deckorate_graphql/lib/graph_q_l/types/deckorate_search.rb
@@ -2,7 +2,7 @@ module GraphQL
   module Types
     # DeckorateSearch module facilitates card and lookup searches
     module DeckorateSearch
-      def card_search codename, limit, offset, is_card, filter
+      def wikirate_card_search codename, limit, offset, is_card, filter
         cql = codename.card.format.filter_cql_class.new(filter).to_cql
         search_base = is_card ? object.card.fetch(codename) : ::Card
         cql[:limit] = limit
