e607e005db4fe383ba559c3c3a011e71be03ee00
vasgat <vasgat@gmail.com>
2023-09-26 15:40:04 +0200

    minor edits

--- mod/deckorate_graphql/lib/graph_q_l/types/deckorate_fields.rb
+++ mod/deckorate_graphql/lib/graph_q_l/types/deckorate_fields.rb
@@ -3,7 +3,7 @@ module GraphQL
     # Decorate Fields for GraphQL contains a number of functions
     # to facilitate the definition of different GraphQL types
     module DeckorateFields
-      def lookup_field(fieldname, type, codename = nil, is_card = false)
+      def lookup_field fieldname, type, codename=nil, is_card=false
         codename ||= fieldname
         plural_fieldname = fieldname.to_s.to_name.vary(:plural).to_sym
         is_card ||= is_card
@@ -13,13 +13,13 @@ module GraphQL
         end
       end
 
-      def card_field(fieldname, type, codename = nil)
+      def card_field fieldname, type, codename=nil
         codename ||= fieldname
         singular_field fieldname, type
         define_method(fieldname) { |**mark| ok_card codename, **mark }
       end
 
-      def cardtype_field(fieldname, type, codename = nil, is_card = false)
+      def cardtype_field fieldname, type, codename=nil, is_card=false
         codename ||= fieldname
         plural_fieldname = fieldname.to_s.to_name.vary(:plural).to_sym
         plural_field plural_fieldname, codename, type
@@ -28,14 +28,14 @@ module GraphQL
         end
       end
 
-      def singular_field(fieldname, type)
+      def singular_field fieldname, type
         field fieldname, type, null: true do
           argument :name, String, required: false
           argument :id, Integer, required: false
         end
       end
 
-      def plural_field(fieldname, codename, type)
+      def plural_field fieldname, codename, type
         field fieldname, [type], null: false do
           argument :limit, Integer, required: false
           argument :offset, Integer, required: false
