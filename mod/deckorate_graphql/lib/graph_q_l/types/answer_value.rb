b21a91eb8ed9b19a0960cc430bd638531ecfb0fb
vasgat <vasgat@gmail.com>
2023-09-27 17:24:09 +0200

    addressing code climate issues

--- mod/deckorate_graphql/lib/graph_q_l/types/answer_value.rb
+++ mod/deckorate_graphql/lib/graph_q_l/types/answer_value.rb
@@ -2,41 +2,38 @@ module GraphQL
   module Types
     # AnswerValue for GraphQL
     class AnswerValue < BaseScalar
-      # self.coerce_input takes a GraphQL input
-      # and converts it into a Ruby value
-      def self.coerce_input input_value, _context
-        case input_value
-        when Float, Integer
-          input_value
-        when String
-          input_value.include?(",") ? input_value.split(", ") : input_value
-        else
-          input_value
+      class << self
+        # self.coerce_input takes a GraphQL input
+        # and converts it into a Ruby value
+        def coerce_input input_value, _context
+          case input_value
+          when Float, Integer
+            input_value
+          when String
+            input_value.include?(",") ? input_value.split(", ") : input_value
+          else
+            input_value
+          end
         end
-      end
 
-      # self.coerce_result takes the return value of a field and
-      # prepares it for the GraphQL response JSON
-      def self.coerce_result ruby_value, _context
-        return Integer(ruby_value) if integer?(ruby_value)
-        return Float(ruby_value) if float?(ruby_value)
-        return ruby_value.split(", ") if ruby_value.is_a?(String) and
-          ruby_value.include?(",")
-        ruby_value
-      end
+        # self.coerce_result takes the return value of a field and
+        # prepares it for the GraphQL response JSON
+        def coerce_result ruby_value, _context
+          return Integer(ruby_value) if integer?(ruby_value)
+          return Float(ruby_value) if float?(ruby_value)
+          return ruby_value.split(", ") if ruby_value.is_a?(String) && ruby_value.include?(",")
+          ruby_value
+        end
 
-      def self.integer? value
-        begin
+        def integer? value
           Integer(value)
-        rescue ArgumentError => e
+        rescue ArgumentError
           false
         end
-      end
 
-      def self.float? value
-        begin
+        def float? value
           Float(value)
-        rescue ArgumentError => e
+        rescue ArgumentError
           false
         end
       end
