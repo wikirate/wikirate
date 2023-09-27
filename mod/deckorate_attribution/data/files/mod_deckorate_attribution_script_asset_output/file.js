b1e4ea1a9c9ab426080a9c798e63d68b4992d9dc
Ethan McCutchen <ethan@decko.org>
2023-09-27 14:26:35 -0400

    fixed attribution alert bug and updated fixtures

--- mod/deckorate_attribution/data/files/mod_deckorate_attribution_script_asset_output/file.js
+++ mod/deckorate_attribution/data/files/mod_deckorate_attribution_script_asset_output/file.js
@@ -1,4 +1,4 @@
 // attribution.js.coffee
-(function(){$(window).ready(function(){return $("._export-button").on("click",function(){return $(this).closest("._attributable-export").find("._hidden-attribution-alert-link").trigger("click")})})}).call(this);
+(function(){$(document).ready(function(){return $("body").on("click","._export-button",function(){return $(this).closest("._attributable-export").find("._hidden-attribution-alert-link").trigger("click")})})}).call(this);
 // clipboard.js.coffee
-(function(){$(function(){return $("body").on("click",".copy-button",function(){var o;return o=$("#clipboard").text(),navigator.clipboard.writeText(o).then(function(){return console.log("Text copied to clipboard: "+o)})["catch"](function(o){return console.error("Copy to clipboard failed:",o)})})})}).call(this);
+(function(){$(function(){return $("body").on("click",".copy-button",function(){var t,o,n;return o=$(this).closest(".tab-pane.active").find("._clipboard").html(),t=new Blob([o],{type:"text/html"}),n=new ClipboardItem({"text/html":t}),navigator.clipboard.write([n]).then(function(){return console.log("Text copied to clipboard: "+o)})["catch"](function(t){return console.error("Copy to clipboard failed:",t)})})})}).call(this);
