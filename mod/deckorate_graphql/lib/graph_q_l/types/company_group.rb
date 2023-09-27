d429a4b99fcbc18a5e34e55100225bf2dec72a81
vasgat <vasgat@gmail.com>
2023-09-27 16:25:22 +0200

    edits on CompanyGroup type

--- mod/deckorate_graphql/lib/graph_q_l/types/company_group.rb
+++ mod/deckorate_graphql/lib/graph_q_l/types/company_group.rb
@@ -2,7 +2,11 @@ module GraphQL
   module Types
     # CompanyGroup type for GraphQL
     class CompanyGroup < WikirateCard
-      cardtype_field :company, Company, :wikirate_company, true
+      field :companies, [Company], null: true
+
+      def companies
+        object.wikirate_company_card.item_cards
+      end
     end
   end
 end
