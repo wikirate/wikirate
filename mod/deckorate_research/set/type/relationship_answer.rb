429fccee153c804914520b36a3bba788fdd22c43
vasgat <vasgat@gmail.com>
2023-09-27 18:21:31 +0200

    edits to make climate code happy

--- mod/deckorate_research/set/type/relationship_answer.rb
+++ mod/deckorate_research/set/type/relationship_answer.rb
@@ -11,7 +11,7 @@ card_accessor :source, type: :list
 
 require_field :value
 require_field :source, when: :source_required?
-delegate *%i[inverse_metric_id subject_company_id object_company_id], to: :lookup
+delegate :inverse_metric_id, :subject_company_id, :object_company_id, to: :lookup
 delegate :relationship_id, to: :lookup
 
 event :ensure_simple_answers, after: :prepare_left_and_right, on: :save do
