0a4fb6215832b8a373917fcfca10416feb3beec4
vasgat <vasgat@gmail.com>
2023-09-29 22:37:50 +0200

    addressing additional codeclimate issues

--- script/swagger_helper.rb
+++ script/swagger_helper.rb
@@ -5,7 +5,7 @@ require "json"
 
 EXTRA_FILTER_HELP = {
   designer: ", available designers can be found [here](https://wikirate.org/:designer)"
-}
+}.freeze
 
 def deep_copy hash
   Marshal.load Marshal.dump(hash)
@@ -32,12 +32,12 @@ def filter_option_values base_codename, filter_name
   card_name_values base_codename.card.format.filter_option_values(filter_name)
 end
 
-def wikirate_cardtypes
+def fetch_wikirate_cardtypes
   %i[wikirate_company metric metric_answer relationship_answer source
    dataset wikirate_topic research_group company_group record]
 end
 
-def optional_subcards
+def fetch_optional_subcards
   {
     wikirate_company: Card::Set::Type::WikirateCompany::Export::NESTED_FIELD_CODENAMES,
     metric: %w[question about methodology unit topics value_options research_policy
@@ -52,7 +52,7 @@ def optional_subcards
   }
 end
 
-def required_subcards
+def fetch_required_subcards
   {
     wikirate_company: [],
     metric_answer: %w[value source],
@@ -66,7 +66,7 @@ def required_subcards
   }
 end
 
-def cardname_descriptions
+def fetch_cardname_descriptions
   descriptions = [
     "Given the company name. " \
       "The company name it can be also substituted with its numerical `~id`.",
@@ -111,13 +111,12 @@ def cardname_descriptions
 end
 
 def generate_swagger_spec input_schema, paths, parameters
-  schemas = Hash.new
+  schemas = {}
 
-  schema_files = Dir['./script/swagger/schemas/*'].sort_by { |file|
-    extract_prefix(File.basename(file))
-  }
+  dir = Dir["./script/swagger/schemas/*"]
+  schema_files = dir.sort_by { |file| extract_prefix(File.basename(file)) }
   schema_files.each do |f|
-    schema = YAML.load_file("#{f}")
+    schema = YAML.load_file(f)
     if schemas == {}
       schemas = schema
     else
@@ -125,7 +124,7 @@ def generate_swagger_spec input_schema, paths, parameters
     end
   end
 
-  securitySchemes = {
+  security_schemes = {
     "apiKey" => {
       "type" => "apiKey",
       "in" => "header",
@@ -140,23 +139,24 @@ def generate_swagger_spec input_schema, paths, parameters
     "servers" => input_schema["servers"],
     "externalDocs" => input_schema["externalDocs"],
     "paths" => paths,
-    "components" => { "securitySchemes" => securitySchemes,
+    "components" => { "securitySchemes" => security_schemes,
                       "parameters" => parameters,
                       "responses" => input_schema["components"]["responses"],
-                      "schemas" => schemas } }
+                      "schemas" => schemas }
+  }
 
   File.open("./script/swagger/output_spec.yml", "w") do |file|
     file.write(swagger.to_yaml)
   end
 end
 
-def initialize_filter_parameters(parameters, wikirate_cardtypes)
+def initialize_filter_parameters parameters, wikirate_cardtypes
   parameter_keys = []
   excluded_option_filters = %i[year designer updated status dataset
                                company_group wikirate_topic country]
   wikirate_cardtypes.each do |cardtype|
     cardtype.card.format.filter_keys.each do |i|
-      if not parameter_keys.include?("filter_by_#{i}")
+      unless parameter_keys.include?("filter_by_#{i}")
         parameter_keys.append "filter_by_#{i}"
         begin
           enumerated_values = filter_option_values(cardtype, i)
@@ -180,15 +180,15 @@ def initialize_filter_parameters(parameters, wikirate_cardtypes)
   end
 end
 
-def initialize_path_parameters(parameters, wikirate_cardtypes, cardname_description)
+def initialize_path_parameters parameters, wikirate_cardtypes, cardname_description
   wikirate_cardtypes.each do |cardtype|
-    parameters["#{cardtype.card.name.url_key.downcase}"] =
-      { "name" => "#{cardtype.card.name.url_key.downcase}",
-        "in" => 'path',
+    parameters[cardtype.card.name.url_key.downcase] =
+      { "name" => cardtype.card.name.url_key.downcase,
+        "in" => "path",
         "required" => true,
         "description" => cardname_description[cardtype],
         "schema" => { "type" => "string",
-                      "example" => "#{cardtype_example(cardtype)}" } }
+                      "example" => cardtype_example(cardtype) } }
   end
 end
 
