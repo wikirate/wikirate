c6062635c68355169bab1c7c1922de34ff6315cf
vasgat <vasgat@gmail.com>
2023-09-29 23:44:36 +0200

    final codeclimate touch-ups

--- script/swagger_automation.rb
+++ script/swagger_automation.rb
@@ -206,10 +206,9 @@ wikirate_cardtypes.each do |cardtype|
   optional_subcards[cardtype].each do |parameter|
     begin
       enumerated_values = filter_option_values(cardtype, parameter)
+      schema = { "type" => "string", "enum" => enumerated_values }
       if parameter == "year" || enumerated_values == []
         schema = { "type" => "string" }
-      else
-        schema = { "type" => "string", "enum" => enumerated_values }
       end
     rescue ArgumentError
       schema = { "type" => "string" }
