52ded1836f61880344df6c20d5cc8d9df1aed184
Ethan McCutchen <ethan@decko.org>
2023-11-01 10:28:48 -0400

    odssc code cleanup

--- mod/wikirate_companies/set/type/wikirate_company/export.rb
+++ mod/wikirate_companies/set/type/wikirate_company/export.rb
@@ -12,62 +12,16 @@ format :json do
 
   view :odyssey do
     options = { filter: { year: "latest", metric_type: "Researched" }, limit: 5 }
-    data = format_answer(lookup_search(:metric_answer, options))
+    data = format_odyssey_answer(odyssey_lookup_search(:metric_answer, options))
     {
       name: card.name,
       os_id: card.oar_id,
-      identifiers: { wikirate_id: card.id
+      identifiers: {
+        wikirate_id: card.id
       },
-      relationships: format_relationships(relationships),
+      relationships: format_odyssey_relationships(odyssey_relationships),
       data: data
     }
-  end
-
-  def lookup_search codename, options
-    query_hash = card.fetch(codename).query_hash.merge(options[:filter])
-    card.fetch(codename).query_class.new(
-      query_hash, {},
-      limit: options[:limit],
-      offset: options[:offset]
-    ).lookup_relation.all
-  end
-
-  def relationships
-    ::Relationship.where(
-      "subject_company_id = #{card.id} OR object_company_id = #{card.id}"
-    ).limit(10).all
-  end
-
-  def format_relationships answers
-    items = []
-    answers.each do |answer|
-      items.append({ metric: answer.metric.name,
-                     subject_company: odyssey_url(answer.subject_company_id),
-                     object_company: odyssey_url(answer.object_company_id),
-                     value: answer.value,
-                     year: answer.year,
-                     source: answer.source.card&.name
-                   })
-    end
-    items
-  end
-
-  def odyssey_url company_id
-    base_url = "#{Env.protocol}#{Env.host}"
-    card_name = company_id.card.name.url_key
-    "#{base_url}/#{card_name}.json?view=odyssey"
-  end
-
-  def format_answer answers
-    items = []
-    answers.each do |answer|
-      items.append({ metric: answer.metric.name,
-                     value: answer.value,
-                     year: answer.year,
-                     source: answer.source.card&.name
-                   })
-    end
-    items
   end
 
   def atom
@@ -84,10 +38,57 @@ format :json do
     end
   end
 
-  def add_fields_to_hash hash, view = :atom
+  private
+
+  def add_fields_to_hash hash, view=:atom
     NESTED_FIELD_CODENAMES.each do |fieldcode|
       hash[fieldcode] = field_nest fieldcode, view: view
     end
+  end
+
+  def odyssey_lookup_search codename, options
+    query_hash = card.fetch(codename).query_hash.merge(options[:filter])
+    card.fetch(codename).query_class.new(
+      query_hash, {},
+      limit: options[:limit],
+      offset: options[:offset]
+    ).lookup_relation.all
+  end
+
+  def odyssey_relationships
+    ::Relationship.where(
+      "subject_company_id = #{card.id} OR object_company_id = #{card.id}"
+    ).limit(10).all
+  end
+
+  def format_odyssey_relationships answers
+    items = []
+    answers.each do |answer|
+      items.append metric: answer.metric.name,
+                   subject_company: odyssey_url(answer.subject_company_id),
+                   object_company: odyssey_url(answer.object_company_id),
+                   value: answer.value,
+                   year: answer.year,
+                   source: answer.source.card&.name
+    end
+    items
+  end
+
+  def odyssey_url company_id
+    base_url = "#{Env.protocol}#{Env.host}"
+    card_name = company_id.card.name.url_key
+    "#{base_url}/#{card_name}.json?view=odyssey"
+  end
+
+  def format_odyssey_answer answers
+    items = []
+    answers.each do |answer|
+      items.append metric: answer.metric.name,
+                   value: answer.value,
+                   year: answer.year,
+                   source: answer.source.card&.name
+    end
+    items
   end
 end
 
