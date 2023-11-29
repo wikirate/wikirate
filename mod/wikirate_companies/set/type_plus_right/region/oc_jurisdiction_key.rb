fd3abc9a00e937a2ebdef63544522daf449bdb45
Christina Schweipert <44006691+cschweipert@users.noreply.github.com>
2023-11-28 17:56:31 -0700

    codeclimate fix

--- mod/wikirate_companies/set/type_plus_right/region/oc_jurisdiction_key.rb
+++ mod/wikirate_companies/set/type_plus_right/region/oc_jurisdiction_key.rb
@@ -6,12 +6,10 @@ event :clear_jurisdiction_key_cache do
   Card::Region.cache.reset_all
 end
 
-event :validate_jurisdiction_key, :validate, on: :save,
-      changed: :content do
-  oc_key = Card.search left: {type: :region},
+event :validate_jurisdiction_key, :validate,
+      on: :save, changed: :content do
+  oc_key = Card.search left: { type: :region },
                        right: :oc_jurisdiction_key,
                        content: card.content
-  if oc_key.present?
-    errors.add :content, "OC key already exists"
-  end
+  errors.add :content, "OC key already exists" if oc_key.present?
 end
--- mod/wikirate_companies/spec/set/type_plus_right/region/oc_jurisdiction_key_spec.rb
+++ mod/wikirate_companies/spec/set/type_plus_right/region/oc_jurisdiction_key_spec.rb
@@ -1,26 +1,26 @@
 RSpec.describe Card::Set::TypePlusRight::Region::OcJurisdictionKey do
-  context "validate_oc_key" do
+  context "when validation of oc keys" do
     let(:oc_key) { Card["Argentina+OpenCorporates Jurisdiction key"] }
 
-    it "fetches the right oc code." do
+    it "fetches the right oc key." do
       expect(oc_key.content).to eq("ar")
     end
 
-    it "does not allow duplicate oc_keys" do
+    it "does not allow duplicate oc keys" do
       expect { oc_key.update! content: "zw" }
         .to raise_error /OC key already exists/
     end
 
     it "updates the oc key if it doesn't already exist" do
-      expect {
+      expect do
         oc_key.update! content: "12"
-      }.to change { oc_key.reload.content }.to eq("12")
+      end.to(change { oc_key.reload.content }.to("12"))
     end
 
     it "does not update the oc key if the content is the same" do
-      expect {
+      expect do
         oc_key.update! content: oc_key.content
-      }.not_to change { oc_key.reload.content }
+      end.not_to(change { oc_key.reload.content })
     end
   end
 end
