24d0f5132f7b49de6acc4206481847eaf91c9dd6
Christina Schweipert <44006691+cschweipert@users.noreply.github.com>
2023-12-01 16:39:33 -0700

    revert

--- mod/wikirate_companies/set/type_plus_right/region/oc_jurisdiction_key.rb
+++ mod/wikirate_companies/set/type_plus_right/region/oc_jurisdiction_key.rb
@@ -8,7 +8,7 @@ end
 
 event :validate_jurisdiction_key, :validate,
       on: :save, changed: :content do
-  oc_keys = Card.search left: { type: :region },
+  oc_key = Card.search left: { type: :region },
                        right: :oc_jurisdiction_key,
                        content: card.content
   errors.add :content, "Open Corporates key already exists" if oc_key.present?
