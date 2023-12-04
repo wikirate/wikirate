2c42c3546d532e4c2de4eecd5f9dd4d4d3daf604
Christina Schweipert <44006691+cschweipert@users.noreply.github.com>
2023-12-04 15:57:01 -0700

    add country code validation

--- mod/wikirate_companies/set/type_plus_right/region/country_code.rb
+++ mod/wikirate_companies/set/type_plus_right/region/country_code.rb
@@ -1,3 +1,11 @@
 def ok_to_update
   Card::Auth.always_ok?
 end
+
+event :validate_country_code, :validate,
+      on: :save, changed: :content do
+  country_code = Card.search left: { type: :region },
+                       right: :country_code,
+                       content: card.content
+  errors.add :content, "Country Code already exists" if country_code.present?
+end
