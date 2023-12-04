fa6d63350cc79dfcf26c86fbfc5e108be72dcd26
Christina Schweipert <44006691+cschweipert@users.noreply.github.com>
2023-12-04 16:28:20 -0700

    codeclimate fix

--- mod/wikirate_companies/set/type_plus_right/region/country_code.rb
+++ mod/wikirate_companies/set/type_plus_right/region/country_code.rb
@@ -5,7 +5,7 @@ end
 event :validate_country_code, :validate,
       on: :save, changed: :content do
   country_code = Card.search left: { type: :region },
-                       right: :country_code,
-                       content: card.content
+                             right: :country_code,
+                             content: card.content
   errors.add :content, "Country Code already exists" if country_code.present?
 end
