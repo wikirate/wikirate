2f457419234ac9d46f89102a8d686c7386b19a70
Ethan McCutchen <ethan@decko.org>
2023-12-12 12:13:34 -0500

    fix company group related slowness

--- mod/wikirate_companies/set/type_plus_right/metric_answer/value.rb
+++ mod/wikirate_companies/set/type_plus_right/metric_answer/value.rb
@@ -1,7 +1,8 @@
 event :update_company_group_lists_based_on_metric,
       :integrate_with_delay, priority: 15 do
   metric_card.company_group_lists.each do |grouplist|
+    next if grouplist.count > 10_000
     grouplist.update_content_from_spec
-    grouplist.save! unless grouplist.count > 10_000
+    grouplist.save!
   end
 end
