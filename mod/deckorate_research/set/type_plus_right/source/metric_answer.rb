b5170e903eee83b117cc6cfd0868c805a4623344
Ethan McCutchen <ethan@decko.org>
2024-02-20 13:21:16 -0500

    field_recount patch

--- mod/deckorate_research/set/type_plus_right/source/metric_answer.rb
+++ mod/deckorate_research/set/type_plus_right/source/metric_answer.rb
@@ -14,12 +14,12 @@ end
 
 # ...or when metric is (un)published
 field_recount_trigger :type_plus_right, :metric, :unpublished do |changed_card|
-  answer_searches_for_sources changed_card.left&.source_card.item_cards
+  answer_searches_for_sources changed_card.left&.source_card&.item_cards
 end
 
 # ...or when answer is (un)published
 field_recount_trigger :type_plus_right, :metric_answer, :unpublished do |changed_card|
-  answer_searches_for_sources changed_card.left&.fetch(:source).item_cards
+  answer_searches_for_sources changed_card.left&.fetch(:source)&.item_cards
 end
 
 def self.answer_searches_for_sources sources
