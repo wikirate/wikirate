ffe671963f8ccb29235cb873f1d21c985ecdf669
Ethan McCutchen <ethan@decko.org>
2024-04-17 16:57:54 -0400

    fixes for errors in delayed jobs that appear for cards in trash

--- mod/deckorate_calculations/set/abstract/value.rb
+++ mod/deckorate_calculations/set/abstract/value.rb
@@ -1,4 +1,6 @@
 event :update_related_calculations, :integrate_with_delay,
       skip: :allowed, priority: 50 do
-  ensure_metric(metric_card).update_depender_values_for! company_id
+  return unless (m = try(:metric_card))
+
+  ensure_metric(m).update_depender_values_for! company_id
 end
