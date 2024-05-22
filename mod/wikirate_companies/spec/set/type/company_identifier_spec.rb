f266ed706210a964e98814abf9318dbff63936cd
Ethan McCutchen <ethan@decko.org>
2024-05-22 14:57:55 -0400

    company identifier spec fixes

--- mod/wikirate_companies/spec/set/type/company_identifier_spec.rb
+++ mod/wikirate_companies/spec/set/type/company_identifier_spec.rb
@@ -1,5 +1,5 @@
 RSpec.describe Card::Set::Type::CompanyIdentifier do
   it "caches excerpts" do
-    expect(described_class.excerpts).to eq(["Open Corporates ID", "OAR ID", "Wikipedia"])
+    expect(described_class.excerpts).to eq(["OpenCorporates ID", "OS ID", "Wikipedia"])
   end
 end
