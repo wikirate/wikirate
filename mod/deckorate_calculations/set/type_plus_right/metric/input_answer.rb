a193a29b222ffb99155774a14e1fb9f94ee3bed9
Ethan McCutchen <ethan@decko.org>
2024-07-24 13:17:16 -0400

    accordion icons, filter label, input_answer updates, etc

--- mod/deckorate_calculations/set/type_plus_right/metric/input_answer.rb
+++ mod/deckorate_calculations/set/type_plus_right/metric/input_answer.rb
@@ -8,7 +8,12 @@ include_set Abstract::FullAnswerSearch
 # recount number of answers for a given metric when an Answer card is
 # created or deleted
 recount_trigger :type, :metric_answer, on: %i[create delete] do |changed_card|
-  input_answer_and_source_fields changed_card.metric_card
+  return if changed_card.unpublished?
+
+  metric = changed_card.metric_card
+  return if metric.unpublished?
+
+  input_answer_and_source_fields metric
 end
 
 # ...or when metric is (un)published
