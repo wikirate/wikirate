d3663047982ad03ecc389d476272cb5384727d89
Ethan McCutchen <ethan@decko.org>
2024-08-22 09:44:20 -0400

    fix before block handling of cardtypes in non-html formats

--- mod/deckorate_profiles/set/type/cardtype.rb
+++ mod/deckorate_profiles/set/type/cardtype.rb
@@ -6,21 +6,17 @@ end
 
 format do
   before :header do
+    voo.variant = :plural
+  end
+end
+
+format :html do
+  before :header do
     title = standard_title.to_name.vary :plural
     title = "#{icon_tag card.codename} #{title}" if wr_icon?
     voo.title = title
   end
 
-  private
-
-  def wr_icon?
-    return false unless card.codename
-
-    basket[:icons][:wikirate].key? card.codename
-  end
-end
-
-format :html do
   def core_with_listing
     output [field_nest(:description),
             render_add_button,
@@ -30,4 +26,13 @@ format :html do
   def items_in_rows
     nest card.item_search_card, items: { view: :bar }
   end
+
+  private
+
+  def wr_icon?
+    return false unless card.codename
+
+    basket[:icons][:wikirate].key? card.codename
+  end
+
 end
