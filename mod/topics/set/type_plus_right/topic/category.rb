8e10b0b356a9964396705e05b27327becce95dcb
Ethan McCutchen <ethan@decko.org>
2024-09-26 16:22:20 -0400

    testing errors (and a legitimate bug or two) in category handling

--- mod/topics/set/type_plus_right/topic/category.rb
+++ mod/topics/set/type_plus_right/topic/category.rb
@@ -11,6 +11,14 @@ def option_names
   Card.search type: :topic, sort: :name, return: :name
 end
 
+def ok_to_create?
+  super && check_category_permissions
+end
+
+def ok_to_update?
+  super && check_category_permissions
+end
+
 format :html do
   def input_type
     :select
@@ -21,10 +29,17 @@ private
 
 def add_categories_to_topic_referers
   topic_referers.each do |pointer|
-    pointer.add_item recursive_categories
+    pointer.add_item left.recursive_categories
+    pointer.save
   end
 end
 
 def topic_referers
   Card.search right: :topic, refer_to: left.id
+end
+
+def check_category_permissions
+  return true if as_moderator?
+
+  deny_because "category editing is currently restricted (beta testing)"
 end
