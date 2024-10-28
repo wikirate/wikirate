c53f78effb83cd3c44d936336152fe4554f7eeb9
Ethan McCutchen <ethan@decko.org>
2024-10-28 19:49:27 -0400

    fixed ok_to_delete? recursion

--- mod/deckorate_core/set/abstract/designer_permissions.rb
+++ mod/deckorate_core/set/abstract/designer_permissions.rb
@@ -5,18 +5,22 @@
 # If the metric is "designer assessed", then cards with this set can only be edited by
 # the designer (or members of the Wikirate team)
 
-delegate :ok_to_delete?, to: :metric_card
-
-def check_designer_permissions action
-  return true if !metric_card || metric_card.ok_as_steward?
-
-  deny_because "Only metric stewards can #{action} this on designer-assessed metrics"
-end
-
 def ok_to_create?
   super && check_designer_permissions(:create)
 end
 
 def ok_to_update?
   super && check_designer_permissions(:update)
+end
+
+def ok_to_delete?
+  metric_card&.steward
+end
+
+private
+
+def check_designer_permissions action
+  return true if !metric_card || metric_card.ok_as_steward?
+
+  deny_because "Only metric stewards can #{action} this on designer-assessed metrics"
 end
