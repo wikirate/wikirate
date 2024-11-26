7d8e6df8765d3ace44af5a471478be6063c28133
Ethan McCutchen <ethan@decko.org>
2024-11-26 11:15:40 -0500

    reseeded with answer fixes

--- mod/deckorate_utility/data/files/mod_deckorate_utility_script_asset_output/file.js
+++ mod/deckorate_utility/data/files/mod_deckorate_utility_script_asset_output/file.js
@@ -1,9 +1,2 @@
 // flip.js.coffee
-(function() {
-  $(document).ready(function() {
-    return $("body").on("click", "._flip-card", function() {
-      return $(this).toggleClass("flipped");
-    });
-  });
-
-}).call(this);
+(function(){$(document).ready(function(){return $("body").on("click","._flip-card",function(){return $(this).toggleClass("flipped")})})}).call(this);
