3da8883928878f3e95747fb5794d045292080f18
Ethan McCutchen <ethan@decko.org>
2025-03-11 15:26:51 -0400

    topic families

--- mod/topics/set/type/topic/listing.rb
+++ mod/topics/set/type/topic/listing.rb
@@ -4,9 +4,9 @@ format :html do
   end
 
   view :category_badge do
-    return "" unless card.restricted_top_categories?
+    return "" unless card.restricted_topic_families?
 
-    nest card.top_category, view: :mini_badge
+    nest card.topic_family, view: :mini_badge
   end
 
   view :bar_left do
