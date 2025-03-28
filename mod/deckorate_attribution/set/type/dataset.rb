26e59d09e3ead04680c896fa9bf49cd46c53efab
Ethan McCutchen <ethan@decko.org>
2025-03-28 12:24:31 -0400

    refactor reference snapshots, lower priority

--- mod/deckorate_attribution/set/type/dataset.rb
+++ mod/deckorate_attribution/set/type/dataset.rb
@@ -4,8 +4,8 @@ include_set Abstract::Attributable
 #     organizer_card.item_names
 #   end
 
-def each_reference_dump_row &block
+def each_snapshot_row &block
   AnswerQuery.new(dataset: name).lookup_query.in_batches do |answers|
-    answers.each { |answer| answer.each_reference_dump_row(&block) }
+    answers.each { |answer| answer.each_snapshot_row(&block) }
   end
 end
