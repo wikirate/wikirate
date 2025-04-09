6271cb2b0e89ecf7ccccaa5df992f2200f8baf5e
Ethan McCutchen <ethan@decko.org>
2025-04-09 17:42:07 -0400

    white space

--- mod/deckorate_import/set/type/answer_import.rb
+++ mod/deckorate_import/set/type/answer_import.rb
@@ -4,7 +4,6 @@ include_set Abstract::WikirateImport
 # following shouldn't be necessary.  handle in Abstract::Import
 attachment :answer_import, uploader: CarrierWave::FileCardUploader
 
-
 def db_content= content
   raise "blank answer import file!!" if content.blank?
 
