16ac20a493a2a7502cffa7471b4215c50b34a844
Ethan McCutchen <ethan@decko.org>
2025-04-10 10:37:01 -0400

    fix bug with import attachments going blank

--- mod/deckorate_import/set/type/answer_import.rb
+++ mod/deckorate_import/set/type/answer_import.rb
@@ -4,12 +4,6 @@ include_set Abstract::WikirateImport
 # following shouldn't be necessary.  handle in Abstract::Import
 attachment :answer_import, uploader: CarrierWave::FileCardUploader
 
-def db_content= content
-  raise "blank answer import file!!" if content.blank?
-
-  super
-end
-
 def import_item_class
   AnswerImportItem
 end
