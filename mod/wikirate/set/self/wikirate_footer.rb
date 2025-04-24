dcb29109f1bf96de1687fb29395c26657a1af227
Ethan McCutchen <ethan@decko.org>
2025-04-24 12:07:29 -0400

    add impressum codename

--- mod/wikirate/set/self/wikirate_footer.rb
+++ mod/wikirate/set/self/wikirate_footer.rb
@@ -1,6 +1,6 @@
 include_set Abstract::SocialMedia
 
-LEGAL_CODENAMES = %i[policy privacy_policy terms_of_use].freeze
+LEGAL_CODENAMES = %i[policy privacy_policy terms_of_use impressum].freeze
 # community_guidelines disclaimers notice_and_take_down
 
 format :html do
@@ -8,12 +8,7 @@ format :html do
 
   def footer_legal_link_list
     LEGAL_CODENAMES.map do |codename|
-      link_to_card codename
-    end << impressum_link
-  end
-
-  def impressum_link
-    link_to "Impressum", title: "Impressum", target: "_legal",
-                         href: "https://wikirate-intl.org/Impressum"
+      link_to_card codename, nil, target: "_legal"
+    end
   end
 end
