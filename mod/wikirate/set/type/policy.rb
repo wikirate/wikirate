9c33f1ab0008330245a3ea70f12db1c5645e20d2
Ethan McCutchen <ethan@decko.org>
2025-05-07 15:00:41 -0400

    policy styling fix

--- mod/wikirate/set/type/policy.rb
+++ mod/wikirate/set/type/policy.rb
@@ -1,5 +1,12 @@
 format :html do
+  # override to avoid d0-card-content class in slot
+  def prepare_content_slot
+    # noop
+  end
+
   view :titled do
-    render_content
+    content_tag "div", class: "policy-container py-5" do
+      render_content
+    end
   end
 end
