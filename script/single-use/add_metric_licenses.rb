3d1ef9f4afe4e1d3daaafe406d92123d9faf907a
Ethan McCutchen <ethan@decko.org>
2025-06-12 18:08:38 -0400

    refine license adding

--- script/single-use/add_metric_licenses.rb
+++ script/single-use/add_metric_licenses.rb
@@ -1,7 +1,10 @@
 require File.expand_path "../../script_helper.rb", __FILE__
 
-Card::MetricQuery.new(
-  metric_type: [:researched, :relation, :inverse_relation]
-).run.each do |metric|
+Card.where(type_id: Card::MetricID).find_each do |metric|
+  metric.include_set_modules
+  next unless metric.researched? && !metric.license_card.real?
+
   metric.license_card.update! content: Card::Set::Right::License::LICENSES.first
+rescue
+  puts "failed to update #{metric.name}".red
 end
