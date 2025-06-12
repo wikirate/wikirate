06aa1b020a96afc1f9a29425a289db2e2a8c5a8f
Ethan McCutchen <ethan@decko.org>
2025-06-12 10:06:10 -0400

    assign_type cleanup

--- mod/deckorate_calculations/set/type_plus_right/metric/license.rb
+++ mod/deckorate_calculations/set/type_plus_right/metric/license.rb
@@ -1,3 +1,7 @@
+include_set Abstract::MetricChild, generation: 1
+
 event :cascade_license, :finalize, content: :changed do
-  # TODO
+  metric_card.direct_depender_metrics.each do |metric|
+    metric
+  end
 end
