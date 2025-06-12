bff24177025e18401e5787800454250267b0252d
Ethan McCutchen <ethan@decko.org>
2025-06-12 16:51:54 -0400

    no bang on infer!

--- mod/deckorate_calculations/set/type_plus_right/metric/license.rb
+++ mod/deckorate_calculations/set/type_plus_right/metric/license.rb
@@ -8,10 +8,10 @@ event :cascade_license, :finalize, changed: :content do
 end
 
 # used by calculated metrics to infer license from its inputs
-def infer!
+def infer
   return false unless metric_card.calculated?
 
-  update! content: compatible(metric_card.direct_dependee_metrics.map(&:license))
+  update content: compatible(metric_card.direct_dependee_metrics.map(&:license))
 end
 
 def ok_to_delete?
@@ -22,12 +22,12 @@ private
 
 def cascade_calculated_metric_licenses
   metric_card.direct_depender_metrics.each do |metric|
-    metric.license_card.infer!
+    metric.license_card.infer
   end
 end
 
 def cascade_dataset_licenses
   metric_card.dataset_card.item_cards.each do |dataset|
-    dataset.license_card.infer!
+    dataset.license_card.infer
   end
 end
