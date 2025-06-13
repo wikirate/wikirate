89c7b4df118e1d1a9e804321664a1917cc06cd91
Ethan McCutchen <ethan@decko.org>
2025-06-13 17:36:07 -0400

    take trashed metrics out of script updates

--- script/single-use/infer_calculated_metric_topics.rb
+++ script/single-use/infer_calculated_metric_topics.rb
@@ -1,6 +1,6 @@
 require File.expand_path "../../script_helper.rb", __FILE__
 
-Card.where(type_id: Card::MetricID).find_each do |metric|
+Card.where(type_id: Card::MetricID, trash: false).find_each do |metric|
   metric.include_set_modules
   next unless metric.researched?
 
