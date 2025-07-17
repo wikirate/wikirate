e4517afc4cd3939d14dc99739fc8c944394921a6
Ethan McCutchen <ethan@decko.org>
2025-07-17 10:57:57 -0400

    fixed missing framework in topic names

--- mod/topics/spec/set/type/topic_spec.rb
+++ mod/topics/spec/set/type/topic_spec.rb
@@ -40,7 +40,8 @@ RSpec.describe Card::Set::Type::Topic do
     it "does not raise error if category is acceptable" do
       expect { create_topic! "new topic", "Environment", :esg_topics }
         .not_to raise_error
-      expect("new topic".card.topic_family_card.first_name).to eq("Environment")
+      expect([:esg_topics, "new topic"].card.topic_family_card.first_name.right)
+        .to eq("Environment")
     end
   end
 end
