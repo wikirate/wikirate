3146eae98ac3f923aa3e6e3513b32dc218359189
Ethan McCutchen <ethan@decko.org>
2025-07-21 18:00:14 -0400

    trying to coax topic migrations through

--- mod/topics/data/transform/20250721180112_compound_topics.rb
+++ mod/topics/data/transform/20250721180112_compound_topics.rb
@@ -1,10 +1,17 @@
 # -*- encoding : utf-8 -*-
 
 class CompoundTopics < Cardio::Migration::Transform
+  FIELDS_TO_UPDATE = %i[topic topic_framework category]
+
   def up
     clean_up_frameworks
-    Card.search type: :topic do |topic|
-      update_topic topic
+    pausing_esg_families do
+      # compoundify_framework_assignments
+      Card.search type: :topic do |topic|
+        update_topic topic
+      end
+      updating_field_cards
+      update_clear_references
     end
   end
 
@@ -16,8 +23,32 @@ class CompoundTopics < Cardio::Migration::Transform
     end
   end
 
+  def update_field_cards
+    field_cards = Card.search right: { name: FIELDS_TO_UPDATE.clone.unshift(:in) }
+    field_cards.each do |card|
+      card.update! content: card.item_cards.map { |i| [:esg_topics, i].cardname }
+    end
+  end
+
+  def pausing_esg_families
+    cat = :esg_topics.card.category_card
+    oldname = cat.name
+    cat.update! name: "esg cat - placeholder"
+    yield
+    cat.update! name: oldname
+
+  end
+
+  # def compoundify_framework_assignments
+  #   Card.search left: { type: :topic }, right: :topic_framework do |field|
+  #     field.
+  #   end
+  # end
+
   def update_topic topic
     return if topic.name.compound?
+
+    puts "updating topic #{topic.name}".green
 
     framepoint = topic.topic_framework_card
     return no_framework(oldname) unless (framework = framepoint.first_name).present?
@@ -45,14 +76,14 @@ class CompoundTopics < Cardio::Migration::Transform
   def rename_conflicts oldname, action
     return unless @conflicts.present?
 
-    placeholder = Card.create! name: "#{oldname} - placeholder", type: :topic_title
+    placeholder = Card.ensure! name: "#{oldname} - placeholder", type: :topic_title
     fieldname = action == :add ? placeholder.name : oldname
 
     @conflicts.each do |c|
       c.update! name: [c.name.left, fieldname].cardname
     end
 
-    placeholder.delete!
+    placeholder.delete! if action == :delete
   end
 
   def no_framework oldname
