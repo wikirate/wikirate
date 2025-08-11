6f99db06dc945a714cdb65a83ba37653301280ec
Vasiliki Gkatziaki <vasgat@gmail.com>
2025-08-11 20:05:02 +0200

    playing with jsonld on companies

--- mod/wikirate_companies/set/type/company/export.rb
+++ mod/wikirate_companies/set/type/company/export.rb
@@ -130,3 +130,34 @@ format :csv do
     field_nest :answer, view: :titled
   end
 end
+
+format :jsonld do
+
+  def atom
+    {
+      "@id": "https://wikirate.org/Apple_Inc"      
+    }
+  end
+
+  def molecule
+    {
+      "@id": "https://wikirate.org/Apple_Inc"      
+    }
+  end
+
+  private
+
+  def add_fields_to_hash hash, view=:atom
+    card.simple_field_cards # prepopulate
+    card.simple_field_names.each do |fld|
+      key = fld.underscore.parameterize.underscore
+      hash[key] = field_val_from_cache fld, view
+    end
+  end
+
+  def field_val_from_cache field, view
+    return unless Card.cache.temp.exist? card.cardname.field_name(field).key
+
+    field_nest field, view: view
+  end
+end
