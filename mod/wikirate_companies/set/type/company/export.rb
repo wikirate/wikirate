a4a8891320118cc2c72b2672375baff757ff467c
Vasiliki Gkatziaki <vasgat@gmail.com>
2025-08-12 19:26:19 +0200

    define jsonld format on export for companies and topics

--- mod/wikirate_companies/set/type/company/export.rb
+++ mod/wikirate_companies/set/type/company/export.rb
@@ -132,32 +132,54 @@ format :csv do
 end
 
 format :jsonld do
-
-  def atom
-    {
-      "@id": "https://wikirate.org/Apple_Inc"      
-    }
-  end
-
-  def molecule
-    {
-      "@id": "https://wikirate.org/Apple_Inc"      
-    }
+  view :molecule do
+    company_jsonld(atom)
   end
 
   private
+  # ---- Company ---------------------------------------------------------------
 
-  def add_fields_to_hash hash, view=:atom
-    card.simple_field_cards # prepopulate
-    card.simple_field_names.each do |fld|
-      key = fld.underscore.parameterize.underscore
-      hash[key] = field_val_from_cache fld, view
-    end
+  def company_jsonld(a)
+    {
+      "@context" => "https://wikirate.org/context/Company.jsonld",
+      "@id"      => path(mark: card.name, format: nil),
+      "@type"    => "Company",
+
+      # core identity
+      "name"     => a[:name] || a["name"],
+      "alias"    => a[:alias] || a["alias"],
+      "logo"     => a[:image] || a["image"],
+      "country"  => a[:headquarters] || a["headquarters"],
+
+      # identifiers (map 1:1 from your atom)
+      "lei"                         => a[:legal_entity_identifier] || a["legal_entity_identifier"],
+      "isin"                        => a[:international_securities_identification_number] || a["international_securities_identification_number"],
+      "open_corporates_id"          => a[:open_corporates_id] || a["open_corporates_id"],
+      "wikidata_id"                 => a[:wikidata_id] || a["wikidata_id"],
+      "australian_business_number"  => a[:australian_business_number] || a["australian_business_number"],
+      "australian_company_number"   => a[:australian_company_number] || a["australian_company_number"],
+      "os_id"                       => a[:open_supply_id] || a["open_supply_id"],
+      "uk_company_number"           => a[:uk_company_number] || a["uk_company_number"],
+      "cik"                         => a[:sec_central_index_key] || a["sec_central_index_key"],
+
+      # outbound equivalences
+      "same_as" => same_as_from_atom(a)
+    }.compact
   end
 
-  def field_val_from_cache field, view
-    return unless Card.cache.temp.exist? card.cardname.field_name(field).key
+  def same_as_from_atom(a)
+    website = a[:website] || a["website"]
+    wiki    = a[:wikipedia] || a["wikipedia"]
+    lei    = a[:legal_entity_identifier] || a["legal_entity_identifier"]
+    oc      = a[:open_corporates_id] || a["open_corporates_id"]
+    wd      = a[:wikidata_id] || a["wikidata_id"]
 
-    field_nest field, view: view
+    links = []
+    links << website if website.present?
+    links << "https://en.wikipedia.org/wiki/#{wiki}" if wiki.present?
+    links << "https://opencorporates.com/companies/#{oc}" if oc.present?
+    links << "https://search.gleif.org/#/record/#{lei}" if lei.present?
+    links << "https://www.wikidata.org/wiki/#{wd}" if wd.present?
+    links.empty? ? nil : links
   end
 end
