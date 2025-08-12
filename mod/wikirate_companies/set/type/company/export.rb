7760f8bb078e73ca9d9ca56460a6e271d02c21b4
Vasiliki Gkatziaki <vasgat@gmail.com>
2025-08-12 19:31:09 +0200

    remove deprecated odyssey view

--- mod/wikirate_companies/set/type/company/export.rb
+++ mod/wikirate_companies/set/type/company/export.rb
@@ -7,20 +7,6 @@ format :json do
     []
   end
 
-  view :odyssey do
-    options = { filter: { year: "latest", metric_type: "Researched" }, limit: 5 }
-    data = format_odyssey_answer(odyssey_lookup_search(:answer, options))
-    {
-      name: card.name,
-      os_id: card.fetch(:oar_id)&.content,
-      identifiers: {
-        wikirate_id: card.id
-      },
-      relationships: format_odyssey_relationships(odyssey_relationships),
-      data: data
-    }
-  end
-
   def atom
     super.tap do |h|
       h.delete :content
@@ -51,50 +37,6 @@ format :json do
     field_nest field, view: view
   end
 
-  def odyssey_lookup_search codename, options
-    query_hash = card.fetch(codename).query_hash.merge(options[:filter])
-    card.fetch(codename).query_class.new(
-      query_hash, {},
-      limit: options[:limit],
-      offset: options[:offset]
-    ).lookup_relation.all
-  end
-
-  def odyssey_relationships
-    ::Relationship.where(
-      "subject_company_id = #{card.id} OR object_company_id = #{card.id}"
-    ).limit(10).all
-  end
-
-  def format_odyssey_relationships answers
-    items = []
-    answers.each do |answer|
-      items.append metric: answer.metric.name,
-                   subject_company: odyssey_url(answer.subject_company_id),
-                   object_company: odyssey_url(answer.object_company_id),
-                   value: answer.value,
-                   year: answer.year,
-                   source: answer.source.card&.name
-    end
-    items
-  end
-
-  def odyssey_url company_id
-    base_url = "#{Env.protocol}#{Env.host}"
-    card_name = company_id.card.name.url_key
-    "#{base_url}/#{card_name}.json?view=odyssey"
-  end
-
-  def format_odyssey_answer answers
-    items = []
-    answers.each do |answer|
-      items.append metric: answer.metric.name,
-                   value: answer.value,
-                   year: answer.year,
-                   source: answer.source.card&.name
-    end
-    items
-  end
 end
 
 format :csv do
