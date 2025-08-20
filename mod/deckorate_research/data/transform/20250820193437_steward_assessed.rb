d6967323956a54c2c3773056e7893cebd5da698e
Ethan McCutchen <ethan@decko.org>
2025-08-20 16:45:57 -0500

    fixed Steward Assessed migration

--- mod/deckorate_research/data/transform/20250820193437_steward_assessed.rb
+++ mod/deckorate_research/data/transform/20250820193437_steward_assessed.rb
@@ -2,6 +2,7 @@
 
 class StewardAssessed < Cardio::Migration::Transform
   def up
+    "Steward Assessed".card.delete!
     "Designer Assessed".card.update! name: "Steward Assessed", codename: :steward_assessed
   end
 end
