5c1d0b275eb51441c1ca982a592caf7c2ee0b302
Ethan McCutchen <ethan@decko.org>
2025-08-20 19:54:33 -0500

    update remaining research_policy references

--- mod/deckorate_graphql/lib/graph_q_l/types/assessment_filter_type.rb
+++ mod/deckorate_graphql/lib/graph_q_l/types/assessment_filter_type.rb
@@ -1,7 +1,7 @@
 module GraphQL
   module Types
     # Assessment FilterType to provide all available assessment options
-    class ResearchPolicyFilterType < FilterType
+    class AssessmentFilterType < FilterType
       description "Assessment on a metric can be either
       CommunityAssessed (anyone can research answers) or DesignerAssessed
       (only the designer can)"
--- mod/deckorate_research/spec/set/type_plus_right/metric/assessment_spec.rb
+++ mod/deckorate_research/spec/set/type_plus_right/metric/assessment_spec.rb
@@ -1,4 +1,4 @@
-RSpec.describe Card::Set::TypePlusRight::Metric::ResearchPolicy do
+RSpec.describe Card::Set::TypePlusRight::Metric::Assessment do
   it "updates lookup", as_bot: true do
     metric_card = sample_metric
     metric_card.assessment_card.update! content: "[[Steward Assessed]]"
