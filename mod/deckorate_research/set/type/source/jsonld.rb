255e33b42bf6b4ecc7eef907b89155d8652964e1
vasgat <vasgat@gmail.com>
2025-09-20 12:57:02 +0200

    refactor(jsonld): centralize helpers and clean up Source JSON-LD export

--- mod/deckorate_research/set/type/source/jsonld.rb
+++ mod/deckorate_research/set/type/source/jsonld.rb
@@ -6,31 +6,24 @@ format :jsonld do
 
   private
 
-    def source_jsonld  
+    def source_jsonld
         {
-            "@context" => "#{request.base_url}/context/#{card.type}.jsonld",
+            "@context" => context,
             "@id"      => path(mark: card.name, format: nil),
             "@type"    => card.type,
             "name"     => card.name,
             "file" => card.file,
-            "report_type" => get_report_type, 
+            "report_type" => card.report_type_card&.item_names.presence,
             "company" => get_company,
-            "year" => get_year       
+            "year" => card.year_card&.item_names.presence
 
         }.compact
     end
 
-    def get_report_type
-        card.report_type&.split("\n").presence
-    end
-
-    def get_year
-        card.year&.split("\n").presence
-    end
-
     def get_company
-        companies = card.company&.split("\n") || nil
-        companies&.any? ? companies.map { |company| path(mark: company, format: nil) } : nil
+        companies = card.company_card&.item_names
+        return unless companies.present?
+        companies.map { |name| path(mark: name, format: nil) }
     end
 
 end
