7efa494d3d244479bac652ea0829f72c1fbdf640
vasgat <vasgat@gmail.com>
2025-09-20 13:22:38 +0200

    refactor(jsonld): centralize helpers and clean up Topics JSON-LD export

--- mod/topics/set/type/topic/export.rb
+++ mod/topics/set/type/topic/export.rb
@@ -11,12 +11,12 @@ end
 format :jsonld do
   def molecule
     {
-      "@context": "#{request.base_url}/context/#{card.type}.jsonld",
-      "@id": path(mark: card.name),
+      "@context": context,
+      "@id": resource_iri,
       "@type": card.type,
       "name": card.name.parents[1],
       "description": card.fetch("Overview")&.content,
-      "inScheme": path(mark: card.name.left), 
+      "inScheme": path(mark: card.name.left),
       "parent": get_parent,
       "children": get_children
     }.compact
@@ -25,19 +25,15 @@ format :jsonld do
   private
 
   def get_parent
-    category = card.fetch(:category)
-    return nil unless category
+    category = card.category_card
+    return unless category
     path mark: category&.item_cards[0].name if category
   end
 
   def get_children
     subtopics = card.fetch("subtopics")
-    
-    return [] unless subtopics
-
-    subtopics.item_cards.map do |c|
-      path mark: c.name
-    end
+    return unless subtopics.present?
+    subtopics.item_names.map { |name| path(mark: name, format: nil) }
   end
 end
 
