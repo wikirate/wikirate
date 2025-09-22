86c6f485626fc63a3298b7b60b44b520edb3dc1b
Ethan McCutchen <ethan@decko.org>
2025-09-22 16:43:37 -0400

    more jsonld code polish

--- mod/deckorate_core/set/abstract/answer/jsonld.rb
+++ mod/deckorate_core/set/abstract/answer/jsonld.rb
@@ -1,18 +1,17 @@
 format :jsonld do
-  def get_value(metric)
-    metric.value_type == "Multi-Category" ? card.value&.split(", ") : card.value
+  def get_value metric
+    metric.multi_categorical? ? card.value&.split(", ") : card.value
   end
 
   def get_sources
-    sources = card.source_card&.item_names
-    return unless sources.present?
-    sources.map { |name| path(mark: name, format: nil) }
+    card.source_card.item_names.map { |name| path(mark: name, format: nil) }.presence
   end
 
   def get_unit metric
-    if metric.metric_type == "Relation" || metric.metric_type == "Inverse Relation"
-      return "related companies"
+    if metric.metric_type.in? ["Relation", "Inverse Relation"]
+      "related companies"
+    else
+      metric.unit.presence
     end
-    metric.unit.presence
   end
 end
