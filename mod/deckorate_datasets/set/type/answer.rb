e60bb8523613eab2d65719328d1919ee6076a056
Ethan McCutchen <ethan@decko.org>
2025-10-09 13:47:18 -0400

    no more answer+dataset search_type card

--- mod/deckorate_datasets/set/type/answer.rb
+++ mod/deckorate_datasets/set/type/answer.rb
@@ -1,1 +1,5 @@
-card_reader :dataset, type: :search_type
+card_reader :dataset, type: :pointer
+# Genuinely don't understand why the above was needed. It was set to :search, which
+# is not a type. Then it was set to :search_type, and many tests started breaking.
+# I couldn't find a reason why it was needed, so I remove it, and again many things
+# broke. Would be nice to be able to get rid of it.  Seems like nonsense.
