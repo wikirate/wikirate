e5cd148e67dd956cd259ba4d70ceaeab9b9a14ae
vasgat <vasgat@gmail.com>
2025-11-19 16:40:00 +0100

    feat(api): update JSON atom (unit added, fields reorganized, blanks removed)

--- mod/deckorate_core/set/type/answer/json.rb
+++ mod/deckorate_core/set/type/answer/json.rb
@@ -4,10 +4,15 @@ format :json do
     super.tap do |atom|
       atom.delete :content
       atom[:metric] = lookup.metric_name
-      %i[company year value source comments].each do |key|
+      %i[company year value].each do |key|
         atom[key] = lookup.send key
       end
+      atom[:unit] = get_unit(lookup.metric)
+      atom[:source] = lookup.source
+      atom[:comments] = lookup.comments
       atom[:answer_url] = path mark: card.name.left, format: :json
+
+      atom.delete_if { |_k, v| v.blank? }
     end
   end
 
@@ -26,5 +31,13 @@ format :json do
 
   def atom_content?
     false
+  end
+
+  def get_unit metric
+    if metric.metric_type.in? ["Relation", "Inverse Relation"]
+      "related companies"
+    else
+      metric.unit.presence
+    end
   end
 end
