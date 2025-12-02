6b0b6541b4601cc90f99255ba980288afc9b41f0
vasgat <vasgat@gmail.com>
2025-12-02 17:28:00 +0100

    refactor: move context method under jsonld format

--- mod/deckorate_utility/set/abstract/jsonld_support.rb
+++ mod/deckorate_utility/set/abstract/jsonld_support.rb
@@ -3,15 +3,17 @@ format do
     nest metric.license_card, view: :url
   end
 
-  def context
-    "#{request&.base_url}/context/#{card.type}.jsonld"
-  end
-
   def get_unit metric
     if metric.metric_type.in? ["Relation", "Inverse Relation"]
       "related companies"
     else
       metric.unit.presence
     end
+  end
+end
+
+format :jsonld do
+  def context
+    "#{request&.base_url}/context/#{card.type}.jsonld"
   end
 end
